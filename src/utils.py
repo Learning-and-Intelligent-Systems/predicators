7621f9fe087e8b72801db17ad9407f9b337e4c53
Rohan Chitnis <ronuchit@mit.edu>
2021-09-29 16:38:56 -0400

    base env and approach

---
+++ src/utils.py
@@ -0,0 +1,26 @@
+"""General utility methods.
+"""
+
+from typing import Sequence, Callable, Tuple
+from predicators.src.structs import _Option, State
+from numpy.typing import ArrayLike
+
+
+def option_to_actions(
+        init: State, simulator: Callable[[State, ArrayLike], State],
+        option: _Option) -> Tuple[Sequence[State], Sequence[ArrayLike]]:
+    """Convert an option into a sequence of actions by invoking the
+    option policy. Also return the state sequence.
+    """
+    actions = []
+    assert option.initiable(init)
+    state = init
+    states = [state]
+    while True:
+        if option.terminal(state):
+            break
+        act = option.policy(state)
+        actions.append(act)
+        state = simulator(state, act)
+        states.append(state)
+    return states, actions
