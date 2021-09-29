b2d578f9de3e032934ff030c4ec0eae46e8ca06b
Rohan Chitnis <ronuchit@mit.edu>
2021-09-29 16:53:33 -0400

    minor naming

--- src/utils.py
+++ src/utils.py
@@ -6,11 +6,12 @@ from numpy.typing import ArrayLike
 from predicators.src.structs import _Option, State
 
 
-def option_to_actions(
+def option_to_trajectory(
         init: State, simulator: Callable[[State, ArrayLike], State],
         option: _Option) -> Tuple[Sequence[State], Sequence[ArrayLike]]:
-    """Convert an option into a sequence of actions by invoking the
-    option policy. Also return the state sequence.
+    """Convert an option into a trajectory, starting at init, by invoking
+    the option policy. This trajectory is a tuple of (state sequence,
+    action sequence), where the state sequence includes init.
     """
     actions = []
     assert option.initiable(init)
@@ -23,4 +24,5 @@ def option_to_actions(
         actions.append(act)
         state = simulator(state, act)
         states.append(state)
+    assert len(states) == len(actions)+1
     return states, actions
