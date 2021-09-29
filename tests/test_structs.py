aea438373e107558d8b655a1b2d8a083a1428dd6
Rohan Chitnis <ronuchit@mit.edu>
2021-09-28 21:03:14 -0400

    structs

--- tests/test_structs.py
+++ tests/test_structs.py
@@ -1,11 +1,55 @@
 """Test cases for structs.
 """
 
-from predicators.src import structs
+import pytest
+from predicators.src.structs import ObjectType, Object, State
 
 
-def test_test_struct():
-    """Test TestStruct.
+def test_object_type():
+    """Tests for ObjectType class.
     """
-    struct = structs.TestStruct()
-    assert struct.x == 3
+    name = "test"
+    feats = ["feat1", "feat2"]
+    my_type = ObjectType(name, feats)
+    assert my_type.name == name
+    assert my_type.dim == len(my_type.feature_names) == len(feats)
+    assert my_type.feature_names == feats
+
+
+def test_object():
+    """Tests for Object class.
+    """
+    my_name = "obj"
+    my_type = ObjectType("type", ["feat1", "feat2"])
+    obj = Object(my_name, my_type)
+    assert obj.name == my_name
+    assert obj.type == my_type
+    assert str(obj) == "obj:type"
+
+
+def test_state():
+    """Tests for State class.
+    """
+    type1 = ObjectType("type1", ["feat1", "feat2"])
+    type2 = ObjectType("type2", ["feat3", "feat4", "feat5"])
+    obj3 = Object("obj3", type1)
+    obj7 = Object("obj7", type1)
+    obj1 = Object("obj1", type2)
+    obj4 = Object("obj4", type2)
+    obj9 = Object("obj9", type2)
+    assert obj7 > obj1
+    assert obj1 < obj4
+    assert obj1 < obj3
+    with pytest.raises(AssertionError):
+        State({obj3: [1, 2, 3]})  # bad feature vector dimension
+    state = State({obj3: [1, 2],
+                   obj7: [3, 4],
+                   obj1: [5, 6, 7],
+                   obj4: [8, 9, 10],
+                   obj9: [11, 12, 13]})
+    sorted_objs = list(state)
+    assert sorted_objs == [obj1, obj3, obj4, obj7, obj9]
+    assert state[obj9] == [11, 12, 13]
+    vec = state.vec([obj3, obj1])
+    assert vec.shape == (5,)
+    assert list(vec) == [1, 2, 5, 6, 7]
