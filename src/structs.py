573b79a4f106f35695334595b2b65dd6176c52f0
Tom Silver <tomssilver@gmail.com>
2021-09-30 13:04:48 -0400

    more oracle wip

--- src/structs.py
+++ src/structs.py
@@ -110,7 +110,7 @@ class State:
     def get(self, obj: Object, feature_name: str) -> np.float32:
         """Look up an object feature by name.
         """
-        idx = obj.feature_names.index(feature_name)
+        idx = obj.type.feature_names.index(feature_name)
         return self.data[obj][idx]
 
     def vec(self, objects: Sequence[Object]) -> Array:
@@ -299,6 +299,16 @@ class ParameterizedOption:
     # which is True iff the option should terminate now.
     _terminal: Callable[[State, Array], bool] = field(repr=False)
 
+    @cached_property
+    def _hash(self) -> int:
+        return hash(str(self))
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __hash__(self):
+        return self._hash
+
     def ground(self, params: Array) -> _Option:
         """Ground into an Option, given parameter values.
         On the Option that is returned, one can call, e.g., policy(state).
@@ -350,9 +360,9 @@ class Operator:
     def _str(self) -> str:
         return f"""{self.name}:
     Parameters: {self.parameters}
-    Preconditions: {self.preconditions}
-    Add Effects: {self.add_effects}
-    Delete Effects: {self.delete_effects}
+    Preconditions: {sorted(self.preconditions, key=lambda a:a.predicate)}
+    Add Effects: {sorted(self.add_effects, key=lambda a:a.predicate)}
+    Delete Effects: {sorted(self.delete_effects, key=lambda a:a.predicate)}
     Option: {self.option}"""
 
     @cached_property
@@ -400,9 +410,9 @@ class _GroundOperator:
     def _str(self) -> str:
         return f"""{self.name}:
     Parameters: {self.objects}
-    Preconditions: {self.preconditions}
-    Add Effects: {self.add_effects}
-    Delete Effects: {self.delete_effects}
+    Preconditions: {sorted(self.preconditions, key=lambda a:a.predicate)}
+    Add Effects: {sorted(self.add_effects, key=lambda a:a.predicate)}
+    Delete Effects: {sorted(self.delete_effects, key=lambda a:a.predicate)}
     Option: {self.option}"""
 
     @cached_property
--- tests/approaches/test_oracle_approach.py
+++ tests/approaches/test_oracle_approach.py
@@ -1,9 +1,56 @@
 """Test cases for the oracle approach class.
 """
 from absl import flags
+import numpy as np
 from predicators.configs.envs import cover_config
 from predicators.src.approaches import OracleApproach
+from predicators.src.approaches.oracle_approach import _get_gt_ops
 from predicators.src.envs import CoverEnv
+from predicators.src import utils
+
+
+def test_get_gt_ops():
+    """Tests for _get_gt_ops.
+    """
+    # All predicates and options
+    flags.env = cover_config.get_config()
+    env = CoverEnv()
+    operators = _get_gt_ops(env.predicates, env.options)
+    assert len(operators) == 2
+    pick_operator, place_operator = sorted(operators, key=lambda o: o.name)
+    assert pick_operator.name == "Pick"
+    assert place_operator.name == "Place"
+    env.seed(123)
+    train_task = env.get_train_tasks()[0]
+    state = train_task.init
+    block0, _, _, target0, _ = sorted(state.data.keys())
+    assert block0.name == "block0"
+    assert target0.name == "target0"
+    pick0_operator = pick_operator.ground([block0])
+    rng = np.random.RandomState(123)
+    pick_param = pick0_operator.sampler(state, rng)
+    pick_option = pick0_operator.option.ground(pick_param)
+    pick_action = pick_option.policy(state)
+    assert env.action_space.contains(pick_action)
+    state = env.simulate(state, pick_action)
+    place0_operator = place_operator.ground([block0, target0])
+    place_param = place0_operator.sampler(state, rng)
+    place_option = place0_operator.option.ground(place_param)
+    place_action = place_option.policy(state)
+    assert env.action_space.contains(place_action)
+    # Excluded option
+    assert _get_gt_ops(env.predicates, set()) == set()
+    # Excluded predicate
+    predicates = {p for p in env.predicates if p.name != "Holding"}
+    operators = _get_gt_ops(predicates, env.options)
+    assert len(operators) == 2
+    pick_operator, place_operator = sorted(operators, key=lambda o: o.name)
+    for atom in pick_operator.preconditions:
+        assert atom.predicate.name != "Holding"
+    for atom in pick_operator.add_effects:
+        assert atom.predicate.name != "Holding"
+    for atom in pick_operator.delete_effects:
+        assert atom.predicate.name != "Holding"
 
 
 def test_oracle_approach():
@@ -11,8 +58,26 @@ def test_oracle_approach():
     """
     flags.env = cover_config.get_config()
     env = CoverEnv()
-    approach = OracleApproach(env.simulate, env.predicates,
-                              env.options, env.action_space)
-    # Test get gt operators
-    operators = approach._get_current_operators()
-    
+    env.seed(123)
+    approach = OracleApproach(env.simulate, env.predicates, env.options,
+                              env.action_space)
+    approach.seed(123)
+    for task in env.get_train_tasks():
+        policy = approach.solve(task, timeout=500)
+        _check_policy(task, env.simulate, env.predicates, policy)
+    for task in env.get_test_tasks():
+        policy = approach.solve(task, timeout=500)
+        _check_policy(task, env.simulate, env.predicates, policy)
+
+
+def _check_policy(task, simulator, predicates, policy):
+    state = task.init
+    atoms = utils.abstract(state, predicates)
+    assert not task.goal.issubset(atoms)  # goal shouldn't be already satisfied
+    for _ in range(100):
+        act = policy(state)
+        state = simulator(state, act)
+        atoms = utils.abstract(state, predicates)
+        if task.goal.issubset(atoms):
+            break
+    assert task.goal.issubset(atoms)
