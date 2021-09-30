bbfd199342333cf8b6dd3104a6864bd96a2e9b99
Tom Silver <tomssilver@gmail.com>
2021-09-30 08:10:22 -0400

    switch to using cover env for unit test

--- tests/approaches/test_random_actions_approach.py
+++ tests/approaches/test_random_actions_approach.py
@@ -1,30 +1,19 @@
 """Test cases for the random actions approach class.
 """
-from gym.spaces import Box  # type: ignore
 from predicators.src.approaches import RandomActionsApproach
-from predicators.src.structs import State, Type, Predicate, Task
+from predicators.src.envs import CoverEnv
 
 
 def test_random_actions_approach():
     """Tests for RandomActionsApproach class.
     """
-    cup_type = Type("cup_type", ["feat1"])
-    plate_type = Type("plate_type", ["feat1", "feat2"])
-    pred1 = Predicate("On", [cup_type, plate_type], _classifier=None)
-    pred2 = Predicate("Is", [cup_type, plate_type, plate_type],
-                      _classifier=None)
-    predicates = {pred1, pred2}
-    cup = cup_type("cup")
-    plate1 = plate_type("plate1")
-    plate2 = plate_type("plate2")
-    state = State({cup: [0.5], plate1: [1.0, 1.2], plate2: [-9.0, 1.0]})
-    goal = {pred1([cup, plate1])}
-    task = Task(state, goal)
-    action_space = Box(0, 0.5, (1,))
-    approach = RandomActionsApproach(lambda s, a: s, predicates, set(),
-                                     action_space)
+    env = CoverEnv()
+    tasks = env.get_train_tasks()
+    task = tasks[0]
+    approach = RandomActionsApproach(env.simulate, env.predicates,
+                                     env.options, env.action_space)
     approach.seed(123)
     policy = approach.solve(task, 500)
     for _ in range(10):
-        act = policy(state)
-        assert action_space.contains(act)
+        act = policy(task.init)
+        assert env.action_space.contains(act)
