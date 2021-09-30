8bdf88355755658393d62b03dd89282fee82c067
Tom Silver <tomssilver@gmail.com>
2021-09-30 09:26:20 -0400

    move cover config into new file

--- configs/envs/cover_config.py
+++ configs/envs/cover_config.py
@@ -9,4 +9,10 @@ def get_config() -> ml_collections.ConfigDict:
     """
     config = default_env_config.get_config()
     config.name = "Cover"
+    config.num_blocks = 2
+    config.num_targets = 2
+    config.num_train_tasks = 5
+    config.num_test_tasks = 10
+    config.block_widths = [0.1, 0.07]
+    config.target_widths = [0.05, 0.03]
     return config
