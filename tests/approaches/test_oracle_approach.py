f9fb65aef92bdd516791a1a0248aa8f043598a01
Rohan Chitnis <ronuchit@mit.edu>
2021-09-30 17:47:42 -0400

    tests for planning failures

--- tests/approaches/test_oracle_approach.py
+++ tests/approaches/test_oracle_approach.py
@@ -6,10 +6,15 @@ import ml_collections
 import numpy as np
 import pytest
 from predicators.configs.envs import cover_config
-from predicators.src.approaches import OracleApproach
+from predicators.configs.approaches import tamp_approach_config
+from predicators.src.approaches import OracleApproach, ApproachFailure, \
+    ApproachTimeout, TAMPApproach
 from predicators.src.approaches.oracle_approach import _get_gt_ops
 from predicators.src.envs import CoverEnv
+from predicators.src.structs import Task, State
 from predicators.src import utils
+
+CONFIG = tamp_approach_config.get_config()
 
 
 def test_cover_get_gt_ops():
@@ -64,8 +69,8 @@ def test_get_gt_ops():
         _get_gt_ops(set(), set())
 
 
-def test_oracle_approach():
-    """Tests for OracleApproach class.
+def test_oracle_approach_cover():
+    """Tests for OracleApproach class with CoverEnv.
     """
     flags.env = cover_config.get_config()
     env = CoverEnv()
@@ -79,6 +84,50 @@ def test_oracle_approach():
     for task in env.get_test_tasks():
         policy = approach.solve(task, timeout=500)
         _check_policy(task, env.simulate, env.predicates, policy)
+
+
+def test_oracle_approach_cover_failures():
+    """Tests for failures in the OracleApproach.
+    """
+    flags.env = cover_config.get_config()
+    env = CoverEnv()
+    env.seed(123)
+    approach = OracleApproach(env.simulate, env.predicates, env.options,
+                              env.action_space)
+    approach.seed(123)
+    task = env.get_train_tasks()[0]
+    trivial_task = Task(task.init, set())
+    policy = approach.solve(trivial_task, timeout=500)
+    with pytest.raises(ApproachFailure):
+        policy(task.init)  # plan should get exhausted immediately
+    assert len(task.goal) == 1
+    Covers = next(iter(task.goal)).predicate
+    block0 = [obj for obj in task.init if obj.name == "block0"][0]
+    target0 = [obj for obj in task.init if obj.name == "target0"][0]
+    target1 = [obj for obj in task.init if obj.name == "target1"][0]
+    impossible_task = Task(task.init, {Covers([block0, target0]),
+                                       Covers([block0, target1])})
+    with pytest.raises(ApproachTimeout):
+        approach.solve(impossible_task, timeout=0.1)  # times out
+    with pytest.raises(ApproachTimeout):
+        approach.solve(impossible_task, timeout=-100)  # times out
+    old_val = CONFIG["max_samples_per_step"]
+    CONFIG["max_samples_per_step"] = 1
+    with pytest.raises(ApproachTimeout):
+        approach.solve(impossible_task, timeout=1)  # backtracking occurs
+    CONFIG["max_samples_per_step"] = old_val
+    operators = _get_gt_ops(env.predicates, env.options)
+    operators = {op for op in operators if op.name == "Place"}
+    with pytest.raises(ApproachFailure):
+        # Goal is not dr-reachable, should fail fast.
+        TAMPApproach.sesame_plan(task, env.simulate, operators,
+                                 env.predicates, timeout=500, seed=123)
+    with pytest.raises(ApproachFailure):
+        # Goal is not dr-reachable, but we disable that check.
+        # Should run out of skeletons.
+        TAMPApproach.sesame_plan(task, env.simulate, operators,
+                                 env.predicates, timeout=500, seed=123,
+                                 check_dr_reachable=False)
 
 
 def _check_policy(task, simulator, predicates, policy):
