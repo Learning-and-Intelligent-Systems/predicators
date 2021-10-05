bf02999266237a92d3357d9d3f9a18abf6f87d76
Tom Silver <tomssilver@gmail.com>
2021-10-04 20:26:16 -0400

    remove replay demo for now, make PR

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -4,7 +4,6 @@ from typing import List
 from predicators.src.envs import BaseEnv
 from predicators.src.structs import Dataset, Task
 from predicators.src.datasets.demo_only import create_demo_data
-from predicators.src.datasets.demo_replay import create_demo_replay_data
 
 
 def create_dataset(env: BaseEnv, train_tasks: List[Task],
@@ -14,7 +13,4 @@ def create_dataset(env: BaseEnv, train_tasks: List[Task],
     if data_config["method"] == "demo":
         return create_demo_data(env, train_tasks,
                                 data_config["planning_timeout"])
-    if data_config["method"] == "demo+replay":
-        return create_demo_replay_data(env, train_tasks,
-                                       data_config["planning_timeout"])
     raise NotImplementedError("Unrecognized dataset method.")
