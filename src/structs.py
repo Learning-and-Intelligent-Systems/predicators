d757a9187d1c482754ffa4d4c085396fb5fdb534
Rohan Chitnis <ronuchit@mit.edu>
2021-10-06 22:10:39 -0400

    first pass at sampler learning

--- src/structs.py
+++ src/structs.py
@@ -514,4 +514,5 @@ Array = NDArray[np.float32]
 PyperplanFacts = FrozenSet[Tuple[str, ...]]
 ObjToVarSub = Dict[Object, Variable]
 VarToObjSub = Dict[Variable, Object]
-Transition = Tuple[Set[GroundAtom], _Option, Set[GroundAtom], Set[GroundAtom]]
+Transition = Tuple[State, Set[GroundAtom], _Option, Set[GroundAtom],
+                   Set[GroundAtom]]
