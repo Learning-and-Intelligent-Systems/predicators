ece81543456ad94a7036ad00b9db5dc6e522f05b
Rohan Chitnis <ronuchit@mit.edu>
2021-10-13 12:27:45 -0400

    update code for random options

--- src/approaches/random_options_approach.py
+++ src/approaches/random_options_approach.py
@@ -4,7 +4,7 @@
 from collections import defaultdict
 from typing import Callable, List
 from predicators.src.approaches import BaseApproach
-from predicators.src.structs import State, Task, Action
+from predicators.src.structs import State, Task, Action, _Option, DefaultOption
 from predicators.src.settings import CFG
 from predicators.src import utils
 
@@ -18,26 +18,25 @@ class RandomOptionsApproach(BaseApproach):
 
     def _solve(self, task: Task, timeout: int) -> Callable[[State], Action]:
         options = sorted(self._initial_options, key=lambda o: o.name)
-        plan: List[Action] = []
+        cur_option: _Option = DefaultOption
+        cur_option_ind = 0
         def _policy(state: State) -> Action:
-            nonlocal plan
-            if plan:
-                return plan.pop(0)
-            for _ in range(CFG.random_options_max_tries):
-                param_opt = options[self._rng.choice(len(options))]
-                types_to_objs = defaultdict(list)
-                for obj in state:
-                    types_to_objs[obj.type].append(obj)
-                objs = [types_to_objs[type][self._rng.choice(
-                    len(types_to_objs[type]))] for type in param_opt.types]
-                params = param_opt.params_space.sample()
-                opt = param_opt.ground(objs, params)
-                if opt.initiable(state):
-                    break
-            else:  # fall back to a random action
-                return Action(self._action_space.sample())
-            _, plan = utils.option_to_trajectory(
-                state, self._simulator, opt,
-                CFG.max_num_steps_option_rollout)
-            return plan.pop(0)
+            nonlocal cur_option, cur_option_ind
+            if cur_option is DefaultOption or cur_option.terminal(state):
+                for _ in range(CFG.random_options_max_tries):
+                    param_opt = options[self._rng.choice(len(options))]
+                    objs = utils.get_random_object_combination(
+                        list(state), param_opt.types, self._rng)
+                    params = param_opt.params_space.sample()
+                    opt = param_opt.ground(objs, params)
+                    if opt.initiable(state):
+                        cur_option = opt
+                        cur_option_ind = 0
+                        break
+                else:  # fall back to a random action
+                    return Action(self._action_space.sample())
+            act = cur_option.policy(state)
+            act.set_option((cur_option, cur_option_ind))
+            cur_option_ind += 1
+            return act
         return _policy
