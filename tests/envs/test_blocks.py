f069b7d54dc99ae136ed4c4155fca3cda24a0c66
Rohan Chitnis <ronuchit@mit.edu>
2021-10-19 23:46:54 -0400

    finish tests

--- tests/envs/test_blocks.py
+++ tests/envs/test_blocks.py
@@ -6,7 +6,7 @@ import numpy as np
 from predicators.src.envs import BlocksEnv
 from predicators.src import utils
 from predicators.src.structs import Action
-from predicators.src.envs import EnvironmentFailure
+from predicators.src.settings import CFG
 
 
 def test_blocks():
@@ -40,3 +40,73 @@ def test_blocks():
             with pytest.raises(NotImplementedError):
                 env.render(state, task, act)
         env.simulate(state, act)
+
+
+def test_blocks_failure_cases():
+    """Tests for the cases where simulate() is a no-op.
+    """
+    utils.update_config({"env": "blocks"})
+    env = BlocksEnv()
+    env.seed(123)
+    Pick = [o for o in env.options if o.name == "Pick"][0]
+    Stack = [o for o in env.options if o.name == "Stack"][0]
+    PutOnTable = [o for o in env.options if o.name == "PutOnTable"][0]
+    On = [o for o in env.predicates if o.name == "On"][0]
+    OnTable = [o for o in env.predicates if o.name == "OnTable"][0]
+    block_type = [t for t in env.types if t.name == "block"][0]
+    block0 = block_type("block0")
+    block1 = block_type("block1")
+    block2 = block_type("block2")
+    task = env.get_train_tasks()[0]
+    state = task.init
+    atoms = utils.abstract(state, env.predicates)
+    assert OnTable([block0]) in atoms
+    assert OnTable([block1]) in atoms
+    assert OnTable([block2]) not in atoms
+    assert On([block2, block1]) in atoms
+    # No block at this pose, pick fails
+    act = Pick.ground([block0], np.array(
+        [-1, -1, -1], dtype=np.float32)).policy(state)
+    next_state = env.simulate(state, act)
+    assert np.all(state[o] == next_state[o] for o in state)
+    # Object not clear, pick fails
+    act = Pick.ground([block1], np.array(
+        [0, 0, 0], dtype=np.float32)).policy(state)
+    next_state = env.simulate(state, act)
+    assert np.all(state[o] == next_state[o] for o in state)
+    # Cannot putontable or stack without picking first
+    act = Stack.ground([block1], np.array(
+        [0, 0, 0], dtype=np.float32)).policy(state)
+    next_state = env.simulate(state, act)
+    assert np.all(state[o] == next_state[o] for o in state)
+    act = PutOnTable.ground([], np.array(
+        [0.5, 0.5], dtype=np.float32)).policy(state)
+    next_state = env.simulate(state, act)
+    assert np.all(state[o] == next_state[o] for o in state)
+    # Perform valid pick
+    act = Pick.ground([block0], np.array(
+        [0, 0, 0], dtype=np.float32)).policy(state)
+    next_state = env.simulate(state, act)
+    assert np.any(state[o] != next_state[o] for o in state)
+    # Change the state
+    state = next_state
+    # Cannot pick twice in a row
+    act = Pick.ground([block2], np.array(
+        [0, 0, 0], dtype=np.float32)).policy(state)
+    next_state = env.simulate(state, act)
+    assert np.all(state[o] == next_state[o] for o in state)
+    # Cannot stack onto non-clear block
+    act = Stack.ground([block1], np.array(
+        [0, 0, CFG.blocks_block_size], dtype=np.float32)).policy(state)
+    next_state = env.simulate(state, act)
+    assert np.all(state[o] == next_state[o] for o in state)
+    # Cannot stack onto no block
+    act = Stack.ground([block1], np.array(
+        [-1, -1, CFG.blocks_block_size], dtype=np.float32)).policy(state)
+    next_state = env.simulate(state, act)
+    assert np.all(state[o] == next_state[o] for o in state)
+    # Cannot stack onto yourself
+    act = Stack.ground([block0], np.array(
+        [0, 0, CFG.blocks_block_size], dtype=np.float32)).policy(state)
+    next_state = env.simulate(state, act)
+    assert np.all(state[o] == next_state[o] for o in state)
