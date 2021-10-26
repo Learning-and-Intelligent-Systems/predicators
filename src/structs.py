7cd41dc84535956b70d652cce80d3ed6da40776d
Rohan Chitnis <ronuchit@mit.edu>
2021-10-26 17:57:13 -0400

    minor

--- src/structs.py
+++ src/structs.py
@@ -165,6 +165,8 @@ class Predicate:
         """Convenience method for generating Atoms.
         """
         assert len(entities) == self.arity
+        for ent, pred_type in zip(entities, self.types):
+            assert ent.type == pred_type
         if all(isinstance(ent, Variable) for ent in entities):
             return LiftedAtom(self, entities)
         if all(isinstance(ent, Object) for ent in entities):
