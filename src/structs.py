3c631e5c04fdb1c4d47caac9fd721140039fc32c
Rohan Chitnis <ronuchit@mit.edu>
2021-11-08 14:34:46 -0500

    minor

--- src/structs.py
+++ src/structs.py
@@ -104,7 +104,9 @@ class State:
     """Struct defining the low-level state of the world.
     """
     data: Dict[Object, Array]
-    simulator_state: Optional[Any] = None  # for implementing simulate
+    # Some environments will need to store additional simulator state, so
+    # this field is provided.
+    simulator_state: Optional[Any] = None
 
     def __post_init__(self) -> None:
         # Check feature vector dimensions.
