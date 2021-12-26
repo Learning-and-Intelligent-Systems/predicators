588d7c677a480e9cad83e1510c27ae2e9901ab14
Tom Silver <tomssilver@gmail.com>
2021-12-26 10:40:22 -0500

    representation and plannin

--- src/planning.py
+++ src/planning.py
@@ -166,7 +166,15 @@ def _skeleton_generator(task: Task,
             # Generate successors.
             metrics["num_nodes_expanded"] += 1
             for nsrt in utils.get_applicable_nsrts(ground_nsrts, node.atoms):
-                child_atoms = utils.apply_nsrt(nsrt, set(node.atoms))
+                # Remove side predicates from the set of atoms that we
+                # definitely expect to be true at this point in the plan.
+                # Note that we are removing the side predicates before the
+                # application of the NSRT, because if the side predicate
+                # appears in the effects, we still know that the effects
+                # will be true, so we don't want to remove them.
+                before_atoms = {a for a in node.atoms
+                                if a.predicate not in nsrt.side_predicates}
+                child_atoms = utils.apply_nsrt(nsrt, before_atoms)
                 child_node = _Node(
                     atoms=child_atoms,
                     skeleton=node.skeleton+[nsrt],
@@ -239,8 +247,12 @@ def _run_low_level_search(
                 # Check atoms against expected atoms_sequence constraint.
                 assert len(traj) == len(atoms_sequence)
                 atoms = utils.abstract(traj[cur_idx], predicates)
-                if atoms == {atom for atom in atoms_sequence[cur_idx]
-                             if atom.predicate.name != _NOT_CAUSES_FAILURE}:
+                # The expected atoms are ones that we definitely expect to be
+                # true at this point in the plan. They are not *all* the atoms
+                # that could be true.
+                expected_atoms = {atom for atom in atoms_sequence[cur_idx]
+                                  if atom.predicate.name != _NOT_CAUSES_FAILURE}
+                if atoms.issuperset(expected_atoms):
                     can_continue_on = True
                     if cur_idx == len(skeleton):  # success!
                         result = plan
