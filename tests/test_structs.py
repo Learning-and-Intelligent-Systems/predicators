af8fcfbf163138103485811f8a2a7386aa5d44c4
Rohan Chitnis <ronuchit@mit.edu>
2021-12-31 12:31:47 -0800

    move pddl_str to structs

--- tests/test_structs.py
+++ tests/test_structs.py
@@ -438,8 +438,9 @@ def test_nsrts():
     strips_operator2 = STRIPSOperator("Pick", parameters, preconditions,
                                       add_effects, delete_effects)
     assert strips_operator == strips_operator2
-    strips_operator3 = STRIPSOperator("Zick", parameters, preconditions,
-                                      add_effects, delete_effects)
+    strips_operator3 = STRIPSOperator("PickDuplicate", parameters,
+                                      preconditions, add_effects,
+                                      delete_effects)
     assert strips_operator < strips_operator3
     assert strips_operator3 > strips_operator
     # _GroundSTRIPSOperator
