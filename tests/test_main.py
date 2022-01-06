89b33138361320c53fd8ff16345069bcbe3e9909
Rohan Chitnis <ronuchit@mit.edu>
2022-01-05 17:53:19 -0800

    minor fix

--- tests/test_main.py
+++ tests/test_main.py
@@ -47,6 +47,12 @@ def test_main():
                 "--video_dir", video_dir]
     main()
     shutil.rmtree(video_dir)
+    results_dir = os.path.join(os.path.dirname(__file__), "_fake_results")
+    sys.argv = ["dummy", "--env", "cover", "--approach", "oracle",
+                "--seed", "123", "--num_test_tasks", "1"]
+                # "--results_dir", results_dir]
+    main()
+    shutil.rmtree(results_dir)
     # Try running main with a strong timeout.
     sys.argv = ["dummy", "--env", "cover", "--approach", "oracle",
                 "--seed", "123", "--timeout", "0.001", "--num_test_tasks", "5"]
@@ -92,10 +98,3 @@ def test_main():
                 "--excluded_predicates", "all",
                 "--num_test_tasks", "5"]
     main()  # correct usage
-    results_dir = os.path.join(os.path.dirname(__file__), "_fake_results")
-    sys.argv = ["dummy", "--env", "cover", "--approach", "oracle",
-                "--seed", "123", "--num_test_tasks", "1",
-                "--results_dir", results_dir]
-    main()
-    assert os.path.exists(results_dir)
-    shutil.rmtree(results_dir)
