4553e06358b90fb16bd09837c0f401d24a6bb065
Rohan Chitnis <ronuchit@mit.edu>
2022-01-10 16:47:33 -0500

    cannot __call__ a zero-arity predicate

--- src/structs.py
+++ src/structs.py
@@ -197,6 +197,10 @@ class Predicate:
 
     def __call__(self, entities: Sequence[_TypedEntity]) -> _Atom:
         """Convenience method for generating Atoms."""
+        if self.arity == 0:
+            raise ValueError("Cannot use __call__ on a 0-arity predicate, "
+                             "since we can't determine whether it becomes a "
+                             "LiftedAtom or a GroundAtom")
         if all(isinstance(ent, Variable) for ent in entities):
             return LiftedAtom(self, entities)
         if all(isinstance(ent, Object) for ent in entities):
