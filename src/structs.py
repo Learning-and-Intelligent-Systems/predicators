9c3f518d314ad5faaf2ec7d949d5660999bb4068
Rohan Chitnis <ronuchit@mit.edu>
2022-01-10 16:48:32 -0500

    minor

--- src/structs.py
+++ src/structs.py
@@ -200,7 +200,8 @@ class Predicate:
         if self.arity == 0:
             raise ValueError("Cannot use __call__ on a 0-arity predicate, "
                              "since we can't determine whether it becomes a "
-                             "LiftedAtom or a GroundAtom")
+                             "LiftedAtom or a GroundAtom. Use the LiftedAtom "
+                             "or GroundAtom constructors directly instead")
         if all(isinstance(ent, Variable) for ent in entities):
             return LiftedAtom(self, entities)
         if all(isinstance(ent, Object) for ent in entities):
