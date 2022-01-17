69eb3c200f05676da6a18a2fb7111bbedd25d07b
Rohan Chitnis <ronuchit@mit.edu>
2022-01-17 18:03:44 -0500

    minor fix

--- src/envs/painting.py
+++ src/envs/painting.py
@@ -577,14 +577,16 @@ class PaintingEnv(BaseEnv):
                            objects: Sequence[Object], params: Array) -> bool:
         # An initiation function for an option that requires holding an object.
         del objects, params  # unused
-        memory["start_state"] = state
+        if "start_state" not in memory:
+            memory["start_state"] = state
         return self._get_held_object(state) is not None
 
     def _handempty_initiable(self, state: State, memory: Dict,
                              objects: Sequence[Object], params: Array) -> bool:
         # An initiation function for an option that requires holding nothing.
         del objects, params  # unused
-        memory["start_state"] = state
+        if "start_state" not in memory:
+            memory["start_state"] = state
         return self._get_held_object(state) is None
 
     def _OpenLid_policy(self, state: State, memory: Dict,
