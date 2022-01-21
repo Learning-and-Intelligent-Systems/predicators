bdb41a7a34994aafa891e2cca69730857d2020af
Amber Li <amli@mit.edu>
2022-01-21 12:14:11 -0500

    better way to label while sticking to the ratio

--- src/approaches/interactive_learning_approach.py
+++ src/approaches/interactive_learning_approach.py
@@ -200,20 +200,24 @@ def create_teacher_dataset(preds: Collection[Predicate],
                            dataset: Dataset) -> List[GroundAtomTrajectory]:
     """Create sparse dataset of GroundAtoms for interactive learning."""
     ratio = float(CFG.teacher_dataset_label_ratio)
-    rng = np.random.default_rng(CFG.seed)
+    # Track running ratios of atoms that the teacher has commented on
+    labeleds = {p: 0 for p in preds}
+    totals = {p: 0 for p in preds}
     teacher_dataset: List[GroundAtomTrajectory] = []
     for traj in dataset:
         ground_atoms_traj: List[Set[GroundAtom]] = []
         for s in traj.states:
             ground_atoms = sorted(utils.abstract(s, preds))
-            # select random subset to keep
-            n_samples = int(len(ground_atoms) * ratio)
             subset_atoms = set()
-            if n_samples > 0:
-                subset = rng.choice(np.arange(len(ground_atoms)),
-                                    size=(n_samples, ),
-                                    replace=False)
-                subset_atoms = {ground_atoms[j] for j in subset}
+            for ga in ground_atoms:
+                pred = ga.predicate
+                if ratio == 0.0:
+                    continue
+                if totals[pred] == 0 or labeleds[pred] / totals[pred] <= ratio:
+                    # Teacher comments on this atom
+                    subset_atoms.add(ga)
+                    labeleds[pred] += 1
+                totals[pred] += 1
             ground_atoms_traj.append(subset_atoms)
         assert len(traj.states) == len(ground_atoms_traj)
         teacher_dataset.append((traj, ground_atoms_traj))
