87abe3f0fcb12c01e6a591e47e4bc99de6f21c56
Rohan Chitnis <ronuchit@mit.edu>
2022-01-25 14:25:49 -0500

    add --remake_data to supercloud script

--- analysis/run_supercloud_experiments.sh
+++ analysis/run_supercloud_experiments.sh
@@ -5,29 +5,30 @@ NUM_SEEDS=5
 FILE="analysis/submit.py"
 
 for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
+    COMMON_ARGS="--remake_data --seed $SEED"
+
     # cover
-    python $FILE --env cover --approach oracle --seed $SEED
-    python $FILE --env cover --approach nsrt_learning --seed $SEED
-    python $FILE --env cover --approach grammar_search_invention --seed $SEED
-    python $FILE --env cover --approach grammar_search_invention --seed $SEED --excluded_predicates all
+    python $FILE --env cover --approach oracle $COMMON_ARGS
+    python $FILE --env cover --approach nsrt_learning $COMMON_ARGS
+    python $FILE --env cover --approach grammar_search_invention $COMMON_ARGS
+    python $FILE --env cover --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS
 
     # blocks
-    python $FILE --env blocks --approach oracle --seed $SEED
-    python $FILE --env blocks --approach nsrt_learning --seed $SEED
-    python $FILE --env blocks --approach grammar_search_invention --seed $SEED
-    python $FILE --env blocks --approach grammar_search_invention --seed $SEED --excluded_predicates all
+    python $FILE --env blocks --approach oracle $COMMON_ARGS
+    python $FILE --env blocks --approach nsrt_learning $COMMON_ARGS
+    python $FILE --env blocks --approach grammar_search_invention $COMMON_ARGS
+    python $FILE --env blocks --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS
 
     # painting
-    python $FILE --env painting --approach oracle --seed $SEED
-    python $FILE --env painting --approach nsrt_learning --seed $SEED
-    python $FILE --env painting --approach grammar_search_invention --seed $SEED --grammar_search_grammar_includes_givens 0 --grammar_search_grammar_includes_foralls 0
-    python $FILE --env painting --approach grammar_search_invention --seed $SEED --excluded_predicates all --grammar_search_grammar_includes_givens 0 --grammar_search_grammar_includes_foralls 0
+    python $FILE --env painting --approach oracle $COMMON_ARGS
+    python $FILE --env painting --approach nsrt_learning $COMMON_ARGS
+    python $FILE --env painting --approach grammar_search_invention --grammar_search_grammar_includes_givens 0 --grammar_search_grammar_includes_foralls 0 $COMMON_ARGS
+    python $FILE --env painting --approach grammar_search_invention --excluded_predicates all --grammar_search_grammar_includes_givens 0 --grammar_search_grammar_includes_foralls 0 $COMMON_ARGS
 
     # repeated_nextto
-    python $FILE --env repeated_nextto --approach oracle --seed $SEED
-    python $FILE --env repeated_nextto --approach nsrt_learning --seed $SEED
+    python $FILE --env repeated_nextto --approach oracle $COMMON_ARGS
+    python $FILE --env repeated_nextto --approach nsrt_learning $COMMON_ARGS
 
     # cluttered_table
-    python $FILE --env cluttered_table --approach oracle --seed $SEED
-    python $FILE --env cluttered_table --approach nsrt_learning --seed $SEED
+    python $FILE --env cluttered_table --approach oracle $COMMON_ARGS
 done
