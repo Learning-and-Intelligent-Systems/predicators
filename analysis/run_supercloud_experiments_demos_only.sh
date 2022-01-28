7954fb4dec18c12ad18399600ee6d2431353dd20
Tom Silver <tomssilver@gmail.com>
2022-01-27 20:14:48 -0500

    fix silly mistake

--- analysis/run_supercloud_experiments_demos_only.sh
+++ analysis/run_supercloud_experiments_demos_only.sh
@@ -32,23 +32,23 @@ for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
     ## 200 max predicates and full painting grammar
 
     # cover
-    python $FILE --experiment_id cover_nsrt_learning_demo --env cover --approach nsrt_learning $COMMON_ARGS --grammar_search_max_predicates 200
-    python $FILE --experiment_id cover_none_excluded_demo --env cover --approach grammar_search_invention $COMMON_ARGS --grammar_search_max_predicates 200
-    python $FILE --experiment_id cover_all_excluded_demo --env cover --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_max_predicates 200
+    python $FILE --experiment_id cover_nsrt_learning_demo_large --env cover --approach nsrt_learning $COMMON_ARGS --grammar_search_max_predicates 200
+    python $FILE --experiment_id cover_none_excluded_demo_large --env cover --approach grammar_search_invention $COMMON_ARGS --grammar_search_max_predicates 200
+    python $FILE --experiment_id cover_all_excluded_demo_large --env cover --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_max_predicates 200
 
     # blocks
-    python $FILE --experiment_id blocks_nsrt_learning_demo --env blocks --approach nsrt_learning $COMMON_ARGS --grammar_search_max_predicates 200
-    python $FILE --experiment_id blocks_none_excluded_demo --env blocks --approach grammar_search_invention $COMMON_ARGS --grammar_search_max_predicates 200
-    python $FILE --experiment_id blocks_all_excluded_demo --env blocks --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_max_predicates 200
+    python $FILE --experiment_id blocks_nsrt_learning_demo_large --env blocks --approach nsrt_learning $COMMON_ARGS --grammar_search_max_predicates 200
+    python $FILE --experiment_id blocks_none_excluded_demo_large --env blocks --approach grammar_search_invention $COMMON_ARGS --grammar_search_max_predicates 200
+    python $FILE --experiment_id blocks_all_excluded_demo_large --env blocks --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS --grammar_search_max_predicates 200
 
     # painting
-    python $FILE --experiment_id painting_nsrt_learning_demo --env painting --approach nsrt_learning $COMMON_ARGS --grammar_search_max_predicates 200
-    python $FILE --experiment_id painting_none_excluded_demo --env painting --approach grammar_search_invention  $COMMON_ARGS --grammar_search_max_predicates 200
-    python $FILE --experiment_id painting_all_excluded_demo --env painting --approach grammar_search_invention --excluded_predicates all  $COMMON_ARGS --grammar_search_max_predicates 200
+    python $FILE --experiment_id painting_nsrt_learning_demo_large --env painting --approach nsrt_learning $COMMON_ARGS --grammar_search_max_predicates 200
+    python $FILE --experiment_id painting_none_excluded_demo_large --env painting --approach grammar_search_invention  $COMMON_ARGS --grammar_search_max_predicates 200
+    python $FILE --experiment_id painting_all_excluded_demo_large --env painting --approach grammar_search_invention --excluded_predicates all  $COMMON_ARGS --grammar_search_max_predicates 200
 
     # painting with box lid always open
-    python $FILE --experiment_id painting_always_open_nsrt_learning_demo --painting_lid_open_prob 1.0 --env painting --approach nsrt_learning $COMMON_ARGS --grammar_search_max_predicates 200
-    python $FILE --experiment_id painting_always_open_none_excluded_demo --painting_lid_open_prob 1.0 --env painting --approach grammar_search_invention  $COMMON_ARGS --grammar_search_max_predicates 200
-    python $FILE --experiment_id painting_always_open_all_excluded_demo --painting_lid_open_prob 1.0 --env painting --approach grammar_search_invention --excluded_predicates all  $COMMON_ARGS --grammar_search_max_predicates 200
+    python $FILE --experiment_id painting_always_open_nsrt_learning_demo_large --painting_lid_open_prob 1.0 --env painting --approach nsrt_learning $COMMON_ARGS --grammar_search_max_predicates 200
+    python $FILE --experiment_id painting_always_open_none_excluded_demo_large --painting_lid_open_prob 1.0 --env painting --approach grammar_search_invention  $COMMON_ARGS --grammar_search_max_predicates 200
+    python $FILE --experiment_id painting_always_open_all_excluded_demo_large --painting_lid_open_prob 1.0 --env painting --approach grammar_search_invention --excluded_predicates all  $COMMON_ARGS --grammar_search_max_predicates 200
 
 done
