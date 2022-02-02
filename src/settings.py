b5a7ab490822f1f52feb9e2c4ddb26710a4da596
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-02-02 15:09:16 -0500

    minor nit fixes

--- src/settings.py
+++ src/settings.py
@@ -206,7 +206,9 @@ class GlobalSettings:
 def get_allowed_query_type_names() -> Set[str]:
     """Get the set of names of query types that the teacher is allowed to
     answer, computed based on the configuration CFG."""
-    if CFG.option_learner == "neural" or CFG.approach == "interactive_learning":
+    if CFG.option_learner == "neural":
+        return {"DemonstrationQuery"}
+    if CFG.approach == "interactive_learning":
         return {"GroundAtomsHoldQuery"}
     if CFG.approach == "unittest":
         return {"GroundAtomsHoldQuery", "DemonstrationQuery"}
