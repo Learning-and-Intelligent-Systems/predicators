dcf398b7d9fa67ab530d17c0f73e5c947844b48a
Tom Silver <tomssilver@gmail.com>
2022-02-02 09:59:52 -0500

    wip

--- src/approaches/grammar_search_invention_approach.py
+++ src/approaches/grammar_search_invention_approach.py
@@ -255,6 +255,10 @@ _DEBUG_PREDICATE_PREFIXES = {
         "NOT-((0:block).grasp<=[idx 0]",  # Holding
         "Forall[0:block].[((0:block).grasp<=[idx 0]",  # HandEmpty
     ],
+    "cover_regrasp": [
+        "NOT-((0:block).grasp<=[idx 0]",  # Holding
+        "Forall[0:block].[((0:block).grasp<=[idx 0]",  # HandEmpty
+    ],
     "blocks": [
         "NOT-((0:robot).fingers<=[idx 0]0.5)",  # GripperOpen
         "Forall[0:block].[NOT-On(0,1)]",  # Clear
@@ -733,7 +737,7 @@ class _TaskPlanningScoreFunction(_OperatorLearningBasedScoreFunction):
                 _, _, metrics = next(
                     task_plan(init_atoms, traj_goal, ground_nsrts,
                               reachable_atoms, heuristic, CFG.seed,
-                              CFG.grammar_search_task_planning_timeout))
+                              CFG.grammar_search_task_planning_timeout, 1))
                 node_expansions = metrics["num_nodes_expanded"]
                 assert node_expansions < node_expansion_upper_bound
                 score += node_expansions
@@ -805,9 +809,14 @@ class _ExpectedNodesScoreFunction(_OperatorLearningBasedScoreFunction):
             # not been found, updated after each new goal-reaching skeleton is
             # considered.
             refinable_skeleton_not_found_prob = 1.0
+            if CFG.grammar_search_max_skeletons_optimized is None:
+                max_skeletons = CFG.max_skeletons_optimized
+            else:
+                max_skeletons = CFG.grammar_search_max_skeletons_optimized
             generator = task_plan(init_atoms, goal, ground_nsrts,
                                   reachable_atoms, heuristic, CFG.seed,
-                                  CFG.grammar_search_task_planning_timeout)
+                                  CFG.grammar_search_task_planning_timeout,
+                                  max_skeletons)
             try:
                 for idx, (_, plan_atoms_sequence,
                           metrics) in enumerate(generator):
@@ -1212,7 +1221,7 @@ class _ExactHeuristicBasedScoreFunction(_HeuristicBasedScoreFunction):
                 skeleton, atoms_sequence, _ = next(
                     task_plan(atoms, goal, ground_nsrts, reachable_atoms,
                               heuristic, CFG.seed,
-                              CFG.grammar_search_task_planning_timeout))
+                              CFG.grammar_search_task_planning_timeout, 1))
             except (ApproachFailure, ApproachTimeout):
                 return float("inf")
             assert atoms_sequence[0] == atoms
--- src/planning.py
+++ src/planning.py
@@ -90,7 +90,8 @@ def sesame_plan(
             new_seed = seed + int(metrics["num_failures_discovered"])
             for skeleton, atoms_sequence in _skeleton_generator(
                     task, reachable_nsrts, init_atoms, heuristic, new_seed,
-                    timeout - (time.time() - start_time), metrics):
+                    timeout - (time.time() - start_time), metrics,
+                    CFG.max_skeletons_optimized):
                 plan, suc = _run_low_level_search(
                     task, option_model, skeleton, atoms_sequence, new_seed,
                     timeout - (time.time() - start_time))
@@ -159,6 +160,7 @@ def task_plan(
     heuristic: _TaskPlanningHeuristic,
     seed: int,
     timeout: float,
+    max_skeletons_optimized: int,
 ) -> Iterator[Tuple[List[_GroundNSRT], List[Set[GroundAtom]], Metrics]]:
     """Run only the task planning portion of SeSamE. A* search is run, and
     skeletons that achieve the goal symbolically are yielded. Specifically,
@@ -181,18 +183,18 @@ def task_plan(
     dummy_task = Task(State({}), goal)
     metrics: Metrics = defaultdict(float)
     generator = _skeleton_generator(dummy_task, ground_nsrts, init_atoms,
-                                    heuristic, seed, timeout, metrics)
+                                    heuristic, seed, timeout, metrics,
+                                    max_skeletons_optimized)
     # Note that we use this pattern to avoid having to catch an ApproachFailure
     # when _skeleton_generator runs out of skeletons to optimize.
-    for skeleton, atoms_sequence in islice(generator,
-                                           CFG.max_skeletons_optimized):
+    for skeleton, atoms_sequence in islice(generator, max_skeletons_optimized):
         yield skeleton, atoms_sequence, metrics.copy()
 
 
 def _skeleton_generator(
     task: Task, ground_nsrts: List[_GroundNSRT], init_atoms: Set[GroundAtom],
     heuristic: _TaskPlanningHeuristic, seed: int, timeout: float,
-    metrics: Metrics
+    metrics: Metrics, max_skeletons_optimized: int
 ) -> Iterator[Tuple[List[_GroundNSRT], List[Set[GroundAtom]]]]:
     """A* search over skeletons (sequences of ground NSRTs).
     Iterates over pairs of (skeleton, atoms sequence).
@@ -209,8 +211,8 @@ def _skeleton_generator(
                 (heuristic(root_node.atoms), rng_prio.uniform(), root_node))
     # Start search.
     while queue and (time.time() - start_time < timeout):
-        if (int(metrics["num_skeletons_optimized"]) ==
-                CFG.max_skeletons_optimized):
+        if (int(metrics["num_skeletons_optimized"]) == max_skeletons_optimized
+            ):
             raise _MaxSkeletonsFailure(
                 "Planning reached max_skeletons_optimized!")
         _, _, node = hq.heappop(queue)
