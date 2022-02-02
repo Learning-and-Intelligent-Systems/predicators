d348920ee46a377959bb7d46ef6e9850f66aca11
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-02-02 15:40:08 -0500

    adds tests for settings.py query_type_names method

---
+++ tests/test_settings.py
@@ -0,0 +1,22 @@
+"""Test cases for some parts of the settings.py file."""
+
+from predicators.src.settings import get_allowed_query_type_names
+from predicators.src import utils
+
+
+def test_get_allowed_query_type_names():
+    """Test the get_allowed_query_type_names method."""
+    utils.reset_config({
+        "option_learner": "neural",
+    })
+    assert get_allowed_query_type_names() == {"DemonstrationQuery"}
+    utils.reset_config({
+        "option_learner": "no_learning",
+        "approach": "interactive_learning"
+    })
+    assert get_allowed_query_type_names() == {"GroundAtomsHoldQuery"}
+    utils.reset_config({
+        "option_learner": "no_learning",
+        "approach": "unittest"
+    })
+    assert {"GroundAtomsHoldQuery", "DemonstrationQuery"}
