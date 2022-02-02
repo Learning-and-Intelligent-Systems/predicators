1679fe53f2f05fed210865581006f29ba4f07a1e
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-02-02 13:32:00 -0500

    suggested fixes for #414

--- src/teacher.py
+++ src/teacher.py
@@ -8,7 +8,7 @@ from predicators.src.structs import State, Task, Query, Response,\
 from predicators.src.settings import CFG, get_allowed_query_type_names
 from predicators.src.envs import create_env
 from predicators.src.approaches import ApproachTimeout, ApproachFailure
-from predicators.src.approaches.oracle_approach import OracleApproach
+from predicators.src.approaches import create_approach
 from predicators.src import utils
 
 ################################################################################
@@ -23,13 +23,9 @@ class Teacher:
         env = create_env(CFG.env)
         self._pred_name_to_pred = {pred.name: pred for pred in env.predicates}
         self._allowed_query_type_names = get_allowed_query_type_names()
-        env_preds, _ = utils.parse_config_excluded_predicates(env)
-        # NOTE: this below var is not actually used, just passed in out
-        # of necessity
-        env_train_tasks = env.get_train_tasks()
-        self._oracle_approach = OracleApproach(env_preds, env.options,
-                                               env.types, env.action_space,
-                                               env_train_tasks)
+        self._oracle_approach = create_approach("oracle", env.predicates,
+                                                env.options, env.types,
+                                                env.action_space, [])
         self._simulator = env.simulate
 
     def answer_query(self, state: State, query: Query) -> Response:
@@ -57,16 +53,8 @@ class Teacher:
         try:
             policy = self._oracle_approach.solve(task, CFG.timeout)
         except (ApproachTimeout, ApproachFailure):
-            return DemonstrationResponse(query, None)
+            return DemonstrationResponse(query, teacher_traj=None)
 
-        def goal_reached(s: State) -> bool:
-            for ground_atom in query.goal:
-                pred = self._pred_name_to_pred[ground_atom.predicate.name]
-                if not pred.holds(s, ground_atom.objects):
-                    return False
-            return True
-
-        teacher_traj = utils.run_policy_until(policy, self._simulator, state,
-                                              goal_reached,
-                                              CFG.max_num_steps_option_rollout)
+        teacher_traj, _, _ = utils.run_policy_on_task(
+            policy, task, self._simulator, CFG.max_num_steps_option_rollout)
         return DemonstrationResponse(query, teacher_traj)
