477f61f2837389396afd8ba49323ce42fe004c56
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-02-03 00:21:44 -0500

    fixes tupleerror in behavior options

--- src/envs/behavior_options.py
+++ src/envs/behavior_options.py
@@ -604,7 +604,7 @@ def create_grasp_policy(
             # But if the corrective action is 0, take the next action
             if np.allclose(
                     low_level_action,
-                    np.zeros((env.action_space.shape, 1), dtype=np.float32),
+                    np.zeros((env.action_space.shape[0], 1), dtype=np.float32),
                     atol=atol_vel,
             ):
                 low_level_action = (get_delta_low_level_hand_action(
