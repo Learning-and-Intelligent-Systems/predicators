c761314dae371a96acd02243277faf8159dc1913
Rohan Chitnis <ronuchit@mit.edu>
2022-02-03 15:22:26 -0500

    sigh

--- analysis/run_supercloud_experiments.sh
+++ analysis/run_supercloud_experiments.sh
@@ -8,7 +8,7 @@ for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
     COMMON_ARGS="--seed $SEED"
 
     # tools
-    python $FILE --env tools --approach oracle $COMMON_ARGS
-    python $FILE --env tools --approach nsrt_learning $COMMON_ARGS
-    python $FILE --env tools --approach nsrt_learning $COMMON_ARGS --sampler_learner oracle
+    python $FILE --experiment_id oracleall --env tools --approach oracle $COMMON_ARGS
+    python $FILE --experiment_id learnall --env tools --approach nsrt_learning $COMMON_ARGS
+    python $FILE --experiment_id oraclesamplers --env tools --approach nsrt_learning $COMMON_ARGS --sampler_learner oracle
 done
