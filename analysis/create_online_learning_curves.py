e7a9b0e67f0c19a4ab0e756abec3f99bb5259d95
Tom Silver <tomssilver@gmail.com>
2022-02-05 17:36:22 -0500

    minor

--- analysis/create_online_learning_curves.py
+++ analysis/create_online_learning_curves.py
@@ -13,12 +13,33 @@ pd.options.mode.chained_assignment = None  # default='warn'
 
 PLOT_GROUPS = {
     ("cover_regrasp", "Cover (Regrasp)"): 
-    {"cover_regrasp_naive": "Naive",
-     "cover_regrasp_targeted": "Targeted"},
+    {
+        "cover_regrasp_naive_allexclude": "Naive (All Excluded)",
+        "cover_regrasp_targeted_allexclude": "Targeted (All Excluded)",
+    },
+    ("blocks", "Blocks"): 
+    {
+        "blocks_naive_allexclude": "Naive (All Excluded)",
+        "blocks_targeted_allexclude": "Targeted (All Excluded)",
+    },
+    ("painting", "Painting"): 
+    {
+        "painting_naive_allexclude": "Naive (All Excluded)",
+        "painting_targeted_allexclude": "Targeted (All Excluded)",
+    },
 }
 
 Y_KEY = "NUM_SOLVED"
 Y_LABEL = "Test tasks solved"
+
+
+def _get_df_for_experiment_id(df: pd.DataFrame, experiment_id: str
+                              ) -> pd.DataFrame:
+    df = df[df["EXPERIMENT_ID"] == experiment_id]
+    df["CYCLE"].replace("None", "-1", inplace=True)
+    df["CYCLE"] = df["CYCLE"].map(pd.to_numeric) + 1
+    df = df.sort_values("CYCLE")
+    return df
 
 
 def _main() -> None:
@@ -30,18 +51,17 @@ def _main() -> None:
     means = grouped_means.reset_index()
     stds = grouped_stds.reset_index()
     for (group_name, plot_title), experiment_id_dict in PLOT_GROUPS.items():
-        # TODO: add error bars
         _, ax = plt.subplots()
         for experiment_id, label in experiment_id_dict.items():
-            df = means[means["EXPERIMENT_ID"] == experiment_id]
-            df["CYCLE"].replace("None", "-1", inplace=True)
-            df["CYCLE"] = df["CYCLE"].map(pd.to_numeric) + 1
-            df = df.sort_values("CYCLE")
-            df.plot(x="CYCLE", y=Y_KEY, label=label, ax=ax)
+            exp_means = _get_df_for_experiment_id(means, experiment_id)
+            exp_stds = _get_df_for_experiment_id(stds, experiment_id)
+            ax.errorbar(exp_means["CYCLE"], exp_means[Y_KEY],
+                        yerr=exp_stds[Y_KEY], label=label)
         ax.xaxis.set_major_locator(MaxNLocator(integer=True))
         ax.set_title(plot_title)
         ax.set_xlabel("Num online learning episodes")
         ax.set_ylabel(Y_LABEL)
+        plt.legend()
         plt.tight_layout()
         outfile = os.path.join(outdir, f"{group_name}.png")
         plt.savefig(outfile)
