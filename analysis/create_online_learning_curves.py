603afdf303afa28047d981e9b1de06ddea81bec2
Tom Silver <tomssilver@gmail.com>
2022-02-06 12:55:08 -0500

    feedback

--- analysis/create_online_learning_curves.py
+++ analysis/create_online_learning_curves.py
@@ -11,17 +11,20 @@ pd.options.mode.chained_assignment = None  # default='warn'
 
 ############################ Change below here ################################
 
-# X_KEY is the name of the metric that will be plotted on the x axis. See
-# analyze_results_directory.py for all available metrics.
-# X_LABEL is used to label the x axis.
-X_KEY = "CYCLE"  # common: CYCLE, NUM_TRANSITIONS, LEARNING_TIME
-X_LABEL = "Num online learning episodes"
+# The first element is the name of the metric that will be plotted on the
+# x axis. See analyze_results_directory.py for all available metrics. The
+# second element is used to label the x axis.
+X_KEY_AND_LABEL = [
+    ("CYCLE", "Num online learning episodes"),
+    ("NUM_TRANSITIONS", "Num transitions"),
+    # ("LEARNING_TIME", "Learning time in seconds"),
+]
 
-# Y_KEY is the name of the metric that will be plotted on the y axes. See
-# analyze_results_directory.py for all available metrics.
-# Y_LABEL is used to label the y axis.
-Y_KEY = "AVG_NODES_CREATED"  # common: NUM_SOLVED, AVG_NODES_CREATED
-Y_LABEL = "Average nodes created"  #"Test tasks solved"
+# Same as above, but for the y axis.
+Y_KEY_AND_LABEL = [
+    ("NUM_SOLVED", "Num test tasks solved"),
+    ("AVG_NODES_CREATED", "Averaged nodes created"),
+]
 
 # PLOT_GROUPS is a nested dict where each outer dict corresponds to one plot,
 # and each inner entry corresponds to one line on the plot.
@@ -42,7 +45,7 @@ PLOT_GROUPS = {
 #################### Should not need to change below here #####################
 
 
-def _get_df_for_experiment_id(df: pd.DataFrame,
+def _get_df_for_experiment_id(x_key: str, df: pd.DataFrame,
                               experiment_id: str) -> pd.DataFrame:
     df = df[df["EXPERIMENT_ID"] == experiment_id]
     # Handle CYCLE as a special case, since the offline learning phase is
@@ -54,7 +57,7 @@ def _get_df_for_experiment_id(df: pd.DataFrame,
     # like num transitions or num queries, will be monotonically increasing
     # as the number of cycles increases.
     df = df.sort_values("CYCLE")
-    df[X_KEY] = df[X_KEY].map(pd.to_numeric)
+    df[x_key] = df[x_key].map(pd.to_numeric)
     return df
 
 
@@ -66,28 +69,32 @@ def _main() -> None:
     grouped_means, grouped_stds, _ = create_dataframes()
     means = grouped_means.reset_index()
     stds = grouped_stds.reset_index()
-    for plot_title, experiment_id_dict in PLOT_GROUPS.items():
-        _, ax = plt.subplots()
-        for experiment_id, label in experiment_id_dict.items():
-            exp_means = _get_df_for_experiment_id(means, experiment_id)
-            exp_stds = _get_df_for_experiment_id(stds, experiment_id)
-            ax.errorbar(exp_means[X_KEY],
-                        exp_means[Y_KEY],
-                        yerr=exp_stds[Y_KEY],
-                        label=label)
-        # Automatically make x ticks integers for certain X KEYS.
-        if X_KEY in ("CYCLE", "NUM_TRANSITIONS"):
-            ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-        ax.set_title(plot_title)
-        ax.set_xlabel(X_LABEL)
-        ax.set_ylabel(Y_LABEL)
-        plt.legend()
-        plt.tight_layout()
-        filename = f"{plot_title}_{X_KEY}_{Y_KEY}.png"
-        filename = filename.replace(" ", "_").lower()
-        outfile = os.path.join(outdir, filename)
-        plt.savefig(outfile)
-        print(f"Wrote out to {outfile}.")
+    for x_key, x_label in X_KEY_AND_LABEL:
+        for y_key, y_label in Y_KEY_AND_LABEL:
+            for plot_title, experiment_id_dict in PLOT_GROUPS.items():
+                _, ax = plt.subplots()
+                for experiment_id, label in experiment_id_dict.items():
+                    exp_means = _get_df_for_experiment_id(
+                        x_key, means, experiment_id)
+                    exp_stds = _get_df_for_experiment_id(
+                        x_key, stds, experiment_id)
+                    ax.errorbar(exp_means[x_key],
+                                exp_means[y_key],
+                                yerr=exp_stds[y_key],
+                                label=label)
+                # Automatically make x ticks integers for certain X KEYS.
+                if y_key in ("CYCLE", "NUM_TRANSITIONS"):
+                    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+                ax.set_title(plot_title)
+                ax.set_xlabel(x_label)
+                ax.set_ylabel(y_label)
+                plt.legend()
+                plt.tight_layout()
+                filename = f"{plot_title}_{x_key}_{y_key}.png"
+                filename = filename.replace(" ", "_").lower()
+                outfile = os.path.join(outdir, filename)
+                plt.savefig(outfile)
+                print(f"Wrote out to {outfile}.")
 
 
 if __name__ == "__main__":
