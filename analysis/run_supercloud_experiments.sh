9322afc120624b181e05b81d46759b3c95edcc7f
Rohan Chitnis <ronuchit@mit.edu>
2022-02-07 15:37:50 -0500

    new supercloud script

--- analysis/run_supercloud_experiments.sh
+++ analysis/run_supercloud_experiments.sh
@@ -4,43 +4,40 @@ START_SEED=456
 NUM_SEEDS=10
 FILE="analysis/submit.py"
 ALL_NUM_TRAIN_TASKS=(
-    "50"
+    # "25"
+    # "50"
+    # "75"
     # "100"
+    # "125"
     # "150"
-    # "200"
+    # "175"
+    "200"
+)
+ALL_ENVS=(
+    "cover_regrasp"
+    "blocks"
+    "painting"
+    "tools"
 )
 
 for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
-    python $FILE --experiment_id cover_oracle --env cover --approach oracle --seed $SEED
-    python $FILE --experiment_id blocks_oracle --env blocks --approach oracle --seed $SEED
-    python $FILE --experiment_id painting_oracle --env painting --approach oracle --seed $SEED
-    python $FILE --experiment_id tools_oracle --env tools --approach oracle --seed $SEED
+    for ENV in ${ALL_ENVS[@]}; do
+        echo "$FILE --experiment_id $ENV_random --env $ENV --approach random_options --seed $SEED"
 
-    for NUM_TRAIN_TASKS in ${ALL_NUM_TRAIN_TASKS[@]}; do
-        COMMON_ARGS="--seed $SEED --num_train_tasks $NUM_TRAIN_TASKS"
+        for NUM_TRAIN_TASKS in ${ALL_NUM_TRAIN_TASKS[@]}; do
+            echo "$FILE --experiment_id ${ENV}_main_${NUM_TRAIN_TASKS}demo --env $ENV --approach grammar_search_invention --excluded_predicates all --seed $SEED --num_train_tasks $NUM_TRAIN_TASKS"
+            echo "$FILE --experiment_id ${ENV}_downref_${NUM_TRAIN_TASKS}demo --env $ENV --approach grammar_search_invention --sesame_max_skeletons_optimized 1 --offline_data_max_skeletons_optimized 8 --excluded_predicates all --seed $SEED --num_train_tasks $NUM_TRAIN_TASKS"
+            echo "$FILE --experiment_id ${ENV}_prederror_${NUM_TRAIN_TASKS}demo --env $ENV --approach grammar_search_invention --grammar_search_score_function prediction_error --excluded_predicates all --seed $SEED --num_train_tasks $NUM_TRAIN_TASKS"
+            echo "$FILE --experiment_id ${ENV}_branchfac_${NUM_TRAIN_TASKS}demo --env $ENV --approach grammar_search_invention --grammar_search_score_function branching_factor --excluded_predicates all --seed $SEED --num_train_tasks $NUM_TRAIN_TASKS"
+            echo "$FILE --experiment_id ${ENV}_energy_${NUM_TRAIN_TASKS}demo --env $ENV --approach grammar_search_invention --grammar_search_score_function lmcut_energy_lookaheaddepth0 --excluded_predicates all --seed $SEED --num_train_tasks $NUM_TRAIN_TASKS"
+            echo "$FILE --experiment_id ${ENV}_noinventallexclude_${NUM_TRAIN_TASKS}demo --env $ENV --approach nsrt_learning --excluded_predicates all --seed $SEED --num_train_tasks $NUM_TRAIN_TASKS"
+            echo "$FILE --experiment_id ${ENV}_noinventnoexclude_${NUM_TRAIN_TASKS}demo --env $ENV --approach nsrt_learning --seed $SEED --num_train_tasks $NUM_TRAIN_TASKS"
 
-        # cover
-        python $FILE --experiment_id cover_noinvent_noexclude_$NUM_TRAIN_TASKS --env cover --approach nsrt_learning $COMMON_ARGS
-        python $FILE --experiment_id cover_noinvent_allexclude_$NUM_TRAIN_TASKS --env cover --approach nsrt_learning --excluded_predicates all $COMMON_ARGS
-        python $FILE --experiment_id cover_invent_noexclude_$NUM_TRAIN_TASKS --env cover --approach grammar_search_invention $COMMON_ARGS
-        python $FILE --experiment_id cover_invent_allexclude_$NUM_TRAIN_TASKS --env cover --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS
-
-        # blocks
-        python $FILE --experiment_id blocks_noinvent_noexclude_$NUM_TRAIN_TASKS --env blocks --approach nsrt_learning $COMMON_ARGS
-        python $FILE --experiment_id blocks_noinvent_allexclude_$NUM_TRAIN_TASKS --env blocks --approach nsrt_learning --excluded_predicates all $COMMON_ARGS
-        python $FILE --experiment_id blocks_invent_noexclude_$NUM_TRAIN_TASKS --env blocks --approach grammar_search_invention $COMMON_ARGS
-        python $FILE --experiment_id blocks_invent_allexclude_$NUM_TRAIN_TASKS --env blocks --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS
-
-        # painting
-        python $FILE --experiment_id painting_noinvent_noexclude_$NUM_TRAIN_TASKS --env painting --approach nsrt_learning $COMMON_ARGS
-        python $FILE --experiment_id painting_noinvent_allexclude_$NUM_TRAIN_TASKS --env painting --approach nsrt_learning --excluded_predicates all $COMMON_ARGS
-        python $FILE --experiment_id painting_invent_noexclude_$NUM_TRAIN_TASKS --env painting --approach grammar_search_invention $COMMON_ARGS
-        python $FILE --experiment_id painting_invent_allexclude_$NUM_TRAIN_TASKS --env painting --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS
-
-        # tools
-        python $FILE --experiment_id tools_noinvent_noexclude_$NUM_TRAIN_TASKS --env tools --approach nsrt_learning $COMMON_ARGS
-        python $FILE --experiment_id tools_noinvent_allexclude_$NUM_TRAIN_TASKS --env tools --approach nsrt_learning --excluded_predicates all $COMMON_ARGS
-        python $FILE --experiment_id tools_invent_noexclude_$NUM_TRAIN_TASKS --env tools --approach grammar_search_invention $COMMON_ARGS
-        python $FILE --experiment_id tools_invent_allexclude_$NUM_TRAIN_TASKS --env tools --approach grammar_search_invention --excluded_predicates all $COMMON_ARGS
+            # Blocks-specific experiments.
+            if [ $ENV = "blocks" ]; then
+                echo "$FILE --experiment_id ${ENV}_mainhadd_${NUM_TRAIN_TASKS}demo --env $ENV --approach grammar_search_invention --sesame_task_planning_heuristic hadd --offline_data_task_planning_heuristic lmcut --excluded_predicates all --seed $SEED --num_train_tasks $NUM_TRAIN_TASKS"
+                echo "$FILE --experiment_id ${ENV}_noinventnoexcludehadd_${NUM_TRAIN_TASKS}demo --env $ENV --approach nsrt_learning --sesame_task_planning_heuristic hadd --offline_data_task_planning_heuristic lmcut --seed $SEED --num_train_tasks $NUM_TRAIN_TASKS"
+            fi
+        done
     done
 done
