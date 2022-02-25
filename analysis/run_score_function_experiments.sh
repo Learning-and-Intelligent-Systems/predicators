a93c4a29d5146cd5c5d7ec8f568fc3a71c2f77eb
Amber Li <amli@mit.edu>
2022-02-25 17:54:42 -0500

    adjust threshold

--- analysis/run_score_function_experiments.sh
+++ analysis/run_score_function_experiments.sh
@@ -9,7 +9,8 @@ for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
     # section kid
     python $FILE --env cover --approach interactive_learning --seed $SEED --excluded_predicates Covers --interactive_query_policy nonstrict_best_seen --interactive_score_function trivial --num_online_learning_cycles $CYCLES
     # ours
-    python $FILE --env cover --approach interactive_learning --seed $SEED --excluded_predicates Covers --interactive_query_policy threshold --interactive_score_threshold 0.5 --interactive_score_function entropy --num_online_learning_cycles $CYCLES
+    python $FILE --env cover --approach interactive_learning --seed $SEED --excluded_predicates Covers --interactive_query_policy threshold --interactive_score_threshold 0.3 --interactive_score_function entropy --num_online_learning_cycles $CYCLES --max_num_steps_interaction_request 3
+    python $FILE --env cover --approach interactive_learning --seed $SEED --excluded_predicates Covers --interactive_query_policy threshold --interactive_score_threshold 0.05 --interactive_score_function BALD --experiment_id BALD_$THRESH --num_online_learning_cycles $CYCLES --max_num_steps_interaction_request 3
     # silent kid
     python $FILE --env cover --approach interactive_learning --seed $SEED --excluded_predicates Covers --interactive_query_policy threshold --interactive_score_threshold 1.0 --interactive_score_function entropy --num_online_learning_cycles $CYCLES
 done
