072491e15085147eb8965ce112526f38e2b3aa0a
Amber Li <amli@mit.edu>
2022-02-25 17:54:20 -0500

    fix seed iterator, adjust thresholds

--- analysis/run_entropy_experiments.sh
+++ analysis/run_entropy_experiments.sh
@@ -2,16 +2,15 @@
 
 START_SEED=456
 NUM_SEEDS=5
-START_THRESH=0.1
+START_THRESH=0
 INCREMENT=0.1
-END_THRESH=0.9
+END_THRESH=0.6
 CYCLES=100
 FILE="analysis/submit.py"
 
 # to find an appropriate threshold
-for SEED in $(seq $START_THRESH $((NUM_SEEDS+START_THRESH-1))); do
+for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
     for THRESH in $(seq $START_THRESH $INCREMENT $END_THRESH); do
-        python $FILE --env cover --approach interactive_learning --seed $SEED --excluded_predicates Covers --interactive_query_policy threshold --interactive_score_threshold $THRESH --interactive_score_function entropy --num_online_learning_cycles $CYCLES
-        python $FILE --env cover --approach interactive_learning --seed $SEED --excluded_predicates Covers --interactive_query_policy threshold --interactive_score_threshold $THRESH --interactive_score_function BALD --num_online_learning_cycles $CYCLES
+        python $FILE --env cover --approach interactive_learning --seed $SEED --excluded_predicates Covers --interactive_query_policy threshold --interactive_score_threshold $THRESH --interactive_score_function entropy --experiment_id entropy_$THRESH --num_online_learning_cycles $CYCLES --max_num_steps_interaction_request 3
     done
 done
