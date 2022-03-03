05e0a3ed2cd6aee14fcfc5db818457b04c88be39
Tom Silver <tomsilver@users.noreply.github.com>
2022-03-03 17:13:04 -0500

    Revert "change create_env to get_cached_env_instance in ground truth nsrts (#507)"
    This reverts commit 7db46eae4120992af5282853cb793535f0aa4d56.

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -1,6 +1,5 @@
 """Default imports for envs folder."""
 
-import sys
 from predicators.src.envs.base_env import BaseEnv
 from predicators.src.envs.cover import CoverEnv, CoverEnvTypedOptions, \
     CoverEnvHierarchicalTypes, CoverMultistepOptions, \
@@ -13,7 +12,6 @@ from predicators.src.envs.painting import PaintingEnv
 from predicators.src.envs.tools import ToolsEnv
 from predicators.src.envs.playroom import PlayroomEnv
 from predicators.src.envs.repeated_nextto import RepeatedNextToEnv
-from predicators.src.settings import CFG
 
 __all__ = [
     "BaseEnv",
@@ -35,62 +33,51 @@ __all__ = [
 _MOST_RECENT_ENV_INSTANCE = {}
 
 
-def create_new_env(name: str, do_cache: bool = False) -> BaseEnv:
+def _create_new_env_instance(name: str) -> BaseEnv:
     """Create a new instance of an environment from its name.
 
-    If do_cache is True, then cache this env instance so that it can
-    later be loaded using get_cached_env().
+    Note that this env instance will not be cached.
     """
     if name == "cover":
-        env: BaseEnv = CoverEnv()
-    elif name == "cover_typed_options":
-        env = CoverEnvTypedOptions()
-    elif name == "cover_hierarchical_types":
-        env = CoverEnvHierarchicalTypes()
-    elif name == "cover_regrasp":
-        env = CoverEnvRegrasp()
-    elif name == "cover_multistep_options":
-        env = CoverMultistepOptions()
-    elif name == "cover_multistep_options_fixed_tasks":
-        env = CoverMultistepOptionsFixedTasks()
-    elif name == "cluttered_table":
-        env = ClutteredTableEnv()
-    elif name == "cluttered_table_place":
-        env = ClutteredTablePlaceEnv()
-    elif name == "blocks":
-        env = BlocksEnv()
-    elif name == "painting":
-        env = PaintingEnv()
-    elif name == "tools":
-        env = ToolsEnv()
-    elif name == "playroom":
-        env = PlayroomEnv()
-    elif name == "behavior":
-        env = BehaviorEnv()  # pragma: no cover
-    elif name == "repeated_nextto":
-        env = RepeatedNextToEnv()
-    else:
-        raise NotImplementedError(f"Unknown env: {name}")
-    if do_cache:
-        assert CFG.allow_env_caching, "CFG.allow_env_caching is off!"
-        _MOST_RECENT_ENV_INSTANCE[name] = env
+        return CoverEnv()
+    if name == "cover_typed_options":
+        return CoverEnvTypedOptions()
+    if name == "cover_hierarchical_types":
+        return CoverEnvHierarchicalTypes()
+    if name == "cover_regrasp":
+        return CoverEnvRegrasp()
+    if name == "cover_multistep_options":
+        return CoverMultistepOptions()
+    if name == "cover_multistep_options_fixed_tasks":
+        return CoverMultistepOptionsFixedTasks()
+    if name == "cluttered_table":
+        return ClutteredTableEnv()
+    if name == "cluttered_table_place":
+        return ClutteredTablePlaceEnv()
+    if name == "blocks":
+        return BlocksEnv()
+    if name == "painting":
+        return PaintingEnv()
+    if name == "tools":
+        return ToolsEnv()
+    if name == "playroom":
+        return PlayroomEnv()
+    if name == "behavior":
+        return BehaviorEnv()  # pragma: no cover
+    if name == "repeated_nextto":
+        return RepeatedNextToEnv()
+    raise NotImplementedError(f"Unknown env: {name}")
+
+
+def create_env(name: str) -> BaseEnv:
+    """Create an environment instance given its name and cache it."""
+    env = _create_new_env_instance(name)
+    _MOST_RECENT_ENV_INSTANCE[name] = env
     return env
 
 
-def get_cached_env(name: str) -> BaseEnv:
-    """Get the most recent cached env instance.
-
-    If you use this function, you should NOT be doing anything that relies on
-    the environment's internal state (i.e., you should not call reset() or
-    step()).
-
-    Note: if CFG.allow_env_caching is False, always makes a new env instance.
-    We do this because unit testing relies on this method working, e.g., when
-    a unit test calls get_gt_nsrts(), which in turn calls this method.
-    """
-    if not CFG.allow_env_caching:
-        return create_new_env(name)
-    assert name in _MOST_RECENT_ENV_INSTANCE, \
-        (f"CFG.allow_env_caching is on, but {name} is not in the cache. "
-         "If you're doing unit testing, you should turn this setting off.")
+def get_cached_env_instance(name: str) -> BaseEnv:
+    """Get the most recent cached env instance (env must have been previously
+    created with create_env() to exist in the cache)."""
+    assert name in _MOST_RECENT_ENV_INSTANCE
     return _MOST_RECENT_ENV_INSTANCE[name]
