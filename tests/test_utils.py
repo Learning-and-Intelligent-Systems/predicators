f0b372db9dee8535add985fc366357b64926f47b
Tom Silver <tomsilver@users.noreply.github.com>
2022-03-03 21:12:28 -0500

    linear chain parameterized options (#511)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -868,6 +868,86 @@ def test_find_substitution():
     assert assignment == {var3: plate0, var0: cup0}
 
 
+def test_LinearChainParameterizedOption():
+    """Tests for LinearChainParameterizedOption()."""
+    cup_type = Type("cup_type", ["feat1"])
+    plate_type = Type("plate_type", ["feat1", "feat2"])
+    cup = cup_type("cup")
+    plate = plate_type("plate")
+    state = State({cup: [0.0], plate: [1.0, 1.2]})
+
+    # This parameterized option takes the action [-4] twice and terminates.
+    def _initiable(_1, m, _3, _4):
+        m["num_steps"] = 0
+        return True
+
+    def _policy(_1, m, _3, _4):
+        m["num_steps"] += 1
+        return Action(np.array([-4]))
+
+    def _terminal(_1, m, _3, _4):
+        return m["num_steps"] >= 2
+
+    param_option0 = ParameterizedOption("dummy0", [cup_type],
+                                        Box(0.1, 1, (1, )), _policy,
+                                        _initiable, _terminal)
+
+    # This parameterized option takes the action [2] four times and terminates.
+    def _policy(_1, m, _3, _4):
+        m["num_steps"] += 1
+        return Action(np.array([2]))
+
+    def _terminal(_1, m, _3, _4):
+        return m["num_steps"] >= 4
+
+    param_option1 = ParameterizedOption("dummy1", [cup_type],
+                                        Box(0.1, 1, (1, )), _policy,
+                                        _initiable, _terminal)
+
+    children = [param_option0, param_option1]
+    chain_param_opt = utils.LinearChainParameterizedOption("chain", children)
+    assert chain_param_opt.types == [cup_type]
+    assert np.allclose(chain_param_opt.params_space.low, [0.1])
+    assert np.allclose(chain_param_opt.params_space.high, [1.0])
+    chain_option = chain_param_opt.ground([cup], [0.5])
+    assert chain_option.objects == [cup]
+
+    assert chain_option.initiable(state)
+    assert chain_option.policy(state).arr[0] == -4
+    assert not chain_option.terminal(state)
+    assert chain_option.policy(state).arr[0] == -4
+    assert not chain_option.terminal(state)
+    assert chain_option.policy(state).arr[0] == 2
+    assert not chain_option.terminal(state)
+    assert chain_option.policy(state).arr[0] == 2
+    assert not chain_option.terminal(state)
+    assert chain_option.policy(state).arr[0] == 2
+    assert not chain_option.terminal(state)
+    assert chain_option.policy(state).arr[0] == 2
+    assert chain_option.terminal(state)
+
+    # Cannot initialize with empty children.
+    with pytest.raises(AssertionError):
+        utils.LinearChainParameterizedOption("chain", [])
+
+    # Test that AssertionError is raised when options don't chain.
+    param_option2 = ParameterizedOption(
+        "dummy2", [cup_type], Box(0.1, 1, (1, )),
+        lambda _1, _2, _3, _4: Action(np.array([0])),
+        lambda _1, _2, _3, _4: False, lambda _1, _2, _3, _4: False)
+
+    children = [param_option0, param_option2]
+    chain_param_opt = utils.LinearChainParameterizedOption("chain2", children)
+    chain_option = chain_param_opt.ground([cup], [0.5])
+    assert chain_option.initiable(state)
+    assert chain_option.policy(state).arr[0] == -4
+    assert not chain_option.terminal(state)
+    assert chain_option.policy(state).arr[0] == -4
+    assert not chain_option.terminal(state)
+    with pytest.raises(AssertionError):
+        chain_option.policy(state)
+
+
 def test_nsrt_methods():
     """Tests for all_ground_nsrts(), extract_preds_and_types()."""
     cup_type = Type("cup_type", ["feat1"])
