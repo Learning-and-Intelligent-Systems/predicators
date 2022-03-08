cf04a6ac7cf0a74f4e54518b300b6ec36986fa49
ronuchit <ronuchit@mit.edu>
2022-03-08 12:02:58 -0500

    GNNPolicyApproach (#538)
    * GNNPolicyApproach
    
    Performs badly with our regular environment parameters, but does
    decently on some simplified settings:
    
    *Cover*
    Settings changes:
    ```
        cover_num_blocks = 1
        cover_num_targets = 1
        cover_block_widths = [0.1]
        cover_target_widths = [0.05]
    ```
    Command: `python src/main.py --env cover --approach gnn_policy --seed 0`
    Results: 38/50
    
    *Blocks*
    Settings changes:
    ```
        num_blocks_train = [3]
        num_blocks_test = [3]
    ```
    Command: `python src/main.py --env blocks --approach gnn_policy --seed
    0`
    Results: 47/50
    
    *Painting*
    Settings changes:
    ```
        painting_num_objs_train = [1]
        painting_num_objs_test = [1]
    ```
    Command: `python src/main.py --env painting --approach gnn_policy --seed
    0`
    Results: 10/50
    
    *Tools*
    Settings changes:
    ```
        tools_num_items_train = [1]
        tools_num_items_test = [1]
        tools_num_contraptions_train = [1]
        tools_num_contraptions_test = [1]
    ```
    Command: `python src/main.py --env tools --approach gnn_policy --seed 0`
    Results: 19/50
    
    * import errors
    
    * oops
    
    * update requirements
    
    * a light smattering of mypy (mostly type: ignore)
    
    * minor
    
    * feedback
    
    * add a 0 lol
    
    * couple changes to the code to make it easier to check the losses later
    
    * add tests (in total, under 1 second)
    
    * merge and checks
    
    * change test to not use blocks
    
    * lint

--- src/settings.py
+++ src/settings.py
@@ -96,8 +96,16 @@ class GlobalSettings:
     pybullet_max_ik_iters = 100
     pybullet_ik_tol = 1e-3
 
-    # parameters for approaches
+    # parameters for random options approach
     random_options_max_tries = 100
+
+    # parameters for GNN policy approach
+    gnn_policy_num_message_passing = 3
+    gnn_policy_learning_rate = 1e-3
+    gnn_policy_num_epochs = 25000
+    gnn_policy_batch_size = 128
+    gnn_policy_do_normalization = False  # performs worse in Cover when True
+    gnn_policy_use_validation_set = True
 
     # SeSamE parameters
     sesame_task_planning_heuristic = "lmcut"
