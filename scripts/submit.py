bf68ac61c8c3876fb5d81c22020429170bf0142d
Tom Silver <tomssilver@gmail.com>
2022-03-10 17:41:01 -0500

    updates to submit.py

--- scripts/submit.py
+++ scripts/submit.py
@@ -3,31 +3,31 @@
 import sys
 import subprocess
 import os
-import numpy as np
-
-RUN_INDEX = np.random.randint(int(1e15))
+from predicators.src import utils
+from predicators.src.settings import CFG
 
 
 def _run() -> None:
-    log_dir = "supercloud_logs"
-    os.makedirs(log_dir, exist_ok=True)
+    args = utils.parse_args()
+    utils.update_config(args)
+    job_name = f"{CFG.experiment_id}_{CFG.seed}"
     argsstr = " ".join(sys.argv[1:])
     mystr = f"#!/bin/bash\npython src/main.py {argsstr}"
-    with open(f"run_{RUN_INDEX}.sh", "w", encoding="utf-8") as f:
+    temp_run_file = "temp_run_file.sh"
+    assert not os.path.exists(temp_run_file)
+    with open(temp_run_file, "w", encoding="utf-8") as f:
         f.write(mystr)
     cmd = ("sbatch -p normal --time=99:00:00 --partition=xeon-p8 "
-           "--nodes=1 --exclusive --job-name=run.sh "
-           f"-o {log_dir}/%j_log.out run_{RUN_INDEX}.sh")
+           f"--nodes=1 --exclusive --job-name={job_name} "
+           f"-o /tmp/%j_log.out {temp_run_file}")
     print(f"Running command: {cmd}")
     output = subprocess.getoutput(cmd)
     if "command not found" in output:
-        os.remove(f"run_{RUN_INDEX}.sh")
+        os.remove(temp_run_file)
         raise Exception("Are you logged into supercloud?")
-    job_id = output.split()[-1]
-    print("Started job, see log with:\ntail -n 10000 "
-          f"-F {log_dir}/{job_id}_log.out")
-    os.remove(f"run_{RUN_INDEX}.sh")
-
+    os.remove(temp_run_file)
+    logfile = os.path.join(CFG.log_dir, f"{utils.get_config_path_str()}.log")
+    print(f"Started job, see log with:\ntail -n 10000 -F {logfile}")
 
 if __name__ == "__main__":
     _run()
