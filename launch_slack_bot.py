e6e44ae1e7d9334cbc6a8f515721390892b72cbc
Rohan Chitnis <ronuchit@mit.edu>
2022-03-14 11:11:31 -0400

    rename some files in scripts/

--- launch_slack_bot.py
+++ launch_slack_bot.py
@@ -14,7 +14,7 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 REPO_NAME = "Learning-and-Intelligent-Systems/predicators"
 ANALYSIS_CMD = "python scripts/analyze_results_directory.py"
-LAUNCH_CMD = "./scripts/run_supercloud_experiments.sh"
+LAUNCH_CMD = "./scripts/run_core_experiments.sh"
 MAX_CHARS_PER_MESSAGE = 3500  # actual limit is 4000, but we keep a buffer
 GITHUB_SEARCH_RESPONSE_MAX_FILE_MATCHES = 3
 SUPERCLOUD_LOGIN_SERVER = "login-2"  # can also use login-3 or login-4
