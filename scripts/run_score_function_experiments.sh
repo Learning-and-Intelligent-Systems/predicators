55d883084404d4bb817c2cd43ca4b0e75cda08db
Amber Li <amli@mit.edu>
2022-03-13 21:32:15 -0400

    change experiments ids

--- scripts/run_score_function_experiments.sh
+++ scripts/run_score_function_experiments.sh
@@ -8,15 +8,15 @@ FILE="scripts/submit.py"
 
 for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
     # section kid
-    python $FILE --env cover --approach interactive_learning --seed $SEED --excluded_predicates Covers,Holding --experiment_id excludeall_section_kid --interactive_query_policy nonstrict_best_seen --interactive_score_function trivial --num_online_learning_cycles $CYCLES --max_num_steps_interaction_request $MAX_STEPS --min_data_for_nsrt 10
+    python $FILE --env cover --approach interactive_learning --seed $SEED --excluded_predicates Covers,Holding --experiment_id excludeall_mindata_section_kid --interactive_query_policy nonstrict_best_seen --interactive_score_function trivial --num_online_learning_cycles $CYCLES --max_num_steps_interaction_request $MAX_STEPS --min_data_for_nsrt 10
     # ours, entropy
     for THRESH in $(seq 0.1 0.1 0.3); do
-        python $FILE --env cover --approach interactive_learning --seed $SEED --excluded_predicates Covers,Holding --experiment_id excludeall_entropy_$THRESH --interactive_query_policy threshold --interactive_score_threshold $THRESH --interactive_score_function entropy --num_online_learning_cycles $CYCLES --max_num_steps_interaction_request $MAX_STEPS --min_data_for_nsrt 10
+        python $FILE --env cover --approach interactive_learning --seed $SEED --excluded_predicates Covers,Holding --experiment_id excludeall_mindata_entropy_$THRESH --interactive_query_policy threshold --interactive_score_threshold $THRESH --interactive_score_function entropy --num_online_learning_cycles $CYCLES --max_num_steps_interaction_request $MAX_STEPS --min_data_for_nsrt 10
     done
     # ours, BALD
     for THRESH in $(seq 0.01 0.01 0.05); do
-        python $FILE --env cover --approach interactive_learning --seed $SEED --excluded_predicates Covers,Holding --experiment_id excludeall_BALD_$THRESH --interactive_query_policy threshold --interactive_score_threshold $THRESH --interactive_score_function BALD --num_online_learning_cycles $CYCLES --max_num_steps_interaction_request $MAX_STEPS --min_data_for_nsrt 10
+        python $FILE --env cover --approach interactive_learning --seed $SEED --excluded_predicates Covers,Holding --experiment_id excludeall_mindata_BALD_$THRESH --interactive_query_policy threshold --interactive_score_threshold $THRESH --interactive_score_function BALD --num_online_learning_cycles $CYCLES --max_num_steps_interaction_request $MAX_STEPS --min_data_for_nsrt 10
     done
     # silent kid
-    python $FILE --env cover --approach interactive_learning --seed $SEED --excluded_predicates Covers,Holding --experiment_id excludeall_silent_kid --interactive_query_policy threshold --interactive_score_threshold 1.0 --interactive_score_function entropy --num_online_learning_cycles $CYCLES --max_num_steps_interaction_request $MAX_STEPS --min_data_for_nsrt 10
+    python $FILE --env cover --approach interactive_learning --seed $SEED --excluded_predicates Covers,Holding --experiment_id excludeall_mindata_silent_kid --interactive_query_policy threshold --interactive_score_threshold 1.0 --interactive_score_function entropy --num_online_learning_cycles $CYCLES --max_num_steps_interaction_request $MAX_STEPS --min_data_for_nsrt 10
 done
