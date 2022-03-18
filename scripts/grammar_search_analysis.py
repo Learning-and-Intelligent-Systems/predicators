634a5973274b5c68062c268cb01fe1bdcec7307f
Tom Silver <tomssilver@gmail.com>
2022-03-18 10:59:50 -0400

    lint

--- scripts/grammar_search_analysis.py
+++ scripts/grammar_search_analysis.py
@@ -20,7 +20,6 @@ from predicators.src.ground_truth_nsrts import _get_predicates_by_names
 from predicators.src.main import _run_testing
 from predicators.src.predicate_search_score_functions import \
     create_score_function
-from predicators.src.settings import CFG
 from predicators.src.structs import Dataset, Object, Predicate, State, Task
 
 DEFAULT_ENV_NAMES = [
