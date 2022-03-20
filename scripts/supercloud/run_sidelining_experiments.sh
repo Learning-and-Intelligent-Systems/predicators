ca59e1960faff7abc5f8cd2ae71b6f3e1e7fa99c
ronuchit <ronuchit@mit.edu>
2022-03-20 12:47:16 -0400

    update nightly to move out sidelining expts (#603)
    * update nightly to move out sidelining expts
    
    * empty for pytest

---
+++ scripts/supercloud/run_sidelining_experiments.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+START_SEED=456
+NUM_SEEDS=10
+FILE="scripts/supercloud/submit_supercloud_job.py"
+
+for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
+    # repeated_nextto
+    python $FILE --experiment_id rnt_oracle --env repeated_nextto --approach oracle --seed $SEED --num_train_tasks 0
+    python $FILE --experiment_id rnt_no_sidepreds --env repeated_nextto --approach nsrt_learning --side_predicate_learner no_learning --seed $SEED --num_train_tasks 50
+    python $FILE --experiment_id rnt_pred_error --env repeated_nextto --approach nsrt_learning --side_predicate_learner prediction_error_hill_climbing --seed $SEED --num_train_tasks 50
+
+    # repeated_nextto_painting
+    python $FILE --experiment_id rnt_painting_oracle --env repeated_nextto_painting --approach oracle --seed $SEED --num_train_tasks 0
+done
