9811239fc5d2f80b6a0e0becab0dd3660c69570a
Tom Silver <tomssilver@gmail.com>
2022-03-25 11:59:23 -0400

    more tests

--- src/envs/pddl_procedural_generation.py
+++ src/envs/pddl_procedural_generation.py
@@ -11,15 +11,21 @@ from predicators.src.structs import PDDLProblemGenerator
 def create_blocks_pddl_generator(min_num_blocks: int, max_num_blocks: int,
                                  min_num_blocks_goal: int,
                                  max_num_blocks_goal: int,
-                                 new_pile_prob: float) -> PDDLProblemGenerator:
+                                 new_pile_prob: float,
+                                 force_goal_not_achieved: bool = True) -> PDDLProblemGenerator:
     """Create a generator for blocks problems."""
+    if force_goal_not_achieved:
+        assert new_pile_prob < 1.0, ("Impossible to create an unsolved problem"
+                                     " with new_pile_prob = 1.0.")
     return partial(_generate_blocks_problems, min_num_blocks, max_num_blocks,
-                   min_num_blocks_goal, max_num_blocks_goal, new_pile_prob)
+                   min_num_blocks_goal, max_num_blocks_goal, new_pile_prob,
+                   force_goal_not_achieved)
 
 
 def _generate_blocks_problems(min_num_blocks: int, max_num_blocks: int,
                               min_num_blocks_goal: int,
                               max_num_blocks_goal: int, new_pile_prob: float,
+                              force_goal_not_achieved: bool,
                               num_problems: int,
                               rng: np.random.Generator) -> List[str]:
     assert max_num_blocks_goal <= min_num_blocks
@@ -29,13 +35,15 @@ def _generate_blocks_problems(min_num_blocks: int, max_num_blocks: int,
         num_goal_blocks = rng.integers(min_num_blocks_goal,
                                        max_num_blocks_goal + 1)
         problem = _generate_blocks_problem(num_blocks, num_goal_blocks,
-                                           new_pile_prob, rng)
+                                           new_pile_prob, force_goal_not_achieved,
+                                           rng)
         problems.append(problem)
     return problems
 
 
 def _generate_blocks_problem(num_blocks: int, num_goal_blocks: int,
                              new_pile_prob: float,
+                             force_goal_not_achieved: bool,
                              rng: np.random.Generator) -> str:
     # Repeat until the goal does not hold in the initial state.
     while True:
@@ -60,7 +68,7 @@ def _generate_blocks_problem(num_blocks: int, num_goal_blocks: int,
         init_strs = _blocks_piles_to_strs(piles)
         goal_strs = _blocks_piles_to_strs(
             goal_piles, excluded_predicates={"clear", "handempty"})
-        if not goal_strs.issubset(init_strs):
+        if not force_goal_not_achieved or not goal_strs.issubset(init_strs):
             break
     # Finalize PDDL problem str.
     blocks_str = " ".join(blocks)
--- tests/envs/test_pddl_procedural_generation.py
+++ tests/envs/test_pddl_procedural_generation.py
@@ -1,1 +1,73 @@
 """Tests for PDDL procedural generation."""
+
+import numpy as np
+
+from predicators.src.envs.pddl_procedural_generation import \
+    create_blocks_pddl_generator
+
+
+def _split_pddl_problem_str(problem_str):
+    """A hacky helper function for splitting up a PDDL problem string.
+
+    Not reliable, definitely don't use anywhere outside of this test
+    file.
+    """
+
+    def _parse_helper(start_marker, end_marker=None):
+        str_start = problem_str.index(start_marker)
+        start = str_start + len(start_marker) + 1
+        if end_marker is None:
+            end = len(problem_str)
+        else:
+            str_end = problem_str.index(end_marker)
+            end = str_end - len(end_marker) - 1
+        return problem_str[start:end + 1]
+
+    object_str = _parse_helper("(:objects", "(:init")
+    init_str = _parse_helper("(:init", "(:goal")
+    goal_str = _parse_helper("(:goal")
+
+    return object_str, init_str, goal_str
+
+
+def test_create_blocks_pddl_generator():
+    """Tests for create_blocks_pddl_generator()."""
+
+    # Test with new_pile_prob = 0.0, meaning that all problems should have one
+    # pile in the initial state and goal.
+    gen = create_blocks_pddl_generator(min_num_blocks=3,
+                                       max_num_blocks=3,
+                                       min_num_blocks_goal=2,
+                                       max_num_blocks_goal=2,
+                                       new_pile_prob=0.0)
+    rng = np.random.default_rng(123)
+    problem_strs = gen(10, rng)
+    for problem_str in problem_strs:
+        obj_str, init_str, goal_str = _split_pddl_problem_str(problem_str)
+        # There should be exactly 3 blocks.
+        for i in range(3):
+            assert f"b{i}" in obj_str
+        assert "b4" not in obj_str
+        assert " - block" in obj_str
+        # One ontable in init and one in goal.
+        assert init_str.count("ontable") == 1
+        assert goal_str.count("ontable") == 1
+        # The goal should have exactly two objects, so one on.
+        assert goal_str.count("on ") == 1
+
+    # Test with new_pile_prob = 1.0, meaning that all problems should have no
+    # "on" predicates in the initial state or goal.
+    gen = create_blocks_pddl_generator(min_num_blocks=3,
+                                       max_num_blocks=3,
+                                       min_num_blocks_goal=2,
+                                       max_num_blocks_goal=2,
+                                       new_pile_prob=1.0,
+                                       force_goal_not_achieved=False)
+    rng = np.random.default_rng(123)
+    problem_strs = gen(10, rng)
+    for problem_str in problem_strs:
+        _, init_str, goal_str = _split_pddl_problem_str(problem_str)
+        assert init_str.count("ontable") == 3
+        assert goal_str.count("ontable") == 2
+        assert init_str.count("on ") == 0
+        assert goal_str.count("on ") == 0
