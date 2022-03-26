04f292dc91ef66a128acc7e905370e798f44f014
Amber Li <amli@mit.edu>
2022-03-26 14:39:52 -0400

    initial commit

---
+++ scripts/evaluate_predicate_classifiers.py
@@ -0,0 +1,148 @@
+"""
+TODO
+"""
+
+import os
+
+import numpy as np
+from typing import List, Optional, Set
+
+from predicators.src import utils
+from predicators.src.approaches import create_approach
+from predicators.src.approaches.bilevel_planning_approach import BilevelPlanningApproach
+from predicators.src.envs import BaseEnv, create_new_env
+from predicators.src.envs.cover import CoverEnv
+from predicators.src.main import _generate_or_load_offline_dataset, \
+    _generate_interaction_results
+from predicators.src.settings import CFG
+from predicators.src.structs import Dataset, Predicate, Task, Action
+from predicators.src.teacher import Teacher
+
+
+def _main() -> None:
+    # Parse & validate args
+    args = utils.parse_args()
+    utils.update_config(args)
+    # Create results directory.
+    outdir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                          "results")
+    os.makedirs(outdir, exist_ok=True)
+    # Create classes. Note that seeding happens inside the env and approach.
+    env = create_new_env(CFG.env, do_cache=True)
+    # The action space and options need to be seeded externally, because
+    # env.action_space and env.options are often created during env __init__().
+    env.action_space.seed(CFG.seed)
+    for option in env.options:
+        option.params_space.seed(CFG.seed)
+    assert env.goal_predicates.issubset(env.predicates)
+    preds, _ = utils.parse_config_excluded_predicates(env)
+    # Create the train tasks.
+    train_tasks = env.get_train_tasks()
+    # If train tasks have goals that involve excluded predicates, strip those
+    # predicate classifiers to prevent leaking information to the approaches.
+    stripped_train_tasks = [
+        utils.strip_task(task, preds) for task in train_tasks
+    ]
+    # Create the agent (approach).
+    approach = create_approach(CFG.approach, preds, env.options, env.types,
+                               env.action_space, stripped_train_tasks)
+    assert approach.is_learning_based
+    assert isinstance(approach, BilevelPlanningApproach)
+    offline_dataset = _generate_or_load_offline_dataset(env, train_tasks)
+    _run_pipeline(env, approach, stripped_train_tasks, offline_dataset)
+
+
+def _run_pipeline(env: BaseEnv,
+                  approach: BilevelPlanningApproach,
+                  train_tasks: List[Task],
+                  offline_dataset: Optional[Dataset] = None) -> None:
+    assert offline_dataset is not None, "Missing offline dataset"
+    total_num_transitions = sum(
+        len(traj.actions) for traj in offline_dataset.trajectories)
+    approach.load(online_learning_cycle=None)
+    teacher = Teacher(train_tasks)
+    for i in range(CFG.num_online_learning_cycles):
+        if total_num_transitions > CFG.online_learning_max_transitions:
+            break
+        interaction_requests = approach.get_interaction_requests()
+        if not interaction_requests:
+            break  # agent doesn't want to learn anything more; terminate
+        interaction_results, _ = _generate_interaction_results(
+            env, teacher, interaction_requests, i)
+        total_num_transitions += sum(
+            len(result.actions) for result in interaction_results)
+        _evaluate_preds(approach._get_current_predicates(), env)
+        approach.load(online_learning_cycle=i)
+
+
+def _evaluate_preds(preds: Set[Predicate], env: BaseEnv) -> None:
+    if CFG.env == "cover":
+        assert type(env) == CoverEnv
+        return _evaluate_preds_cover(preds, env)
+    raise NotImplementedError(f"Held out test set not yet implemented for {CFG.env}")
+
+
+def _evaluate_preds_cover(preds: Set[Predicate], env: CoverEnv) -> None:
+    Holding = [p for p in preds if p.name == "Holding"][0]
+    Covers = [p for p in preds if p.name == "Covers"][0]
+    # TODO: track true/false positives/negatives
+    # Create initial state
+    task = env.get_test_tasks()[0]
+    state = task.init
+    block0 = [b for b in state if b.name == "block0"][0]
+    block1 = [b for b in state if b.name == "block1"][0]
+    target0 = [b for b in state if b.name == "target0"][0]
+    target1 = [b for b in state if b.name == "target1"][0]
+    robot = [b for b in state if b.name == "robby"][0]
+    blocks = [block0, block1]
+    targets = [target0, target1]
+    block_poses = [0.15, 0.605]
+    target_poses = [0.375, 0.815]
+    for block, pose in zip(blocks, block_poses):
+        # [is_block, is_target, width, pose, grasp]
+        state.set(block, "pose", pose)
+    for target, pose in zip(targets, target_poses):
+        # [is_block, is_target, width, pose]
+        state.set(target, "pose", pose)
+    # [hand]
+    state.set(robot, "hand", 0.0)
+    # Test 1: block does not overlap target, not held
+    if Holding.holds(state, [block0]):
+        print("Incorrectly evaluated Holding in state where block0 is not held")
+    if Holding.holds(state, [block1]):
+        print("Incorrectly evaluated Holding in state where block1 is not held")
+    if Covers.holds(state, [block0, target0]):
+        print("Incorrectly evaluated Covers in state where block0 does not cover target0")
+    # TODO: other combos?
+    # Pick up block0 and cover target0
+    action = Action(np.array([0.15], dtype=np.float32))
+    state = env.simulate(state, action)
+    action = Action(np.array([0.375], dtype=np.float32))
+    state = env.simulate(state, action)
+    # Test 2: block0 covers target0, is not held
+    if Holding.holds(state, [block0]):
+        print("Incorrectly evaluated Holding in state where block0 is not held")
+    if not Covers.holds(state, [block0, target0]):
+        print("Incorrectly evaluated Covers in state where block0 covers target0")
+    # Pick up block1 and attempt to place over target0, but block0 is already there
+    action = Action(np.array([0.6], dtype=np.float32))
+    state = env.simulate(state, action)
+    action = Action(np.array([0.37], dtype=np.float32))
+    state = env.simulate(state, action)
+    # Test 3: block1 is held above target0
+    if not Holding.holds(state, [block1]):
+        print("Incorrectly evaluated Holding in state where block1 is held")
+    if Covers.holds(state, [block1, target0]):
+        print("Incorrectly evaluated Covers in state where block1 does not cover target0")
+    # Place block1 so it partially overlaps but does not cover target1
+    action = Action(np.array([0.78], dtype=np.float32))
+    state = env.simulate(state, action)
+    # Test 4: block1 partially overlaps target1
+    if not Holding.holds(state, [block1]):
+        print("Incorrectly evaluated Holding in state where block1 is held")
+    if Covers.holds(state, [block1, target1]):
+        print("Incorrectly evaluated Covers in state where block1 does not cover target1")
+
+
+if __name__ == "__main__":
+    _main()
