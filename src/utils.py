5e8ce2650cef5a8ed437a9dcf3ab63208bb153c8
ronuchit <ronuchit@mit.edu>
2022-03-29 21:35:14 -0400

    add a utility (#638)

--- src/utils.py
+++ src/utils.py
@@ -130,6 +130,23 @@ def entropy(p: float) -> float:
     return -(p * np.log(p) + (1 - p) * np.log(1 - p))
 
 
+def create_state_from_dict(data: Dict[Object, Dict[str, float]],
+                           simulator_state: Optional[Any] = None) -> State:
+    """Small utility to generate a state from a dictionary `data` of individual
+    feature values for each object.
+
+    A simulator_state for the outputted State may optionally be
+    provided.
+    """
+    state_dict = {}
+    for obj, obj_data in data.items():
+        obj_vec = []
+        for feat in obj.type.feature_names:
+            obj_vec.append(obj_data[feat])
+        state_dict[obj] = np.array(obj_vec)
+    return State(state_dict, simulator_state)
+
+
 def intersects(p1: Tuple[float, float], p2: Tuple[float, float],
                p3: Tuple[float, float], p4: Tuple[float, float]) -> bool:
     """Checks if line segment p1p2 and p3p4 intersect.
