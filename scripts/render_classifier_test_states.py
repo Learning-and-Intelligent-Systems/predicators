604cbf6399c457062f2abb12c70c4deb4ad40995
Amber Li <amli@mit.edu>
2022-03-31 19:55:33 -0400

    need to parse args

--- scripts/render_classifier_test_states.py
+++ scripts/render_classifier_test_states.py
@@ -10,6 +10,8 @@ from predicators.src.envs.cover import CoverEnv
 from predicators.src.settings import CFG
 
 if __name__ == "__main__":
+    args = utils.parse_args()
+    utils.update_config(args)
     env = create_new_env(CFG.env, do_cache=True)
     task = env.get_test_tasks()[0]
     if CFG.env == "cover":
