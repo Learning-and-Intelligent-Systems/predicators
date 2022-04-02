1ea9ada34d7e80943bcfd3269fe4b42518ed589c
Tom Silver <tomssilver@gmail.com>
2022-04-02 14:07:52 -0400

    prepare for another supercloud run but with fewer classifier its

--- scripts/supercloud/run_score_function_experiments.sh
+++ scripts/supercloud/run_score_function_experiments.sh
@@ -7,12 +7,11 @@ CYCLES=100  # way too many cycles
 REQUESTS=10
 MAX_STEPS=3
 MIN_DATA=10
-MAX_ITR=10000
 FILE="scripts/supercloud/submit_supercloud_job.py"
 
 for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
 
-    COMMON_ARGS="--env cover --approach interactive_learning --seed $SEED --excluded_predicates Covers,Holding --online_learning_max_transitions $MAX_TRANSITIONS --num_online_learning_cycles $CYCLES --interactive_num_requests_per_cycle $REQUESTS --max_num_steps_interaction_request $MAX_STEPS --min_data_for_nsrt $MIN_DATA --sampler_disable_classifier True --mlp_classifier_balance_data False --predicate_mlp_classifier_max_itr $MAX_ITR"
+    COMMON_ARGS="--env cover --approach interactive_learning --seed $SEED --excluded_predicates Covers,Holding --online_learning_max_transitions $MAX_TRANSITIONS --num_online_learning_cycles $CYCLES --interactive_num_requests_per_cycle $REQUESTS --max_num_steps_interaction_request $MAX_STEPS --min_data_for_nsrt $MIN_DATA --sampler_disable_classifier True --mlp_classifier_balance_data False"
 
     # section kid
     python $FILE $COMMON_ARGS --experiment_id excludeall_section_kid --interactive_query_policy nonstrict_best_seen --interactive_score_function trivial
