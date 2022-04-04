bd27c5b75574593b9da7284485fc7c0557f8ef92
Nishanth Kumar <NishanthJKumar@users.noreply.github.com>
2022-04-04 18:52:25 -0400

    Non 1 to 1 sidelining (#651)
    * includes breakpoint showing exactly where interesting problem happens!
    
    * working 502 and 123 seeds!
    
    * yapf
    
    * new supercloud experiments!
    
    * fix painting ish
    
    * fixes openlid issue
    
    * removes unnecessary debugging code
    
    * removes length check and extra call to recompute_datastores
    
    * minor fixes to side predicate learning code
    
    * minor cleanup
    
    * removes specifizing calls
    
    * synthesizes methods in side_predicate_learning together
    
    * cleanup + ci things
    
    * gets everything to work except test passing
    
    * yapf
    
    * isort
    
    * minor QoL change requested by @ronuchit
    
    * autoformat fix
    
    * second round of fixes requested by @ronuchit
    
    * fixes minor comment issue
    
    * removes unnecessary isinstance check
    
    * adds necessary test
    
    * removes change that sets init side predicates to null
    
    * minor fixes + new supercloud exprs
    
    * fixes requested by @tomsilver
    
    * fixes unfortunate comment problem
    
    * deletes unnecessary commented-out code
    
    * fixes pylint issue
    
    * reverts sidelining change
    
    * ypaf fix
    
    Co-authored-by: Rohan Chitnis <ronuchit@mit.edu>

--- tests/nsrt_learning/test_side_predicate_learning.py
+++ tests/nsrt_learning/test_side_predicate_learning.py
@@ -22,20 +22,22 @@ class MockBackchainingSPL(BackchainingSidePredicateLearner):
                                            segment)
 
     @staticmethod
-    def get_partially_satisfying_grounding(necessary_add_effects, pnad,
-                                           segment):
+    def find_unification(necessary_add_effects,
+                         pnad,
+                         segment,
+                         find_partial_grounding=True):
         """Exposed for testing."""
-        return (BackchainingSidePredicateLearner.
-                _get_partially_satisfying_grounding(necessary_add_effects,
-                                                    pnad, segment))
+        return (BackchainingSidePredicateLearner._find_unification(
+            necessary_add_effects, pnad, segment, find_partial_grounding))
 
 
 def test_backchaining():
     """Test the BackchainingSidePredicateLearner."""
 
     # Set up the PNADs.
-    human_type = Type("human_type", ["feat"])
+    human_type = Type("human_type", ["feat1", "feat2"])
     Asleep = Predicate("Asleep", [human_type], lambda s, o: s[o[0]][0] > 0.5)
+    Sad = Predicate("Sad", [human_type], lambda s, o: s[o[0]][1] < 0.5)
     initial_pnads = set()
     opt_name_to_opt = {}
     for opt_name in ["Cry", "Eat"]:
@@ -43,41 +45,47 @@ def test_backchaining():
         opt_name_to_opt[opt_name] = opt
         human_var = human_type("?human")
         params = [human_var]
-        add_effects = {Asleep([human_var])}
+        add_effects = {Asleep([human_var]), Sad([human_var])}
         op = STRIPSOperator(f"{opt_name}Op", params, set(), add_effects, set(),
                             set())
         initial_pnads.add(PartialNSRTAndDatastore(op, [], (opt, [])))
     # Set up the data.
     bob = human_type("bob")
-    state_awake = State({bob: [0.0]})
-    state_asleep = State({bob: [1.0]})
-    assert not Asleep.holds(state_awake, [bob])
-    assert Asleep.holds(state_asleep, [bob])
+    state_awake_and_sad = State({bob: [0.0, 0.0]})
+    state_awake_and_happy = State({bob: [0.0, 1.0]})
+    state_asleep_and_sad = State({bob: [1.0, 0.0]})
+    assert not Asleep.holds(state_awake_and_sad, [bob])
+    assert Sad.holds(state_awake_and_sad, [bob])
+    assert not Asleep.holds(state_awake_and_happy, [bob])
+    assert not Sad.holds(state_awake_and_happy, [bob])
+    assert Asleep.holds(state_asleep_and_sad, [bob])
+    assert Sad.holds(state_asleep_and_sad, [bob])
     Cry = opt_name_to_opt["Cry"].ground([], [])
     Eat = opt_name_to_opt["Eat"].ground([], [])
     goal1 = {Asleep([bob])}
     act1 = Action([], Cry)
-    traj1 = LowLevelTrajectory([state_awake, state_asleep], [act1], True, 0)
-    task1 = Task(state_awake, goal1)
+    traj1 = LowLevelTrajectory([state_awake_and_sad, state_asleep_and_sad],
+                               [act1], True, 0)
+    task1 = Task(state_awake_and_sad, goal1)
     segment1 = Segment(traj1, set(), goal1, Cry)
     goal2 = set()
     act2 = Action([], Eat)
-    traj2 = LowLevelTrajectory([state_awake, state_awake], [act2], True, 1)
-    task2 = Task(state_awake, set())
+    traj2 = LowLevelTrajectory([state_awake_and_sad, state_awake_and_sad],
+                               [act2], True, 1)
+    task2 = Task(state_awake_and_sad, set())
     segment2 = Segment(traj2, set(), goal2, Eat)
-    # Create and run the sidelining approach.
     spl = MockBackchainingSPL(initial_pnads, [traj1, traj2], [task1, task2],
                               {Asleep}, [[segment1], [segment2]])
     pnads = spl.sideline()
     # Verify the results are as expected.
     expected_strs = [
-        """STRIPS-Cry:
+        """STRIPS-Cry0:
     Parameters: [?x0:human_type]
     Preconditions: []
     Add Effects: [Asleep(?x0:human_type)]
     Delete Effects: []
     Side Predicates: []
-    Option Spec: Cry()""", """STRIPS-Eat:
+    Option Spec: Cry()""", """STRIPS-Eat0:
     Parameters: []
     Preconditions: []
     Add Effects: []
@@ -89,10 +97,38 @@ def test_backchaining():
                              expected_strs):
         assert str(pnad) == repr(pnad) == exp_str
 
+    # Test sidelining where an existing operator needs to
+    # be specialized.
+    goal3 = {Asleep([bob]), Sad([bob])}
+    act3 = Action([], Cry)
+    traj3 = LowLevelTrajectory([state_awake_and_sad, state_asleep_and_sad],
+                               [act3], True, 0)
+    traj4 = LowLevelTrajectory([state_awake_and_happy, state_asleep_and_sad],
+                               [act3], True, 0)
+    task3 = Task(state_awake_and_sad, goal3)
+    segment3 = Segment(traj3, set([Sad([bob])]), goal3, Cry)
+    segment4 = Segment(traj4, set(), goal3, Cry)
+    cry_pnads = [pnad for pnad in initial_pnads if pnad.op.name == "CryOp"]
+    assert len(cry_pnads) == 1
+    cry_pnad = cry_pnads[0]
+    # Create and run the sidelining approach.
+    spl = MockBackchainingSPL(set([cry_pnad]), [traj3, traj4], [task3],
+                              {Asleep, Sad}, [[segment3], [segment4]])
+    pnads = spl.sideline()
+    assert len(pnads) == 1
+    expected_str = """STRIPS-Cry0:
+    Parameters: [?x0:human_type]
+    Preconditions: []
+    Add Effects: [Asleep(?x0:human_type), Sad(?x0:human_type)]
+    Delete Effects: []
+    Side Predicates: []
+    Option Spec: Cry()"""
+    assert str(pnads[0]) == repr(pnads[0]) == expected_str
 
-def test_get_partially_satisfying_grounding_and_try_refining_pnad():
-    """Test the _get_partially_satisfying_grounding() and try_refining_pnad()
-    methods in the BackchainingSidePredicateLearner."""
+
+def test_find_unification_and_try_specializing_pnad():
+    """Test the find_unification() and try_specializing_pnad() methods in the
+    BackchainingSidePredicateLearner."""
 
     human_type = Type("human_type", ["feat"])
     Asleep = Predicate("Asleep", [human_type], lambda s, o: s[o[0]][0] > 0.5)
@@ -114,7 +150,7 @@ def test_get_partially_satisfying_grounding_and_try_refining_pnad():
                               [[segment]])
     # Normal usage: the PNAD add effects can capture a subset of
     # the necessary_add_effects.
-    ground_op = spl.get_partially_satisfying_grounding(
+    ground_op = spl.find_unification(
         {Asleep([bob]), Happy([bob])}, pnad, Segment(traj, set(), set(), Move))
     assert ground_op is not None
     assert str(ground_op) == repr(ground_op) == """GroundSTRIPS-MoveOp:
@@ -125,14 +161,17 @@ def test_get_partially_satisfying_grounding_and_try_refining_pnad():
     Side Predicates: []"""
     # The necessary_add_effects is empty, but the PNAD has an add effect,
     # so no grounding is possible.
-    ground_op = spl.get_partially_satisfying_grounding(
-        set(), pnad, Segment(traj, set(), set(), Move))
+    ground_op = spl.find_unification(set(), pnad,
+                                     Segment(traj, set(), set(), Move))
+    assert ground_op is None
+    ground_op = spl.find_unification(set(), pnad,
+                                     Segment(traj, set(), set(), Move), False)
     assert ground_op is None
     # Change the PNAD to have non-trivial preconditions.
     pnad.op = pnad.op.copy_with(preconditions={Happy([human_var])})
     # The new preconditions are not satisfiable in the segment's init_atoms,
     # so no grounding is possible.
-    ground_op = spl.get_partially_satisfying_grounding(
+    ground_op = spl.find_unification(
         set(), pnad, Segment(traj, {Asleep([bob])}, set(), Move))
     assert ground_op is None
     new_pnad = spl.try_specializing_pnad(
@@ -140,8 +179,9 @@ def test_get_partially_satisfying_grounding_and_try_refining_pnad():
     assert new_pnad is None
     # Make the preconditions be satisfiable in the segment's init_atoms.
     # Now, we are back to normal usage.
-    ground_op = spl.get_partially_satisfying_grounding(
-        {Asleep([bob])}, pnad, Segment(traj, {Happy([bob])}, set(), Move))
+    ground_op = spl.find_unification({Asleep([bob])}, pnad,
+                                     Segment(traj, {Happy([bob])}, set(),
+                                             Move))
     assert ground_op is not None
     assert str(ground_op) == repr(ground_op) == """GroundSTRIPS-MoveOp:
     Parameters: [bob:human_type]
