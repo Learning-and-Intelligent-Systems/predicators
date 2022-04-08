ff4cf44b58217f5b0d32644b779e5cbe64b8abbf
Tom Silver <tomssilver@gmail.com>
2022-04-07 20:42:51 -0400

    add single block experiment

--- scripts/supercloud/run_option_learning_experiments.sh
+++ scripts/supercloud/run_option_learning_experiments.sh
@@ -38,5 +38,14 @@ for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
     # implicit BC: grid
     python $FILE $COMMON_ARGS --experiment_id cover_holding_multi_implicit_grid --env cover_multistep_options_holding --option_learner implicit_bc --implicit_mlp_regressor_inference_method grid
 
+    ## cover_multistep_options single block
+    # direct BC
+    python $FILE $COMMON_ARGS --experiment_id cover_single_multi_direct --env cover_multistep_options --cover_num_targets 1 --num_test_tasks 1 --option_learner direct_bc
+
+    # implicit BC: derivative_free
+    python $FILE $COMMON_ARGS --experiment_id cover_single_multi_implicit_df --env cover_multistep_options --cover_num_targets 1 --num_test_tasks 1 --option_learner implicit_bc --implicit_mlp_regressor_inference_method derivative_free
+
+    # implicit BC: grid
+    python $FILE $COMMON_ARGS --experiment_id cover_single_multi_implicit_grid --env cover_multistep_options --cover_num_targets 1 --num_test_tasks 1 --option_learner implicit_bc --implicit_mlp_regressor_inference_method grid
 
 done
