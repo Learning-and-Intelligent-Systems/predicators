4596aaccb4d4eaa51cfcc050aba22abd4390c97a
Tom Silver <tomssilver@gmail.com>
2022-04-10 11:21:18 -0400

    panda is not going where it is told

--- src/envs/pybullet_robots.py
+++ src/envs/pybullet_robots.py
@@ -250,8 +250,7 @@ class PandaPyBulletRobot(_SingleArmPyBulletRobot):
     def _initialize(self) -> None:
         self._panda_id = p.loadURDF(
             utils.get_env_asset_path(
-                "urdf/franka_description/robots/panda_arm_hand.urdf"
-            ),
+                "urdf/franka_description/robots/panda_arm_hand.urdf"),
             basePosition=self._base_pose,
             baseOrientation=self._base_orientation,
             useFixedBase=True,
@@ -272,14 +271,13 @@ class PandaPyBulletRobot(_SingleArmPyBulletRobot):
 
         self._ee_id = joint_names.index("tool_joint")
         self._arm_joints = get_kinematic_chain(
-            self._panda_id, self._ee_id, physics_client_id=self._physics_client_id
-        )
+            self._panda_id,
+            self._ee_id,
+            physics_client_id=self._physics_client_id)
         self._left_finger_id = joint_names.index("panda_finger_joint1")
         self._right_finger_id = joint_names.index("panda_finger_joint2")
         self._arm_joints.append(self._left_finger_id)
         self._arm_joints.append(self._right_finger_id)
-
-        import ipdb; ipdb.set_trace()
 
         self._initial_joint_values = inverse_kinematics(
             self._panda_id,
@@ -289,7 +287,6 @@ class PandaPyBulletRobot(_SingleArmPyBulletRobot):
             self._arm_joints,
             physics_client_id=self._physics_client_id,
         )
-
 
     @property
     def robot_id(self) -> int:
@@ -325,14 +322,15 @@ class PandaPyBulletRobot(_SingleArmPyBulletRobot):
                 physicsClientId=self._physics_client_id,
             )
         for finger_id in [self._left_finger_id, self._right_finger_id]:
-            p.resetJointState(
-                self._panda_id, finger_id, rf, physicsClientId=self._physics_client_id
-            )
+            p.resetJointState(self._panda_id,
+                              finger_id,
+                              rf,
+                              physicsClientId=self._physics_client_id)
 
     def get_state(self) -> Array:
-        ee_link_state = p.getLinkState(
-            self._panda_id, self._ee_id, physicsClientId=self._physics_client_id
-        )
+        ee_link_state = p.getLinkState(self._panda_id,
+                                       self._ee_id,
+                                       physicsClientId=self._physics_client_id)
         rx, ry, rz = ee_link_state[4]
         rf = p.getJointState(
             self._panda_id,
@@ -343,9 +341,63 @@ class PandaPyBulletRobot(_SingleArmPyBulletRobot):
         return np.array([rx, ry, rz, rf], dtype=np.float32)
 
     def set_motors(self, ee_delta: Pose3D, f_delta: float) -> None:
-        raise NotImplementedError("Override me!")
+        ee_link_state = p.getLinkState(self._panda_id,
+                                       self._ee_id,
+                                       physicsClientId=self._physics_client_id)
+        current_position = ee_link_state[4]
+        target_position = np.add(current_position, ee_delta).tolist()
 
+        # We assume that the robot is already close enough to the target
+        # position that IK will succeed with one call, so validate is False.
+        # Furthermore, updating the state of the robot during simulation, which
+        # validate=True would do, is risky and discouraged by PyBullet.
+        joint_values = inverse_kinematics(
+            self._panda_id,
+            self._ee_id,
+            target_position,
+            self._ee_orientation,
+            self._arm_joints,
+            physics_client_id=self._physics_client_id,
+            validate=False,
+        )
 
+        # Set arm joint motors.
+        for joint_idx, joint_val in zip(self._arm_joints, joint_values):
+            p.setJointMotorControl2(
+                bodyIndex=self._panda_id,
+                jointIndex=joint_idx,
+                controlMode=p.POSITION_CONTROL,
+                targetPosition=joint_val,
+                physicsClientId=self._physics_client_id,
+            )
+
+        # Set finger joint motors.
+        for finger_id in [self._left_finger_id, self._right_finger_id]:
+            current_val = p.getJointState(
+                self._panda_id,
+                finger_id,
+                physicsClientId=self._physics_client_id)[0]
+            # Fingers drift if left alone. If the finger action is near zero,
+            # nudge the fingers toward being open or closed, based on which end
+            # of the spectrum they are currently closer to.
+            if abs(f_delta) < self._finger_action_tol:
+                assert self._open_fingers > self._closed_fingers
+                if abs(current_val -
+                       self._open_fingers) < abs(current_val -
+                                                 self._closed_fingers):
+                    nudge = self._finger_action_nudge_magnitude
+                else:
+                    nudge = -self._finger_action_nudge_magnitude
+                target_val = current_val + nudge
+            else:
+                target_val = current_val + f_delta
+            p.setJointMotorControl2(
+                bodyIndex=self._panda_id,
+                jointIndex=finger_id,
+                controlMode=p.POSITION_CONTROL,
+                targetPosition=target_val,
+                physicsClientId=self._physics_client_id,
+            )
 
 
 def create_single_arm_pybullet_robot(
@@ -358,5 +410,5 @@ def create_single_arm_pybullet_robot(
                                   finger_action_tol, physics_client_id)
     if robot_name == "panda":
         return PandaPyBulletRobot(ee_home_pose, open_fingers, closed_fingers,
-                                  finger_action_tol, physics_client_id)        
+                                  finger_action_tol, physics_client_id)
     raise NotImplementedError(f"Unrecognized robot name: {robot_name}.")
