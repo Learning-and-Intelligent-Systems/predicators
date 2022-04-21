acd1abaf6b21bd3023bc25c01016d8fb2d2d91d7
Tom Silver <tomssilver@gmail.com>
2022-04-21 15:03:59 -0400

    wip

--- tests/nsrt_learning/strips_learning/test_oracle_learner.py
+++ tests/nsrt_learning/strips_learning/test_oracle_learner.py
@@ -34,3 +34,33 @@ def test_oracle_strips_learner():
     Delete Effects: [Clear(?block:block), GripperOpen(?robot:robot), On(?block:block, ?otherblock:block)]
     Side Predicates: []
     Option Spec: Pick(?robot:robot, ?block:block)]"""  # pylint: disable=line-too-long
+    # Test with unknown options. Expected behavior is that the operators should
+    # be identical, except the option specs will be dummies.
+    utils.reset_config({"env": "blocks", "strips_learner": "oracle",
+                        "option_learner": "oracle"})
+    pnads = learn_strips_operators([], None, None, [])
+    assert str(sorted(pnads, key=str)) == """[STRIPS-PickFromTable:
+    Parameters: [?block:block, ?robot:robot]
+    Preconditions: [Clear(?block:block), GripperOpen(?robot:robot), OnTable(?block:block)]
+    Add Effects: [Holding(?block:block)]
+    Delete Effects: [Clear(?block:block), GripperOpen(?robot:robot), OnTable(?block:block)]
+    Side Predicates: []
+    Option Spec: DummyOption(), STRIPS-PutOnTable:
+    Parameters: [?block:block, ?robot:robot]
+    Preconditions: [Holding(?block:block)]
+    Add Effects: [Clear(?block:block), GripperOpen(?robot:robot), OnTable(?block:block)]
+    Delete Effects: [Holding(?block:block)]
+    Side Predicates: []
+    Option Spec: DummyOption(), STRIPS-Stack:
+    Parameters: [?block:block, ?otherblock:block, ?robot:robot]
+    Preconditions: [Clear(?otherblock:block), Holding(?block:block)]
+    Add Effects: [Clear(?block:block), GripperOpen(?robot:robot), On(?block:block, ?otherblock:block)]
+    Delete Effects: [Clear(?otherblock:block), Holding(?block:block)]
+    Side Predicates: []
+    Option Spec: DummyOption(), STRIPS-Unstack:
+    Parameters: [?block:block, ?otherblock:block, ?robot:robot]
+    Preconditions: [Clear(?block:block), GripperOpen(?robot:robot), On(?block:block, ?otherblock:block)]
+    Add Effects: [Clear(?otherblock:block), Holding(?block:block)]
+    Delete Effects: [Clear(?block:block), GripperOpen(?robot:robot), On(?block:block, ?otherblock:block)]
+    Side Predicates: []
+    Option Spec: DummyOption()]"""  # pylint: disable=line-too-long
