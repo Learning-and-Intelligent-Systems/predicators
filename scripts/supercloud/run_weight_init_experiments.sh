0031e7b2848867d8186b95326a506a6ad20821c3
Amber Li <amli@mit.edu>
2022-04-22 17:38:41 -0400

    refactor out common args, add default greedy to script

--- scripts/supercloud/run_weight_init_experiments.sh
+++ scripts/supercloud/run_weight_init_experiments.sh
@@ -13,17 +13,19 @@ FILE="scripts/supercloud/submit_supercloud_job.py"
 for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
 
     COMMON_ARGS="--env cover --approach interactive_learning --seed $SEED --excluded_predicates Covers,Holding \
+    --interactive_score_function entropy --interactive_query_policy threshold --interactive_score_threshold 0.05 \
     --online_learning_max_transitions $MAX_TRANSITIONS --num_online_learning_cycles $CYCLES \
     --interactive_num_requests_per_cycle $REQUESTS --max_num_steps_interaction_request $MAX_STEPS \
     --min_data_for_nsrt $MIN_DATA --sampler_disable_classifier True --mlp_classifier_balance_data False \
     --predicate_mlp_classifier_max_itr $MAX_ITR"
 
     # glib
-    python $FILE $COMMON_ARGS --experiment_id glib_default --interactive_action_strategy glib --interactive_query_policy threshold --interactive_score_function entropy --interactive_score_threshold 0.05 --predicate_mlp_classifier_init default --predicate_mlp_classifier_init_param None
-    python $FILE $COMMON_ARGS --experiment_id glib_normal_1 --interactive_action_strategy glib --interactive_query_policy threshold --interactive_score_function entropy --interactive_score_threshold 0.05 --predicate_mlp_classifier_init normal --predicate_mlp_classifier_init_param 1.0
-    python $FILE $COMMON_ARGS --experiment_id glib_uniform_3 --interactive_action_strategy glib --interactive_query_policy threshold --interactive_score_function entropy --interactive_score_threshold 0.05 --predicate_mlp_classifier_init uniform --predicate_mlp_classifier_init_param 3.0
+    python $FILE $COMMON_ARGS --experiment_id glib_default --interactive_action_strategy glib --predicate_mlp_classifier_init default --predicate_mlp_classifier_init_param None
+    python $FILE $COMMON_ARGS --experiment_id glib_normal_1 --interactive_action_strategy glib --predicate_mlp_classifier_init normal --predicate_mlp_classifier_init_param 1.0
+    python $FILE $COMMON_ARGS --experiment_id glib_uniform_3 --interactive_action_strategy glib --predicate_mlp_classifier_init uniform --predicate_mlp_classifier_init_param 3.0
 
     # greedy
-    python $FILE $COMMON_ARGS --experiment_id greedy_normal_1 --interactive_action_strategy greedy_lookahead --interactive_query_policy threshold --interactive_score_function entropy --interactive_score_threshold 0.05 --predicate_mlp_classifier_init normal --predicate_mlp_classifier_init_param 1.0
-    python $FILE $COMMON_ARGS --experiment_id greedy_uniform_3 --interactive_action_strategy greedy_lookahead --interactive_query_policy threshold --interactive_score_function entropy --interactive_score_threshold 0.05 --predicate_mlp_classifier_init uniform --predicate_mlp_classifier_init_param 3.0
+    python $FILE $COMMON_ARGS --experiment_id greedy_default --interactive_action_strategy greedy_lookahead --predicate_mlp_classifier_init default --predicate_mlp_classifier_init_param None
+    python $FILE $COMMON_ARGS --experiment_id greedy_normal_1 --interactive_action_strategy greedy_lookahead --predicate_mlp_classifier_init normal --predicate_mlp_classifier_init_param 1.0
+    python $FILE $COMMON_ARGS --experiment_id greedy_uniform_3 --interactive_action_strategy greedy_lookahead --predicate_mlp_classifier_init uniform --predicate_mlp_classifier_init_param 3.0
 done
