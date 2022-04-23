3ab5ccc00b96202fcc427bf543f917f280d7e115
Tom Silver <tomssilver@gmail.com>
2022-04-23 19:18:13 -0400

    updates to option learning script

--- scripts/supercloud/run_option_learning_experiments.sh
+++ scripts/supercloud/run_option_learning_experiments.sh
@@ -6,32 +6,16 @@ FILE="scripts/supercloud/submit_supercloud_job.py"
 
 for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
 
-    ## all in the stick point environment
-    ## all with oracle operators and segmentation for now!
-    COMMON_ARGS="--env stick_point --approach nsrt_learning --strips_learner oracle --segmenter oracle --seed $SEED"
+    COMMON_ARGS="--env stick_point --min_data_for_nsrt 10 --segmenter oracle \
+        --num_train_tasks 500 --timeout 300 --seed $SEED"
 
-    # nsrt learning (oracle options) 500, default test time
-    python $FILE $COMMON_ARGS --experiment_id given_500 --num_train_tasks 500
+    # nsrt learning (oracle operators and options)
+    python $FILE $COMMON_ARGS --experiment_id oracle_options --approach nsrt_learning --strips_learner oracle
 
-    # nsrt learning (oracle options) 5000, default test time
-    python $FILE $COMMON_ARGS --experiment_id given_5000 --num_train_tasks 5000
+    # direct BC (main approach)
+    python $FILE $COMMON_ARGS --experiment_id direct_bc --approach nsrt_learning --option_learner direct_bc
 
-    # nsrt learning (oracle options) 500, long test time
-    python $FILE $COMMON_ARGS --experiment_id given_500_long --num_train_tasks 500 --timeout 300
-
-    # nsrt learning (oracle options) 5000, long test time
-    python $FILE $COMMON_ARGS --experiment_id given_5000_long --num_train_tasks 5000 --timeout 300
-
-    # direct BC 500, default test time
-    python $FILE $COMMON_ARGS --experiment_id direct_bc_500 --option_learner direct_bc --num_train_tasks 500
-
-    # direct BC 5000, default test time
-    python $FILE $COMMON_ARGS --experiment_id direct_bc_5000 --option_learner direct_bc --num_train_tasks 5000
-
-    # direct BC 500, long test time
-    python $FILE $COMMON_ARGS --experiment_id direct_bc_500_long --option_learner direct_bc --num_train_tasks 500 --timeout 300
-
-    # direct BC 5000, long test time
-    python $FILE $COMMON_ARGS --experiment_id direct_bc_5000_long --option_learner direct_bc --num_train_tasks 5000 --timeout 300
+    # GNN BC with shooting baseline
+    python $FILE $COMMON_ARGS --experiment_id gnn_shooting --approach gnn_policy
 
 done
