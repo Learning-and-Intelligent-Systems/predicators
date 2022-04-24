885ab4bab599da4d244ba94cf9950bbc9a3e58fa
Tom Silver <tomssilver@gmail.com>
2022-04-23 21:06:09 -0400

    range number of train tasks

--- scripts/supercloud/run_option_learning_experiments.sh
+++ scripts/supercloud/run_option_learning_experiments.sh
@@ -3,22 +3,33 @@
 START_SEED=456
 NUM_SEEDS=10
 FILE="scripts/supercloud/submit_supercloud_job.py"
+ALL_NUM_TRAIN_TASKS=(
+    "50"
+    "100"
+    "500"
+    "1000"
+    "5000"
+    "10000"
+)
 
 for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
+    for NUM_TRAIN_TASKS in ${ALL_NUM_TRAIN_TASKS[@]}; do
 
-    COMMON_ARGS="--env stick_point --min_data_for_nsrt 10 --segmenter oracle \
-        --num_train_tasks 500 --timeout 300 --seed $SEED"
+        # NOTE: segmenter is oracle
+        COMMON_ARGS="--env stick_point --min_data_for_nsrt 10 --segmenter oracle \
+            --num_train_tasks $NUM_TRAIN_TASKS --timeout 300 --seed $SEED"
 
-    # nsrt learning (oracle operators and options)
-    python $FILE $COMMON_ARGS --experiment_id oracle_options --approach nsrt_learning --strips_learner oracle
+        # nsrt learning (oracle operators and options)
+        python $FILE $COMMON_ARGS --experiment_id oracle_options_${NUM_TRAIN_TASKS} --approach nsrt_learning --strips_learner oracle
 
-    # direct BC (main approach)
-    python $FILE $COMMON_ARGS --experiment_id direct_bc --approach nsrt_learning --option_learner direct_bc
+        # direct BC (main approach)
+        python $FILE $COMMON_ARGS --experiment_id direct_bc_${NUM_TRAIN_TASKS} --approach nsrt_learning --option_learner direct_bc
 
-    # GNN BC with shooting baseline
-    python $FILE $COMMON_ARGS --experiment_id gnn_shooting --approach gnn_policy
+        # GNN BC with shooting baseline
+        python $FILE $COMMON_ARGS --experiment_id gnn_shooting_${NUM_TRAIN_TASKS} --approach gnn_policy
 
-    # GNN BC model-free
-    python $FILE $COMMON_ARGS --experiment_id gnn_modelfree --approach gnn_policy --gnn_policy_solve_with_shooting False
+        # GNN BC model-free
+        python $FILE $COMMON_ARGS --experiment_id gnn_modelfree_${NUM_TRAIN_TASKS} --approach gnn_policy --gnn_policy_solve_with_shooting False
 
+    done
 done
