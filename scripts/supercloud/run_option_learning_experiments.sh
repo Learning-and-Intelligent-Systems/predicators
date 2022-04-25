0517d0e95a53b1d70e101c8f3e843606787f92ea
Tom Silver <tomssilver@gmail.com>
2022-04-25 09:38:15 -0400

    script updates

--- scripts/supercloud/run_option_learning_experiments.sh
+++ scripts/supercloud/run_option_learning_experiments.sh
@@ -9,10 +9,9 @@ RUN_LOAD_EXPERIMENTS=false
 ALL_NUM_TRAIN_TASKS=(
     "50"
     "100"
+    "250"
     "500"
     "1000"
-    "5000"
-    "10000"
 )
 
 for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
@@ -24,18 +23,14 @@ for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
             --seed $SEED"
 
         if [ "$RUN_LOAD_EXPERIMENTS" = true ]; then
-
-            # TODO: can't launch this part yet.
-            # See https://github.com/Learning-and-Intelligent-Systems/predicators/issues/783
-
             # direct BC max skeletons 1
-            python $FILE $COMMON_ARGS --experiment_id direct_bc_max_skel1_${NUM_TRAIN_TASKS} --approach nsrt_learning --option_learner direct_bc --sesame_max_skeletons_optimized 1 --load_a --load_d
+            python $FILE $COMMON_ARGS --load_experiment_id direct_bc_${NUM_TRAIN_TASKS} --experiment_id direct_bc_max_skel1_${NUM_TRAIN_TASKS} --approach nsrt_learning --option_learner direct_bc --sesame_max_skeletons_optimized 1 --load_a --load_d
 
             # direct BC max samples 1
-            python $FILE $COMMON_ARGS --experiment_id direct_bc_max_samp1_${NUM_TRAIN_TASKS} --approach nsrt_learning --option_learner direct_bc --sesame_max_samples_per_step 1 --load_a --load_d
+            python $FILE $COMMON_ARGS --load_experiment_id direct_bc_${NUM_TRAIN_TASKS} --experiment_id direct_bc_max_samp1_${NUM_TRAIN_TASKS} --approach nsrt_learning --option_learner direct_bc --sesame_max_samples_per_step 1 --load_a --load_d
 
             # GNN BC model-free
-            python $FILE $COMMON_ARGS --experiment_id gnn_modelfree_${NUM_TRAIN_TASKS} --approach gnn_policy --gnn_policy_solve_with_shooting False --load_a --load_d
+            python $FILE $COMMON_ARGS --load_experiment_id gnn_shooting_${NUM_TRAIN_TASKS} --experiment_id gnn_modelfree_${NUM_TRAIN_TASKS} --approach gnn_policy --gnn_policy_solve_with_shooting False --load_a --load_d
         else
             # nsrt learning (oracle operators and options)
             python $FILE $COMMON_ARGS --experiment_id oracle_options_${NUM_TRAIN_TASKS} --approach nsrt_learning --strips_learner oracle
