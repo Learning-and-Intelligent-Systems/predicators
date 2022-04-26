01c76578adbe395bb3b83aaaea7e3f1b9edede8e
Tom Silver <tomssilver@gmail.com>
2022-04-26 14:31:35 -0400

    more flags

--- scripts/supercloud/temp_script.sh
+++ scripts/supercloud/temp_script.sh
@@ -12,10 +12,10 @@ for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
     for ENV in ${ALL_ENVS[@]}; do
 
         # GNN option policy (model-free).
-        python $FILE --experiment_id ${ENV}_options_mf --env $ENV --approach gnn_option_policy --seed $SEED --gnn_option_policy_solve_with_shooting False
+        python $FILE --experiment_id ${ENV}_options_mf --env $ENV --approach gnn_option_policy --seed $SEED --gnn_option_policy_solve_with_shooting False --excluded_predicates all --num_train_tasks 200
 
         # GNN action policy (model-free).
-        python $FILE --experiment_id ${ENV}_actions_mf --env $ENV --approach gnn_action_policy --seed $SEED --gnn_option_policy_solve_with_shooting False
+        python $FILE --experiment_id ${ENV}_actions_mf --env $ENV --approach gnn_action_policy --seed $SEED --gnn_option_policy_solve_with_shooting False --excluded_predicates all --num_train_tasks 200
 
     done
 done
