7804ae4bca9099f13225f48c7474f98337dd7f2f
Amber Li <amli@mit.edu>
2022-04-29 09:27:48 -0400

    adjust probabilities

--- scripts/supercloud/run_random_query_experiments.sh
+++ scripts/supercloud/run_random_query_experiments.sh
@@ -10,9 +10,9 @@ MAX_STEPS=3
 MIN_DATA=10
 THRESH="0.05"
 MAX_ITR=100000
-START_PROB="0.1"
-INCREMENT="0.1"
-END_PROB="0.9"
+START_PROB="0.01"
+INCREMENT="0.01"
+END_PROB="0.1"
 FILE="scripts/supercloud/submit_supercloud_job.py"
 
 for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
@@ -25,6 +25,9 @@ for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
     --max_num_steps_interaction_request $MAX_STEPS --min_data_for_nsrt $MIN_DATA \
     --sampler_disable_classifier True --mlp_classifier_balance_data False \
     --predicate_mlp_classifier_max_itr $MAX_ITR"
+
+    # Main approach
+    python $FILE $COMMON_ARGS --experiment_id main
 
     for PROB in $(seq $START_PROB $INCREMENT $END_PROB); do
 
