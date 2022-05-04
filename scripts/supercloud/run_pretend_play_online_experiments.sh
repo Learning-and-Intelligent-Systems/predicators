677a2ee6778e691b541fa1e8ab183dca5fb483e2
Tom Silver <tomssilver@gmail.com>
2022-05-04 13:35:17 -0400

    oops this is why i get throttled

--- scripts/supercloud/run_pretend_play_online_experiments.sh
+++ scripts/supercloud/run_pretend_play_online_experiments.sh
@@ -5,7 +5,7 @@ NUM_SEEDS=10
 FILE="scripts/supercloud/submit_supercloud_job.py"
 
 for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
-    python src/main.py --experiment_id pretend_play_blocks --env play_blocks \
+    python $FILE --experiment_id pretend_play_blocks --env play_blocks \
     --approach pretend_play --seed $SEED --max_initial_demos 1 \
     --num_online_learning_cycles 10000 --online_learning_max_transitions 1000 \
     --allow_interaction_in_demo_tasks False --max_num_steps_interaction_request 10 \
