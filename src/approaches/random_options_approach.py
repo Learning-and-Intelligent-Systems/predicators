fc15e7938416e8e19400f1dcf989eef342be9eb8
Tom Silver <tomssilver@gmail.com>
2022-05-05 18:50:24 -0400

    feedback

--- src/approaches/random_options_approach.py
+++ src/approaches/random_options_approach.py
@@ -26,6 +26,7 @@ class RandomOptionsApproach(BaseApproach):
         def _policy(state: State) -> Action:
             nonlocal cur_option
             if cur_option is DummyOption or cur_option.terminal(state):
+                cur_option = DummyOption
                 for _ in range(CFG.random_options_max_tries):
                     param_opt = options[self._rng.choice(len(options))]
                     objs = utils.get_random_object_combination(
@@ -38,7 +39,6 @@ class RandomOptionsApproach(BaseApproach):
                         cur_option = opt
                         break
                 else:  # fall back to a random action
-                    cur_option = DummyOption
                     return Action(self._action_space.sample())
             act = cur_option.policy(state)
             return act
