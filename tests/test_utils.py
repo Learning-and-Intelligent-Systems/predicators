ba74b99ec2013d33e30c1b153962575196c6e902
Tom Silver <tomssilver@gmail.com>
2022-05-06 11:09:12 -0400

    minor

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2159,7 +2159,7 @@ def test_VideoMonitor():
     assert len(traj.actions) == 2
     video = monitor.get_video()
     assert len(video) == len(traj.states)
-    first_state_rendered = env.render_state(task.init, task=None)
+    first_state_rendered = env.render_state(task.init, task)
     assert np.allclose(first_state_rendered, video[0])
     assert not np.allclose(first_state_rendered, video[1])
 
@@ -2182,7 +2182,7 @@ def test_SimulateVideoMonitor():
     assert len(traj.actions) == 2
     video = monitor.get_video()
     assert len(video) == len(traj.states)
-    first_state_rendered = env.render_state(task.init, task=None)
+    first_state_rendered = env.render_state(task.init, task)
     assert np.allclose(first_state_rendered, video[0])
     assert not np.allclose(first_state_rendered, video[1])
 
