5e0da1b21611587f7f79e0a7c3100146c53fd0d3
Tom Silver <tomssilver@gmail.com>
2022-05-14 12:43:08 -0400

    speed up nsrt rl approach test

--- tests/approaches/test_nsrt_rl_approach.py
+++ tests/approaches/test_nsrt_rl_approach.py
@@ -3,20 +3,17 @@
 import pytest
 
 from predicators.src import utils
-from predicators.src.approaches import ApproachFailure, ApproachTimeout
 from predicators.src.approaches.nsrt_rl_approach import \
     NSRTReinforcementLearningApproach
 from predicators.src.datasets import create_dataset
 from predicators.src.envs.cover import CoverMultistepOptions
 from predicators.src.main import _generate_interaction_results
-from predicators.src.settings import CFG
 from predicators.src.teacher import Teacher
 
 
 def test_nsrt_reinforcement_learning_approach():
     """Test for NSRTReinforcementLearningApproach class, entire pipeline."""
-
-    utils.reset_config({
+    config = {
         "env": "cover_multistep_options",
         "cover_multistep_thr_percent": 0.99,
         "cover_multistep_bhr_percent": 0.99,
@@ -27,7 +24,8 @@ def test_nsrt_reinforcement_learning_approach():
         "sampler_learner": "random",
         "num_online_learning_cycles": 1,
         "mlp_regressor_max_itr": 1,
-    })
+    }
+    utils.reset_config(config)
     env = CoverMultistepOptions()
     train_tasks = env.get_train_tasks()
     options = utils.parse_config_included_options(env)
@@ -39,6 +37,10 @@ def test_nsrt_reinforcement_learning_approach():
     assert approach.is_learning_based
     approach.learn_from_offline_dataset(dataset)
     approach.load(online_learning_cycle=None)
+    # Reduce the timeout for planning in the interaction loop, to make the
+    # test faster.
+    config["timeout"] = 0.01
+    utils.reset_config(config)
     interaction_requests = approach.get_interaction_requests()
     interaction_results, _ = _generate_interaction_results(
         env, teacher, interaction_requests)
@@ -46,10 +48,3 @@ def test_nsrt_reinforcement_learning_approach():
     approach.load(online_learning_cycle=1)
     with pytest.raises(FileNotFoundError):
         approach.load(online_learning_cycle=2)
-    # We won't check the policy here because we don't want unit tests to
-    # have to train very good models, since that would be slow.
-    for task in env.get_test_tasks():
-        try:
-            approach.solve(task, timeout=CFG.timeout)
-        except (ApproachTimeout, ApproachFailure):  # pragma: no cover
-            pass
