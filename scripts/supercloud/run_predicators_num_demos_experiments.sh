aa1f4a031394e275a5c5744960f51882b495e6d6
ronuchit <ronuchit@mit.edu>
2022-05-17 19:11:35 -0400

    use job arrays for supercloud (#921)
    * try stuff
    
    * minor
    
    * changes
    
    * revert
    
    * remove prints
    
    * fix other scripts too
    
    * yapf

--- scripts/supercloud/run_predicators_num_demos_experiments.sh
+++ scripts/supercloud/run_predicators_num_demos_experiments.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-START_SEED=456
-NUM_SEEDS=10
 FILE="scripts/supercloud/submit_supercloud_job.py"
 ALL_NUM_TRAIN_TASKS=(
     "25"
@@ -19,13 +17,11 @@ ALL_ENVS=(
     "tools"
 )
 
-for SEED in $(seq $START_SEED $((NUM_SEEDS+START_SEED-1))); do
-    for ENV in ${ALL_ENVS[@]}; do
-        for NUM_TRAIN_TASKS in ${ALL_NUM_TRAIN_TASKS[@]}; do
-            # Main approach.
-            python $FILE --experiment_id ${ENV}_main_${NUM_TRAIN_TASKS}demo --env $ENV --approach grammar_search_invention --excluded_predicates all --seed $SEED --num_train_tasks $NUM_TRAIN_TASKS
-            # GNN option policy approach.
-            python $FILE --experiment_id ${ENV}_gnn_shooting_${NUM_TRAIN_TASKS}demo --env $ENV --approach gnn_option_policy --excluded_predicates all --seed $SEED --num_train_tasks $NUM_TRAIN_TASKS
-        done
+for ENV in ${ALL_ENVS[@]}; do
+    for NUM_TRAIN_TASKS in ${ALL_NUM_TRAIN_TASKS[@]}; do
+        # Main approach.
+        python $FILE --experiment_id ${ENV}_main_${NUM_TRAIN_TASKS}demo --env $ENV --approach grammar_search_invention --excluded_predicates all --num_train_tasks $NUM_TRAIN_TASKS
+        # GNN option policy approach.
+        python $FILE --experiment_id ${ENV}_gnn_shooting_${NUM_TRAIN_TASKS}demo --env $ENV --approach gnn_option_policy --excluded_predicates all --num_train_tasks $NUM_TRAIN_TASKS
     done
 done
