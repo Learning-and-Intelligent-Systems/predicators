4601830f98049e98fc85439f618b81998c7d4bff
Rohan Chitnis <ronuchit@mit.edu>
2022-05-17 17:47:27 -0400

    try stuff

--- scripts/supercloud/submit_supercloud_job.py
+++ scripts/supercloud/submit_supercloud_job.py
@@ -7,22 +7,27 @@ import sys
 from predicators.src import utils
 from predicators.src.settings import CFG
 
+START_SEED=456
+NUM_SEEDS=10
+
 
 def _run() -> None:
-    args = utils.parse_args()
+    args = utils.parse_args(seed_required=False)
     utils.update_config(args)
-    job_name = f"{CFG.experiment_id}_{CFG.seed}"
+    job_name = f"{CFG.experiment_id}"
     os.makedirs(CFG.log_dir, exist_ok=True)
     logfile_pattern = os.path.join(CFG.log_dir,
                                    f"{utils.get_config_path_str()}__%j.log")
     argsstr = " ".join(sys.argv[1:])
-    mystr = f"#!/bin/bash\npython src/main.py {argsstr}"
+    mystr = (f"#!/bin/bash\npython src/main.py {argsstr} "
+             f"--seed $SLURM_ARRAY_TASK_ID")
     temp_run_file = "temp_run_file.sh"
     assert not os.path.exists(temp_run_file)
     with open(temp_run_file, "w", encoding="utf-8") as f:
         f.write(mystr)
     cmd = ("sbatch -p normal --time=99:00:00 --partition=xeon-p8 "
            f"--nodes=1 --exclusive --job-name={job_name} "
+           f"--array={START_SEED}-{START_SEED+NUM_SEEDS-1} "
            f"-o {logfile_pattern} {temp_run_file}")
     print(f"Running command: {cmd}")
     output = subprocess.getoutput(cmd)
