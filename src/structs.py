ca98c919e27c82c8db3c5dc289b64e33002403a8
Willie McClinton <wbm3@mit.edu>
2022-05-17 15:32:51 -0400

    TODOed all_close

--- src/structs.py
+++ src/structs.py
@@ -166,10 +166,11 @@ class State:
     def allclose(self, other: State) -> bool:
         """Return whether this state is close enough to another one, i.e., its
         objects are the same, and the features are close."""
-        if self.simulator_state is not None or \
-           other.simulator_state is not None:
-            raise NotImplementedError("Cannot use allclose when "
-                                      "simulator_state is not None.")
+        # TODO Override for behavior when simulation traj
+        # if self.simulator_state is not None or \
+        #    other.simulator_state is not None:
+        #     raise NotImplementedError("Cannot use allclose when "
+        #                               "simulator_state is not None.")
         if not sorted(self.data) == sorted(other.data):
             return False
         for obj in self.data:
