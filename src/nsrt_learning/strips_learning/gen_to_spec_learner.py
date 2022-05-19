beb1ec2b3fafb908ef4bf208a0c87bc6865ed7a5
Rohan Chitnis <ronuchit@mit.edu>
2022-05-19 17:57:55 -0400

    try a thing

--- src/nsrt_learning/strips_learning/gen_to_spec_learner.py
+++ src/nsrt_learning/strips_learning/gen_to_spec_learner.py
@@ -253,8 +253,8 @@ class BackchainingSTRIPSLearner(GeneralToSpecificSTRIPSLearner):
         final_pnads = []
         for pnad in nec_pnads:
             if pnad.datastore:
-                pre = self._induce_preconditions_via_intersection(pnad)
-                pnad.op = pnad.op.copy_with(preconditions=pre)
+                # pre = self._induce_preconditions_via_intersection(pnad)
+                # pnad.op = pnad.op.copy_with(preconditions=pre)
                 final_pnads.append(pnad)
 
         return final_pnads
