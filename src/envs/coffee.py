1edee18175286486b0e373287a9f685af2a70f64
Tom Silver <tomssilver@gmail.com>
2022-05-22 09:01:27 -0400

    goal 2

--- src/envs/coffee.py
+++ src/envs/coffee.py
@@ -453,7 +453,7 @@ class CoffeeEnv(BaseEnv):
             cups = [Object(f"cup{i}", self._cup_type) for i in range(num_cups)]
             # TODO change back
             # goal = {GroundAtom(self._CupFilled, [c]) for c in cups}
-            goal = {GroundAtom(self._Holding, [self._robot, self._jug])}
+            goal = {GroundAtom(self._JugInMachine, [self._jug, self._machine])}
             # Sample initial positions for cups, making sure to keep them
             # far enough apart from one another.
             radius = self.cup_radius + self.init_padding
