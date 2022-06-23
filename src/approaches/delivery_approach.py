368686afb0932aa08f39e90a274181f1d1fc3241
Tom Silver <tomssilver@gmail.com>
2022-06-23 15:55:54 -0400

    wip

--- src/approaches/delivery_approach.py
+++ src/approaches/delivery_approach.py
@@ -2,57 +2,64 @@
 
 Example command line:
     python src/main.py --approach delivery_policy --seed 0 \
-        --env pddl_easy_delivery_procedural_tasks
+        --env pddl_easy_delivery_procedural_tasks \
+        --strips_learner oracle --num_train_tasks 1
 """
 
 from typing import Callable, cast
 
 import numpy as np
 
-from predicators.src.approaches import BaseApproach
+from predicators.src.approaches import ApproachFailure
+from predicators.src.approaches.nsrt_learning_approach import NSRTLearningApproach
 from predicators.src.envs.pddl_env import _PDDLEnvState
-from predicators.src.structs import Action, GroundAtom, State, Task
+from predicators.src.structs import Action, GroundAtom, State, Task, LiftedDecisionList, LDLRule
+from predicators.src import utils
 
 
-class DeliverySpecificApproach(BaseApproach):
+class DeliverySpecificApproach(NSRTLearningApproach):
     """Implements a delivery-specific policy."""
 
     @classmethod
     def get_name(cls) -> str:
         return "delivery_policy"
 
-    @property
-    def is_learning_based(self) -> bool:
-        return False
-
     def _solve(self, task: Task, timeout: int) -> Callable[[State], Action]:
+        goal = task.goal
+        options = {o.name: o for o in self._initial_options}
+        predicates = {p.name: p for p in self._initial_predicates}
+        types = {t.name: t for t in self._types}
+        nsrts = {n.name: n for n in self._get_current_nsrts()}
+        deliver = nsrts["deliver"]
+        move = nsrts["move"]
+        pick_up = nsrts["pick_up"]
+        paper_var, loc_var = deliver.parameters
+        from_var, to_var = move.parameters
+
+        # TODO: Fix the code here!
+
+        rules = [
+            LDLRule(
+                "Deliver",
+                parameters=[paper_var, loc_var],
+                state_preconditions={at([loc_var]),
+                                     wants_paper([loc_var])},
+                goal_preconditions=set(),
+                nsrt=pick_nsrt
+            ),
+        ]
+        
+        # Below this line should not need changing.
+
+        ldl_policy = LiftedDecisionList("DeliveryPolicy", rules)
 
         def _policy(state: State) -> Action:
-            options = {o.name: o for o in self._initial_options}
-            predicates = {p.name: p for p in self._initial_predicates}
-            types = {t.name: t for t in self._types}
             state = cast(_PDDLEnvState, state)
             ground_atoms = state.get_ground_atoms()
-            locations = state.get_objects(types["loc"])
-            papers = state.get_objects(types["paper"])
-            at = predicates["at"]
-            wants_paper = predicates["wantspaper"]
-            move = options["move"]
-            # TODO: Fix the code here!
-            selected_option = move
-            move_to_loc = None
-            move_from_loc = None
-            for loc in locations:
-                if GroundAtom(at, [loc]) in ground_atoms:
-                    move_from_loc = loc
-                elif GroundAtom(wants_paper, [loc]) in ground_atoms:
-                    move_to_loc = loc
-            assert move_to_loc is not None
-            assert move_from_loc is not None
-            object_args = [move_from_loc, move_to_loc]
-            # Below this line should not need changing.
-            params = np.zeros(0, dtype=np.float32)
-            ground_option = selected_option.ground(object_args, params)
+            ground_nsrt = utils.query_ldl(ldl_policy, ground_atoms, goal)
+            if ground_nsrt is None:
+                raise ApproachFailure("LDL policy was not applicable!")
+            ground_option = ground_nsrt.sample_option(state, goal, self._rng)
             assert ground_option.initiable(state)
             return ground_option.policy(state)
 
