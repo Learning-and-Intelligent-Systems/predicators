dc0e65062f5495ea949871469309169c996da65d
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-06-27 15:45:22 -0400

    fix for type bug

--- src/envs/behavior.py
+++ src/envs/behavior.py
@@ -265,7 +265,7 @@ class BehaviorEnv(BaseEnv):
                 pred = Predicate(pred_name, list(type_combo), classifier)
                 # We only care about reachable when the agent is one of the
                 # types.
-                if name == "reachable" and not any([type.name == "agent" in type_combo]):
+                if name == "reachable" and not any([type_i.name == "agent" for type_i in type_combo]):
                     continue
                 else:
                     predicates.add(pred)
