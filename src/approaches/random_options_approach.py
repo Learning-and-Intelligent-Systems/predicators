c6b8031004a590e6525862e6836cc4fb17da36a4
Tom Silver <tomssilver@gmail.com>
2022-07-01 08:30:29 -0400

    resolve circular imports

--- src/approaches/random_options_approach.py
+++ src/approaches/random_options_approach.py
@@ -4,8 +4,7 @@ from typing import Callable
 
 from predicators.src import utils
 from predicators.src.approaches import BaseApproach
-from predicators.src.settings import CFG
-from predicators.src.structs import Action, DummyOption, State, Task
+from predicators.src.structs import Action, State, Task
 
 
 class RandomOptionsApproach(BaseApproach):
@@ -20,27 +19,5 @@ class RandomOptionsApproach(BaseApproach):
         return False
 
     def _solve(self, task: Task, timeout: int) -> Callable[[State], Action]:
-        options = sorted(self._initial_options, key=lambda o: o.name)
-        cur_option = DummyOption
-
-        def _policy(state: State) -> Action:
-            nonlocal cur_option
-            if cur_option is DummyOption or cur_option.terminal(state):
-                cur_option = DummyOption
-                for _ in range(CFG.random_options_max_tries):
-                    param_opt = options[self._rng.choice(len(options))]
-                    objs = utils.get_random_object_combination(
-                        list(state), param_opt.types, self._rng)
-                    if objs is None:
-                        continue
-                    params = param_opt.params_space.sample()
-                    opt = param_opt.ground(objs, params)
-                    if opt.initiable(state):
-                        cur_option = opt
-                        break
-                else:  # fall back to a random action
-                    return Action(self._action_space.sample())
-            act = cur_option.policy(state)
-            return act
-
-        return _policy
+        return utils.create_random_option_policy(self._initial_options,
+                                                 self._action_space, self._rng)
--- src/interaction/random_options_explorer.py
+++ src/interaction/random_options_explorer.py
@@ -1,26 +1,12 @@
 """An explorer that takes random options."""
 
-from typing import List, Set
-
-from gym.spaces import Box
-
-from predicators.src.approaches.random_options_approach import \
-    RandomOptionsApproach
+from predicators.src import utils
 from predicators.src.interaction import BaseExplorer
-from predicators.src.structs import ExplorationStrategy, ParameterizedOption, \
-    Predicate, Task, Type
+from predicators.src.structs import ExplorationStrategy, Task
 
 
 class RandomOptionsExplorer(BaseExplorer):
     """Samples random options."""
-
-    def __init__(self, predicates: Set[Predicate],
-                 options: Set[ParameterizedOption], types: Set[Type],
-                 action_space: Box, train_tasks: List[Task]) -> None:
-        super().__init__(predicates, options, types, action_space, train_tasks)
-        # Set up a random options approach.
-        self._random_options_approach = RandomOptionsApproach(
-            predicates, options, types, action_space, train_tasks)
 
     @classmethod
     def get_name(cls) -> str:
@@ -29,7 +15,9 @@ class RandomOptionsExplorer(BaseExplorer):
     def get_exploration_strategy(self, task: Task,
                                  timeout: int) -> ExplorationStrategy:
         # Take random options.
-        policy = self._random_options_approach.solve(task, timeout)
+        policy = utils.create_random_option_policy(self._options,
+                                                   self._action_space,
+                                                   self._rng)
         # Never terminate (until the interaction budget is exceeded).
         termination_function = lambda _: False
         return policy, termination_function
