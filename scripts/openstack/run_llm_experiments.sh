6ea44777864e9c6b658381210b1804cd95131e8a
Tom Silver <tomssilver@gmail.com>
2022-07-16 13:42:09 -0400

    delivery and blocks updates

--- scripts/openstack/run_llm_experiments.sh
+++ scripts/openstack/run_llm_experiments.sh
@@ -1,16 +1,99 @@
 #!/bin/bash
 
-# Experimental settings.
-EXPERIMENT_ID="test"
-ENV="blocks"
-APPROACH="nsrt_learning"
-OTHER_FLAGS=""
+# General settings.
+FLAGS="--num_train_tasks 5 --num_test_tasks 10 --strips_learner oracle --timeout 100"
 
-# Run this script with either "launch" or "download" as the first argument.
-if [[ $1 != "launch" && $1 != "download" ]]; then
-echo "ERROR: Run this script with either 'launch' or 'download'."
-exit
-fi
+# Approach settings.
+
+# Oracle planning approach.
+APPROACH_EXPERIMENT_ID="oracle"
+APPROACH="oracle"
+APPROACH_FLAGS=""
+
+# # GNN option policy approach.
+# APPROACH_EXPERIMENT_ID="gnn_mf"
+# APPROACH="gnn_option_policy"
+# APPROACH_FLAGS="--gnn_option_policy_solve_with_shooting False"
+
+# # LLM approach (coming soon).
+
+
+# Environment settings.
+
+# Easy blocks environment.
+ENV_EXPERIMENT_ID="easy-blocks"
+ENV="pddl_blocks_procedural_tasks"
+ENV_FLAGS="--pddl_blocks_procedural_train_min_num_blocks 3 \
+    --pddl_blocks_procedural_train_max_num_blocks 4 \
+    --pddl_blocks_procedural_train_min_num_blocks_goal 2 \
+    --pddl_blocks_procedural_train_max_num_blocks_goal 3 \
+    --pddl_blocks_procedural_test_min_num_blocks 5 \
+    --pddl_blocks_procedural_test_max_num_blocks 6 \
+    --pddl_blocks_procedural_test_min_num_blocks_goal 2 \
+    --pddl_blocks_procedural_test_max_num_blocks_goal 5"
+
+# # Medium blocks environment.
+# ENV_EXPERIMENT_ID="medium-blocks"
+# ENV="pddl_blocks_procedural_tasks"
+# ENV_FLAGS="--pddl_blocks_procedural_train_min_num_blocks 7 \
+#     --pddl_blocks_procedural_train_max_num_blocks 8 \
+#     --pddl_blocks_procedural_train_min_num_blocks_goal 6 \
+#     --pddl_blocks_procedural_train_max_num_blocks_goal 7 \
+#     --pddl_blocks_procedural_test_min_num_blocks 9 \
+#     --pddl_blocks_procedural_test_max_num_blocks 10 \
+#     --pddl_blocks_procedural_test_min_num_blocks_goal 6 \
+#     --pddl_blocks_procedural_test_max_num_blocks_goal 9"
+
+# # Hard blocks environment.
+# ENV_EXPERIMENT_ID="hard-blocks"
+# ENV="pddl_blocks_procedural_tasks"
+# ENV_FLAGS="--pddl_blocks_procedural_train_min_num_blocks 11 \
+#     --pddl_blocks_procedural_train_max_num_blocks 12 \
+#     --pddl_blocks_procedural_train_min_num_blocks_goal 10 \
+#     --pddl_blocks_procedural_train_max_num_blocks_goal 11 \
+#     --pddl_blocks_procedural_test_min_num_blocks 13 \
+#     --pddl_blocks_procedural_test_max_num_blocks 14 \
+#     --pddl_blocks_procedural_test_min_num_blocks_goal 10 \
+#     --pddl_blocks_procedural_test_max_num_blocks_goal 13"
+
+# # Easy delivery environment.
+# ENV_EXPERIMENT_ID="easy-delivery"
+# ENV="pddl_delivery_procedural_tasks"
+# ENV_FLAGS="--pddl_delivery_procedural_train_min_num_locs 3 \
+#     --pddl_delivery_procedural_train_max_num_locs 5 \
+#     --pddl_delivery_procedural_train_min_want_locs 1 \
+#     --pddl_delivery_procedural_train_max_want_locs 2 \
+#     --pddl_delivery_procedural_test_min_num_locs 4 \
+#     --pddl_delivery_procedural_test_max_num_locs 6 \
+#     --pddl_delivery_procedural_test_min_want_locs 2 \
+#     --pddl_delivery_procedural_test_max_want_locs 3 \
+#     --pddl_delivery_procedural_test_max_extra_newspapers 1"
+
+# # Medium delivery environment.
+# ENV_EXPERIMENT_ID="medium-delivery"
+# ENV="pddl_delivery_procedural_tasks"
+# ENV_FLAGS="--pddl_delivery_procedural_train_min_num_locs 4 \
+#     --pddl_delivery_procedural_train_max_num_locs 7 \
+#     --pddl_delivery_procedural_train_min_want_locs 1 \
+#     --pddl_delivery_procedural_train_max_want_locs 3 \
+#     --pddl_delivery_procedural_test_min_num_locs 17 \
+#     --pddl_delivery_procedural_test_max_num_locs 23 \
+#     --pddl_delivery_procedural_test_min_want_locs 11 \
+#     --pddl_delivery_procedural_test_max_want_locs 16 \
+#     --pddl_delivery_procedural_test_max_extra_newspapers 5"
+
+# # Hard delivery environment.
+# ENV_EXPERIMENT_ID="hard-delivery"
+# ENV="pddl_delivery_procedural_tasks"
+# ENV_FLAGS="--pddl_delivery_procedural_train_min_num_locs 5 \
+#     --pddl_delivery_procedural_train_max_num_locs 10 \
+#     --pddl_delivery_procedural_train_min_want_locs 2 \
+#     --pddl_delivery_procedural_train_max_want_locs 4 \
+#     --pddl_delivery_procedural_test_min_num_locs 31 \
+#     --pddl_delivery_procedural_test_max_num_locs 40 \
+#     --pddl_delivery_procedural_test_min_want_locs 20 \
+#     --pddl_delivery_procedural_test_max_want_locs 30 \
+#     --pddl_delivery_procedural_test_max_extra_newspapers 10"
 
 # Set this to the private key that has access to the machines.
 SSH_PRIVATE_KEY_PATH="~/.ssh/cloud.key"
@@ -30,7 +113,16 @@ MACHINES=(
 )
 
 # The main command (without the seed specified).
-CMD="python3.8 src/main.py --env ${ENV} --approach ${APPROACH} --experiment_id ${EXPERIMENT_ID} ${OTHER_FLAGS}"
+EXPERIMENT_ID="${ENV_EXPERIMENT_ID}__${APPROACH_EXPERIMENT_ID}"
+CMD="python3.8 src/main.py --env ${ENV} --approach ${APPROACH} \
+    --experiment_id $EXPERIMENT_ID \
+    ${FLAGS} ${ENV_FLAGS} ${APPROACH_FLAGS} --debug"
+
+# Run this script with either "launch" or "download" as the first argument.
+if [[ $1 != "launch" && $1 != "download" ]]; then
+echo "ERROR: Run this script with either 'launch' or 'download'."
+exit
+fi
 
 # The main loop.
 SEED=$START_SEED
@@ -88,3 +180,5 @@ fi
 SEED=$(($SEED+1))
 
 done
+
+echo "Finished $1 for experiment: ${EXPERIMENT_ID}."
