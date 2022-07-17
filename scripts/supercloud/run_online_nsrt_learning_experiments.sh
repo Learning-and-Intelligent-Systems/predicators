44a5020207ab580adbd205e9a0ffba287493a439
Tom Silver <tomssilver@gmail.com>
2022-07-17 12:22:42 -0400

    some important flag updates

--- scripts/supercloud/run_online_nsrt_learning_experiments.sh
+++ scripts/supercloud/run_online_nsrt_learning_experiments.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+MAX_TRANSITIONS=10000  # want this to be stop signal
+CYCLES=1000000  # way too many cycles
+
 FILE="scripts/supercloud/submit_supercloud_job.py"
 ENVS=(
     "cover"
@@ -17,8 +20,11 @@ EXPLORERS=(
 COMMON_ARGS=" --approach online_nsrt_learning \
     --max_initial_demos 1 \
     --num_train_tasks 1000 \
+    --num_online_learning_cycles $CYCLES \
+    --online_learning_max_transitions $MAX_TRANSITIONS \
     --num_test_tasks 10 \
-    --min_data_for_nsrt 10"
+    --min_data_for_nsrt 10 \
+    --sesame_allow_noops False"
 
 for ENV in ${ENVS[@]}; do
     for EXPLORER in ${EXPLORERS[@]}; do
