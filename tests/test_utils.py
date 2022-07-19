470f1a1caa27e3bfb2139cfc6a21b2e5d352618e
Abraham-Mitchell <85500357+Abraham-Mitchell@users.noreply.github.com>
2022-07-19 14:36:01 -0400

    add early termination to hill climbing for PG3 (#1091)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2719,6 +2719,17 @@ def test_run_hill_climbing():
             8.0, float("inf"), 6.0, 5.0, 4.0, 3.0, 2.0, 1.0, 0.0
         ]
 
+    # Test early_termination_heuristic_thresh with very high value.
+    initial_state = (0, 0)
+    state_sequence, action_sequence, heuristics = utils.run_hill_climbing(
+        initial_state,
+        _grid_check_goal_fn,
+        _grid_successor_fn,
+        _grid_heuristic_fn,
+        early_termination_heuristic_thresh=10000000)
+    assert state_sequence == [(0, 0)]
+    assert not action_sequence
+
 
 def test_run_policy_guided_astar():
     """Tests for run_policy_guided_astar()."""
