0a925a2be8e665e9a1b3156ccf8b4e62b85c2b81
Tom Silver <tomssilver@gmail.com>
2022-07-20 19:37:11 -0400

    refactor pg4 and bilevel planning

--- src/approaches/pg4_approach.py
+++ src/approaches/pg4_approach.py
@@ -12,15 +12,16 @@ Example command line:
 """
 from __future__ import annotations
 
-from typing import Callable
+from typing import Callable, List, Set, Tuple
 
 from predicators.src import utils
-from predicators.src.approaches import ApproachFailure, ApproachTimeout
+from predicators.src.approaches.bilevel_planning_approach import \
+    BilevelPlanningApproach
 from predicators.src.approaches.pg3_approach import PG3Approach
-from predicators.src.planning import PlanningFailure, PlanningTimeout, \
-    sesame_plan
+from predicators.src.planning import sesame_plan
 from predicators.src.settings import CFG
-from predicators.src.structs import AbstractPolicy, Action, State, Task
+from predicators.src.structs import NSRT, AbstractPolicy, Action, Metrics, \
+    Predicate, State, Task, _Option
 
 
 class PG4Approach(PG3Approach):
@@ -32,41 +33,30 @@ class PG4Approach(PG3Approach):
         return "pg4"
 
     def _solve(self, task: Task, timeout: int) -> Callable[[State], Action]:
+        # This is a rare case where protected access seems like the best thing
+        # to do, because this approach subclasses from BilevelPlanningApproach,
+        # but it's not the direct child, so we can't use super().
+        return BilevelPlanningApproach._solve(self, task, timeout)  # pylint: disable=protected-access
+
+    def _run_sesame_plan(self, task: Task, nsrts: Set[NSRT],
+                         preds: Set[Predicate], timeout: float,
+                         seed: int) -> Tuple[List[_Option], Metrics]:
         """Generates a plan choosing the best skeletons generated from policy-
         based skeletons and primitive successors."""
-        seed = self._seed + self._num_calls
-        nsrts = self._get_current_nsrts()
-        preds = self._get_current_predicates()
-        abstract_policy: AbstractPolicy = lambda a,o,g: \
-                                            utils.query_ldl(self._current_ldl,
-                                            a, o, g)
+        abstract_policy: AbstractPolicy = lambda a, o, g: utils.query_ldl(
+            self._current_ldl, a, o, g)
         max_policy_guided_rollout = CFG.pg3_max_policy_guided_rollout
-        try:
-            plan, _ = sesame_plan(
-                task,
-                self._option_model,
-                nsrts,
-                preds,
-                self._types,
-                timeout,
-                seed,
-                self._task_planning_heuristic,
-                self._max_skeletons_optimized,
-                max_horizon=CFG.horizon,
-                abstract_policy=abstract_policy,
-                max_policy_guided_rollout=max_policy_guided_rollout,
-                allow_noops=CFG.sesame_allow_noops)
-        except PlanningFailure as e:
-            raise ApproachFailure(e.args[0], e.info)
-        except PlanningTimeout as e:
-            raise ApproachTimeout(e.args[0], e.info)
 
-        option_policy = utils.option_plan_to_policy(plan)
-
-        def _policy(s: State) -> Action:
-            try:
-                return option_policy(s)
-            except utils.OptionExecutionFailure as e:
-                raise ApproachFailure(e.args[0], e.info)
-
-        return _policy
+        return sesame_plan(task,
+                           self._option_model,
+                           nsrts,
+                           preds,
+                           self._types,
+                           timeout,
+                           seed,
+                           self._task_planning_heuristic,
+                           self._max_skeletons_optimized,
+                           max_horizon=CFG.horizon,
+                           abstract_policy=abstract_policy,
+                           max_policy_guided_rollout=max_policy_guided_rollout,
+                           allow_noops=CFG.sesame_allow_noops)
