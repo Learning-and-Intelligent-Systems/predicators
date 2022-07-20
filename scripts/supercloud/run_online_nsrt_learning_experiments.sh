838621fe1fd0ea4d48cee51ef8344c32fcb67c65
Tom Silver <tomssilver@gmail.com>
2022-07-20 08:48:09 -0400

    increase glib max goal size

--- scripts/supercloud/run_online_nsrt_learning_experiments.sh
+++ scripts/supercloud/run_online_nsrt_learning_experiments.sh
@@ -24,7 +24,8 @@ COMMON_ARGS=" --approach online_nsrt_learning \
     --max_num_steps_interaction_request 25 \
     --num_test_tasks 10 \
     --min_data_for_nsrt 10 \
-    --sesame_allow_noops False"
+    --sesame_allow_noops False \
+    --glib_max_goal_size 2"
 
 for ENV in ${ENVS[@]}; do
     for EXPLORER in ${EXPLORERS[@]}; do
