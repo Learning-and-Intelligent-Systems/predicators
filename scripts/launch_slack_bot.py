50674abd26960faedc3fbf2120de41cb40351c75
Rohan Chitnis <ronuchit@mit.edu>
2022-07-21 17:53:35 -0400

    move file

--- scripts/launch_slack_bot.py
+++ scripts/launch_slack_bot.py
@@ -1,7 +1,7 @@
 """Predicatorobot slack bot code.
 
 To launch on supercloud (e.g., after a downtime has ended): `rm -f
-nohup.out && nohup python launch_slack_bot.py &`
+nohup.out && nohup python scripts/launch_slack_bot.py &`
 """
 
 import abc
