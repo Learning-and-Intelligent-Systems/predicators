640b388f05c98d8617c91f77b0e1dc08c6ac70f5
Tom Silver <tomssilver@gmail.com>
2022-07-25 15:37:07 -0400

    hacks on hacks on hacks

--- src/approaches/bilevel_planning_approach.py
+++ src/approaches/bilevel_planning_approach.py
@@ -70,6 +70,10 @@ class BilevelPlanningApproach(BaseApproach):
 
         For example, PG4 inserts an abstract policy into kwargs.
         """
+        if "use_visited_state_set" in kwargs:
+            use_visited_state_set = kwargs.pop("use_visited_state_set")
+        else:
+            use_visited_state_set = CFG.sesame_use_visited_state_set
         try:
             plan, metrics = sesame_plan(
                 task,
@@ -83,7 +87,7 @@ class BilevelPlanningApproach(BaseApproach):
                 self._max_skeletons_optimized,
                 max_horizon=CFG.horizon,
                 allow_noops=CFG.sesame_allow_noops,
-                use_visited_state_set=CFG.sesame_use_visited_state_set,
+                use_visited_state_set=use_visited_state_set,
                 **kwargs)
         except PlanningFailure as e:
             raise ApproachFailure(e.args[0], e.info)
