4db7a6bfa1553c9c586cb84f30cbe480b0e23064
Tom Silver <tomssilver@gmail.com>
2022-08-15 09:28:21 -0400

    joint tests

---
+++ tests/pybullet_helpers/test_joint.py
@@ -0,0 +1,53 @@
+"""Tests for joint PyBullet helper utilities."""
+
+import pybullet as p
+
+from predicators.src.pybullet_helpers.joint import JointInfo
+
+
+def test_joint_info():
+    """Tests for JointInfo()."""
+
+    fixed_joint_info = JointInfo(jointIndex=0,
+                                 jointName="fake-fixed-joint",
+                                 jointType=p.JOINT_FIXED,
+                                 qIndex=0,
+                                 uIndex=0,
+                                 flags=0,
+                                 jointDamping=0.1,
+                                 jointFriction=0.1,
+                                 jointLowerLimit=0.0,
+                                 jointUpperLimit=0.0,
+                                 jointMaxForce=1.0,
+                                 jointMaxVelocity=1.0,
+                                 linkName="fake-link",
+                                 jointAxis=(0.0, 0.0, 0.0),
+                                 parentFramePos=(0.0, 0.0, 0.0),
+                                 parentFrameOrn=(0.0, 0.0, 0.0, 1.0),
+                                 parentIndex=-1)
+
+    assert fixed_joint_info.is_fixed
+    assert not fixed_joint_info.is_circular
+    assert not fixed_joint_info.is_movable
+
+    circular_joint_info = JointInfo(jointIndex=0,
+                                    jointName="fake-circular-joint",
+                                    jointType=p.JOINT_REVOLUTE,
+                                    qIndex=0,
+                                    uIndex=0,
+                                    flags=0,
+                                    jointDamping=0.1,
+                                    jointFriction=0.1,
+                                    jointLowerLimit=1.0,
+                                    jointUpperLimit=0.0,
+                                    jointMaxForce=1.0,
+                                    jointMaxVelocity=1.0,
+                                    linkName="fake-link",
+                                    jointAxis=(0.0, 0.0, 0.0),
+                                    parentFramePos=(0.0, 0.0, 0.0),
+                                    parentFrameOrn=(0.0, 0.0, 0.0, 1.0),
+                                    parentIndex=-1)
+
+    assert not circular_joint_info.is_fixed
+    assert circular_joint_info.is_circular
+    assert circular_joint_info.is_movable
