5c21722638e57877f9621a03747dabd659532bc7
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-08-19 16:21:42 -0400

    fixes setup

--- setup.py
+++ setup.py
@@ -1,24 +1,22 @@
-"""Setup script.
+"""Setup script."""
 
-Usage examples:     pip install -e .     pip install -e .[develop]
-"""
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="predicators",
-    version="0.1.0",
-    install_requires=[
-        "numpy>=1.22.3", "pytest", "gym==0.21.0", "matplotlib", "imageio",
-        "imageio-ffmpeg", "pandas", "torch", "scipy", "tabulate", "dill",
-        "pyperplan", "pathos", "requests", "slack_bolt", "pybullet>=3.2.0",
-        "sklearn", "pyqt5", "graphlib-backport", "openai", "pyyaml",
-        "types-PyYAML"
-    ],
-    include_package_data=True,
-    extras_require={
-        "develop": [
-            "pytest-cov>=2.12.1", "pytest-pylint>=0.18.0", "yapf==0.32.0",
-            "docformatter", "isort",
-            "mypy@git+https://github.com/python/mypy.git@9bd651758e8ea2494837814092af70f8d9e6f7a1"
-        ]
-    })
+setup(name="predicators",
+      version="0.1.0",
+      install_requires=[
+          "numpy>=1.22.3", "pytest", "gym==0.21.0", "matplotlib", "imageio",
+          "imageio-ffmpeg", "pandas", "torch", "scipy", "tabulate", "dill",
+          "pyperplan", "pathos", "requests", "slack_bolt", "pybullet>=3.2.0",
+          "sklearn", "pyqt5", "graphlib-backport", "openai", "pyyaml",
+          "types-PyYAML"
+      ],
+      include_package_data=True,
+      extras_require={
+          "develop": [
+              "pytest-cov>=2.12.1", "pytest-pylint>=0.18.0", "yapf==0.32.0",
+              "docformatter", "isort",
+              "mypy@git+https://github.com/python/mypy.git@9bd651758e8ea2494" +
+              "837814092af70f8d9e6f7a1"
+          ]
+      })
