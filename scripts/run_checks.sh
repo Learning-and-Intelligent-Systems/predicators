ca81d7de7c4642b0d4911c917e0fad36f5d52179
Nishanth Kumar <nishanth.kumar20@gmail.com>
2022-08-29 12:05:17 -0400

    updates run_checks script to match new repo re-org

--- scripts/run_checks.sh
+++ scripts/run_checks.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo "Running autoformatting."
-yapf -i -r --style .style.yapf . && docformatter -i -r . && isort .
+yapf -i -r --style .style.yapf --exclude '**/third_party' predicators
+yapf -i -r --style .style.yapf scripts
+yapf -i -r --style .style.yapf tests
+docformatter -i -r . --exclude venv predicators/third_party
+isort .
 echo "Autoformatting complete."
 
 echo "Running type checking."
