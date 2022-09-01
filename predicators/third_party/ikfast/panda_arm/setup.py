6d28964deaec01abd0dc1cc09d25d33d0d964752
William Shen <william.w.y.shen@gmail.com>
2022-09-01 11:55:33 -0400

    Make IKfast compilation work (99.96% cov)
    - Very annoying, just made symlink to parent compile file

--- predicators/third_party/ikfast/panda_arm/setup.py
+++ predicators/third_party/ikfast/panda_arm/setup.py
@@ -3,7 +3,7 @@ by Caelan Garrett (https://github.com/caelan/pybullet-planning/)."""
 
 import sys
 
-from predicators.third_party.ikfast.compile import compile_ikfast
+from compile import compile_ikfast
 
 
 def main():
