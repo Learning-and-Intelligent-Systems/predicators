5769beb394fad4a2a1cd9a9be4907af11b83e6bd
Tom Silver <tomssilver@gmail.com>
2022-09-06 21:47:40 -0400

    try again

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@ setup(name="predicators",
           "numpy>=1.22.3", "pytest", "gym==0.21.0", "matplotlib", "imageio",
           "imageio-ffmpeg", "pandas", "torch", "scipy", "tabulate", "dill",
           "pyperplan", "pathos", "requests", "slack_bolt", "pybullet>=3.2.0",
-          "sklearn", "graphlib-backport", "openai", "pyyaml", "types-PyYAML"
+          "sklearn", "graphlib-backport", "openai", "pyyaml", "pylint==2.14.5",
+          "types-PyYAML"
       ],
       include_package_data=True,
       extras_require={
           "develop": [
-              "pytest-cov>=2.12.1", "pylint==2.14.5", "pytest-pylint>=0.18.0",
-              "yapf==0.32.0", "docformatter", "isort",
+              "pytest-cov>=2.12.1", "pytest-pylint>=0.18.0", "yapf==0.32.0",
+              "docformatter", "isort",
               "mypy@git+https://github.com/python/mypy.git@9bd651758e8ea2494" +
               "837814092af70f8d9e6f7a1"
           ]
