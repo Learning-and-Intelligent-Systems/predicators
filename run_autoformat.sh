287e9d1b3c09f0c278ef22b43b6358736c52e8ab
Nishanth Kumar <NishanthJKumar@users.noreply.github.com>
2022-09-07 10:14:56 -0400

    fixing github checks (#1196) (#23)
    Co-authored-by: Tom Silver <tomsilver@users.noreply.github.com>

--- run_autoformat.sh
+++ run_autoformat.sh
@@ -2,5 +2,6 @@
 yapf -i -r --style .style.yapf --exclude '**/third_party' predicators
 yapf -i -r --style .style.yapf scripts
 yapf -i -r --style .style.yapf tests
+yapf -i -r --style .style.yapf setup.py
 docformatter -i -r . --exclude venv predicators/third_party
 isort .
