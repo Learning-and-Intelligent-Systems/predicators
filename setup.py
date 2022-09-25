530eafe6029b3613d6ac2f03b85beb7f57ec2dcf
Tom Silver <tomssilver@gmail.com>
2022-09-25 16:50:14 -0400

    trying again

--- setup.py
+++ setup.py
@@ -5,11 +5,30 @@ setup(name="predicators",
       version="0.1.0",
       packages=find_packages(include=["predicators", "predicators.*"]),
       install_requires=[
-          "numpy>=1.22.3", "pytest", "gym==0.21.0", "matplotlib", "imageio",
-          "imageio-ffmpeg", "pandas", "torch", "scipy", "tabulate", "dill",
-          "pyperplan", "pathos", "requests", "slack_bolt", "pybullet>=3.2.0",
-          "sklearn", "graphlib-backport", "openai", "pyyaml", "pylint==2.14.5",
-          "types-PyYAML"
+          "numpy>=1.22.3",
+          "pytest",
+          "gym==0.21.0",
+          "matplotlib",
+          "imageio",
+          "imageio-ffmpeg",
+          "pandas",
+          "torch",
+          "scipy",
+          "tabulate",
+          "dill",
+          "pyperplan",
+          "pathos",
+          "requests",
+          "slack_bolt",
+          "pybullet>=3.2.0",
+          "sklearn",
+          "graphlib-backport",
+          "openai",
+          "pyyaml",
+          "pylint==2.14.5",
+          "types-PyYAML",
+          "lisdf@git+https://github.com/" +
+          "Learning-and-Intelligent-Systems/lisdf.git",
       ],
       include_package_data=True,
       extras_require={
@@ -21,7 +40,5 @@ setup(name="predicators",
               "isort",
               "mypy@git+https://github.com/python/mypy.git@9bd651758e8ea2494" +
               "837814092af70f8d9e6f7a1",
-              "lisdf@git+https://github.com/" +
-              "Learning-and-Intelligent-Systems/lisdf.git",
           ]
       })
