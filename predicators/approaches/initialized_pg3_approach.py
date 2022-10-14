628fc9deac156376d31d80340b5774c435e35963
Ryan Yang <ryanyang@mit.edu>
2022-10-14 15:50:36 -0400

    lint initialized pg3 comments

--- predicators/approaches/initialized_pg3_approach.py
+++ predicators/approaches/initialized_pg3_approach.py
@@ -1,4 +1,5 @@
-"""Policy-guided planning for generalized policy generation (PG3) with an initialized policy.
+"""Policy-guided planning for generalized policy generation (PG3) with an
+initialized policy.
 
 PG3 requires known STRIPS operators. The command below uses oracle operators,
 but it is also possible to use this approach with operators learned from
@@ -17,8 +18,10 @@ from predicators.approaches.pg3_approach import PG3Approach
 from predicators.settings import CFG
 from predicators.structs import LiftedDecisionList
 
+
 class InitializedPG3Approach(PG3Approach):
-    """Policy-guided planning for generalized policy generation (PG3) with initialized policy."""
+    """Policy-guided planning for generalized policy generation (PG3) with
+    initialized policy."""
 
     @classmethod
     def get_name(cls) -> str:
@@ -26,8 +29,7 @@ class InitializedPG3Approach(PG3Approach):
 
     @staticmethod
     def _get_policy_search_initial_ldl() -> LiftedDecisionList:
-        # Initialize with intialized policy frmo file. 
+        # Initialize with intialized policy from file.
         assert CFG.pg3_init_policy
         with open(CFG.pg3_init_policy, "rb") as f:
             return pkl.load(f)
-
