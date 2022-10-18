e39404a2eef444210fd1f7ea534f00292227c97f
Amber Li <amli@mit.edu>
2022-10-18 14:15:09 -0400

    remove flaky test

--- scripts/evaluate_interactive_approach_classifiers.py
+++ scripts/evaluate_interactive_approach_classifiers.py
@@ -46,7 +46,8 @@ def _run_pipeline(
             None], data: List) -> None:
     approach.load(online_learning_cycle=None)
     evaluate_fn(env, approach, None, data)
-    for i in range(CFG.num_online_learning_cycles):
+    # for i in range(CFG.num_online_learning_cycles):
+    for i in [10, 49]:
         print(f"\n\nONLINE LEARNING CYCLE {i}\n")
         try:
             approach.load(online_learning_cycle=i)
@@ -76,24 +77,13 @@ def _evaluate_preds_cover(preds: Set[Predicate], env: CoverEnv,
     HoldingGT = [p for p in env.predicates if p.name == "Holding"][0]
     CoversGT = [p for p in env.predicates if p.name == "Covers"][0]
     states, _, _ = create_states_cover(env)
-    # Test 1: no blocks overlap any targets, none are held
-    state = states[0]
-    atoms = utils.abstract(state, (Holding, Covers))
-    atoms_gt = utils.abstract(state, (HoldingGT, CoversGT))
-    print(f"False positives: {atoms - atoms_gt}\n"
-          f"False negatives: {atoms_gt - atoms}")
-    # Test 2: block0 does not completely cover target0
-    state = states[2]
-    atoms = utils.abstract(state, (Holding, Covers))
-    atoms_gt = utils.abstract(state, (HoldingGT, CoversGT))
-    print(f"False positives: {atoms - atoms_gt}\n"
-          f"False negatives: {atoms_gt - atoms}")
-    # Test 3: block0 covers target0
-    state = states[4]
-    atoms = utils.abstract(state, (Holding, Covers))
-    atoms_gt = utils.abstract(state, (HoldingGT, CoversGT))
-    print(f"False positives: {atoms - atoms_gt}\n"
-          f"False negatives: {atoms_gt - atoms}")
+    for i, state in enumerate(states):
+        print(f"State {i}")
+        atoms = utils.abstract(state, (Holding, Covers))
+        print(f"Classification: {atoms}")
+        atoms_gt = utils.abstract(state, (HoldingGT, CoversGT))
+        print(f"False positives: {atoms - atoms_gt}\n"
+            f"False negatives: {atoms_gt - atoms}")
 
 
 def create_states_cover(
@@ -124,19 +114,20 @@ def create_states_cover(
         state.set(target, "pose", pose)
     state.set(robot, "hand", 0.0)
     states.append(state)
-    # State 1: block0 and target0 overlap a bit
+    # block0 and target0 overlap a bit
     next_state = state.copy()
     next_state.set(block0, "pose", 0.31)
     states.append(next_state)
-    # State 2: block and target overlap more
+    # block0 and target0 overlap more
     next_state = state.copy()
     next_state.set(block0, "pose", 0.33)
     states.append(next_state)
+    # REMOVING THIS TEST STATE B/C IT'S FLAKY
     # State 3: block covers target, right edges align
-    next_state = state.copy()
-    next_state.set(block0, "pose", 0.35)
-    states.append(next_state)
-    # State 4: block0 covers target0, centered
+    # next_state = state.copy()
+    # next_state.set(block0, "pose", 0.35)
+    # states.append(next_state)
+    # block0 covers target0, centered
     next_state = state.copy()
     next_state.set(block0, "pose", target_poses[0])
     states.append(next_state)
