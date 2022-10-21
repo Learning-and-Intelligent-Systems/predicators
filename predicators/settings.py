d6759eb125aff817c183b9217bcc2fc55b8d6f1c
Tom Silver <tomssilver@gmail.com>
2022-10-21 06:32:23 -0400

    merge

--- predicators/settings.py
+++ predicators/settings.py
@@ -215,6 +215,7 @@ class GlobalSettings:
     random_options_max_tries = 100
 
     # option model parameters
+    option_model_terminate_on_repeat = True
     option_model_use_gui = False
 
     # parameters for abstract GNN approach
@@ -353,10 +354,6 @@ class GlobalSettings:
 
     # option learning parameters
     option_learning_action_converter = "identity"
-
-    # option model parameters
-    option_model_terminate_on_repeat = True
-    option_model_use_gui = False
 
     # interactive learning parameters
     interactive_num_ensemble_members = 10
