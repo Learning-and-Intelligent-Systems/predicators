012aeeff4ca6ae84f825d6b7d49a96e8e919f9d3
Tom Silver <tomssilver@gmail.com>
2022-10-31 17:10:32 -0400

    junk

--- scripts/blocks_perception.py
+++ scripts/blocks_perception.py
@@ -20,6 +20,11 @@ BLOCK_COLORS = {
 }
 COLOR_THRESH = 25
 
+CAMERA_TO_LINE = {
+    "left": LineSegment(1030, 700, 250, 230),
+    "right": LineSegment(120, 705, 975, 190),
+}
+
 
 def _show_image(img: Image, title: str) -> None:
     # plt.figure()
@@ -32,55 +37,49 @@ def _show_image(img: Image, title: str) -> None:
     cv2.destroyAllWindows()
 
 
-def parse_state_from_images(images: Sequence[Image], debug: bool = False) -> State:
+def parse_state_from_images(right_color_img: Image, left_color_img: Image,
+                            debug: bool = False) -> State:
     # Convert to RBG for cv2. 
-    images = [cv2.cvtColor(img, cv2.COLOR_BGR2RGB) for img in images]
+    right_color_img = cv2.cvtColor(right_color_img, cv2.COLOR_BGR2RGB)
+    left_color_img = cv2.cvtColor(left_color_img, cv2.COLOR_BGR2RGB)
 
     # Display raw images.
     if debug:
-        for i, img in enumerate(images):
-            _show_image(img, f"Original Image {i}")
+        _show_image(right_color_img, "Original Right Image")
+        _show_image(left_color_img, "Original Left Image")
     
-    states = [parse_state_from_image(img, debug=debug) for img in images]
+    right_state = parse_state_from_image(right_color_img, "right", debug=debug)
+    left_state = parse_state_from_image(left_color_img, "left", debug=debug)
+
     # TODO: Average states together.
-    return states[0]
+    return right_state
 
 
-def parse_state_from_image(image: Image, debug: bool = False) -> State:
+def parse_state_from_image(image: Image, camera: str, debug: bool = False) -> State:
     # Find line that orients the table.
-    table_line = parse_table_line_from_image(image, debug=debug)
+    table_line = parse_table_line_from_image(image, camera, debug=debug)
 
 
-def parse_table_line_from_image(image: Image, debug: bool = False) -> LineSegment:
-    img_blur = cv2.GaussianBlur(image, (5, 5), 0)
+def parse_table_line_from_image(image: Image, camera: str, debug: bool = False) -> LineSegment:
     
-    color_to_mask = {}
-    for name, (r, g, b) in BLOCK_COLORS.items():
-        mean_color = np.array([b, g, r], dtype=np.uint8)
-        lower_color = mean_color - COLOR_THRESH
-        upper_color = mean_color + COLOR_THRESH
-        mask = cv2.inRange(img_blur, lower_color, upper_color)
-        color_to_mask[name] = mask
+    line = CAMERA_TO_LINE[camera]
 
-    for name, mask in color_to_mask.items():
-        full_mask = mask
-        for other_name in color_to_mask:
-            if name == other_name:
-                continue
-            other_mask = color_to_mask[other_name]
-            full_mask = cv2.bitwise_and(full_mask, cv2.bitwise_not(other_mask))
-        detections = cv2.bitwise_and(img_blur, img_blur, mask=full_mask)
-        if debug:
-            _show_image(detections, f"Detections for {name}")
+    if debug:
+        start_point = (line.x1, line.y1)
+        end_point = (line.x2, line.y2)
+        color = (255, 255, 255)
+        thickness = 5
+        line_image = cv2.line(image, start_point, end_point, color, thickness)
+        _show_image(line_image, f"Line for {camera}")
 
 
 if __name__ == "__main__":
     color_imgs_path = Path("~/Desktop/blocks-images/color/")
-    right_camera_id = 231122071283
-    left_camera_id = 231122071284
+    right_camera_id = 231122071284
+    left_camera_id = 231122071283
     img_id = 0
-    right_color_img_path = color_imgs_path / f"color-{img_id}-{left_camera_id}.png"
-    left_color_img_path = color_imgs_path / f"color-{img_id}-{right_camera_id}.png"
+    right_color_img_path = color_imgs_path / f"color-{img_id}-{right_camera_id}.png"
+    left_color_img_path = color_imgs_path / f"color-{img_id}-{left_camera_id}.png"
     right_color_img = iio.imread(right_color_img_path)
     left_color_img = iio.imread(left_color_img_path)
-    state = parse_state_from_images([right_color_img, left_color_img], debug=True)
+    state = parse_state_from_images(right_color_img, left_color_img, debug=True)
