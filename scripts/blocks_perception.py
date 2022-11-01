ab237d8eb21139f35e0485515a3592f118a2d1cd
Tom Silver <tomssilver@gmail.com>
2022-10-31 21:53:04 -0400

    wip

--- scripts/blocks_perception.py
+++ scripts/blocks_perception.py
@@ -20,7 +20,8 @@ BLOCK_COLORS = {
     "yellow": (160, 120, 60),
     "blue": (75, 100, 120),
 }
-COLOR_THRESH = 25
+INIT_COLOR_THRESH = 25
+COLOR_THRESH_INC = 5
 MARKER_THRESH = 500
 
 CAMERA_TO_LINE = {
@@ -109,41 +110,41 @@ def crop_from_bottom_row_line(image: Image, bottom_row_line: LineSegment, debug:
 def segment_image(image: Image, debug: bool = False) -> Image:
     img_blur = cv2.GaussianBlur(image, (3, 3), 0)
 
-    color_to_mask = {}
     for name, (r, g, b) in BLOCK_COLORS.items():
-        mean_color = np.array([b, g, r], dtype=np.uint8)
-        lower_color = mean_color - COLOR_THRESH
-        upper_color = mean_color + COLOR_THRESH
-        mask = cv2.inRange(img_blur, lower_color, upper_color)
-        color_to_mask[name] = mask
-
-    for name, mask in color_to_mask.items():
-        detected_mask = mask
-        # Suppress other colors
-        # for other_name in color_to_mask:
-        #     if name == other_name:
-        #         continue
-        #     other_mask = color_to_mask[other_name]
-        #     detected_mask = cv2.bitwise_and(detected_mask, cv2.bitwise_not(other_mask))
-        detections = cv2.bitwise_and(image, image, mask=detected_mask)
+        # Repeat until number of components goes up.
+        thresh = INIT_COLOR_THRESH
+        components = find_ccs_by_color(img_blur, r, g, b, thresh)
+        init_num_components = len(np.unique(components)) - 1
+        while True:
+            thresh += COLOR_THRESH_INC
+            new_components = find_ccs_by_color(img_blur, r, g, b, thresh)
+            new_num_components = len(np.unique(new_components)) - 1
+            if new_num_components != init_num_components:
+                if debug:
+                    print(f"Converged at thresh={thresh}")
+                break
+            components = new_components
         if debug:
-            # _show_image(detected_mask, f"Detections for {name}")
-            kernel = np.ones((3,3),np.uint8)
-            opening = cv2.morphologyEx(detected_mask, cv2.MORPH_OPEN, kernel, iterations = 2)
-            # Marker labelling
-            ret, markers = cv2.connectedComponents(opening)
-            # Find the markers above a threshold.
-            keep_markers = []
-            for marker in np.unique(markers):
-                if marker == 0:  # background
-                    continue
-                if np.sum(markers == marker) >= MARKER_THRESH:
-                    keep_markers.append(marker)
-            keep_marker_mask = np.zeros_like(markers)
-            for marker in keep_markers:
-                keep_marker_mask = keep_marker_mask | (markers == marker)
-            _show_image((255 * keep_marker_mask).astype(np.uint8), f"Markers for {name}")
+            _show_image((255 * (components > 0)).astype(np.uint8), f"Components for {name}")
         
+
+def find_ccs_by_color(img: Image, r: float, g: float, b: float, color_thresh: float) -> Image:
+    mean_color = np.array([b, g, r], dtype=np.uint8)
+    lower_color = mean_color - color_thresh
+    upper_color = mean_color + color_thresh
+    detected_mask = cv2.inRange(img, lower_color, upper_color)
+    detections = cv2.bitwise_and(img, img, mask=detected_mask)
+    kernel = np.ones((3,3),np.uint8)
+    opening = cv2.morphologyEx(detected_mask, cv2.MORPH_OPEN, kernel, iterations = 2)
+    # Marker labelling
+    ret, markers = cv2.connectedComponents(opening)
+    # Find the markers above a threshold size.
+    for marker in np.unique(markers):
+        if marker == 0:  # background
+            continue
+        if np.sum(markers == marker) < MARKER_THRESH:
+            markers[markers == marker] = 0
+    return markers
 
 
 if __name__ == "__main__":
@@ -155,4 +156,4 @@ if __name__ == "__main__":
     left_color_img_path = color_imgs_path / f"color-{img_id}-{left_camera_id}.png"
     right_color_img = iio.imread(right_color_img_path)
     left_color_img = iio.imread(left_color_img_path)
-    state = parse_state_from_images(right_color_img, left_color_img, debug=False)
+    state = parse_state_from_images(right_color_img, left_color_img, debug=True)
