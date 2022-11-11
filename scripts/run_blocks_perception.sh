26a9c1051518d983a54f880b8ed5f5cb51daf375
Tom Silver <tomssilver@gmail.com>
2022-11-11 12:00:24 -0500

    wip

--- scripts/run_blocks_perception.sh
+++ scripts/run_blocks_perception.sh
@@ -5,6 +5,7 @@ TASK_NUM="0"
 IMG_SUFFIX="821212060083.png"
 TASK_DIR=$DATA_DIR/tasks
 OUTPUT=$TASK_DIR/blocks-vision-task$TASK_NUM.json
+VIZ_PLANNING="True"
 
 mkdir -p $TASK_DIR
 
@@ -21,5 +22,11 @@ echo "Running planning."
 python predicators/main.py --env pybullet_blocks --approach oracle --seed 0 \
     --num_test_tasks 1 --blocks_test_task_json_dir $TASK_DIR \
     --pybullet_robot panda \
-    --option_model_use_gui True --option_model_name oracle \
-    --blocks_num_blocks_test [15] # just needs to be big enough
+    --option_model_use_gui $VIZ_PLANNING \
+    --option_model_name oracle --option_model_terminate_on_repeat False \
+    --blocks_num_blocks_test [15] # just needs to be an upper bound
+
+echo "Converting plan to LISDF."
+python scripts/eval_trajectory_to_lisdf.py \
+        --input eval_trajectories/pybullet_blocks__oracle__0________task1.traj \
+        --output /tmp/pybullet_blocks__oracle__0________task1.json
