825a0fd2d3d853379ddbb40e74892c715d2317f9
Tom Silver <tomssilver@gmail.com>
2022-11-12 17:11:08 -0500

    rename and add learned models version

--- scripts/run_blocks_real.sh
+++ scripts/run_blocks_real.sh
@@ -18,13 +18,26 @@ python scripts/run_blocks_perception.py \
         --intrinsics $DATA_DIR/intrinsics.json \
         --output $OUTPUT
 
-echo "Running planning."
+echo "Running planning with oracle models."
 python predicators/main.py --env pybullet_blocks --approach oracle --seed 0 \
     --num_test_tasks 1 --blocks_test_task_json_dir $TASK_DIR \
     --pybullet_robot panda \
     --option_model_use_gui $VIZ_PLANNING \
     --option_model_name oracle --option_model_terminate_on_repeat False \
     --blocks_num_blocks_test [15] # just needs to be an upper bound
+
+# echo "Running planning with learned models."
+# python predicators/main.py --env pybullet_blocks --seed 0 \
+#     --experiment_id panda_blocks_invent_allexclude \
+#     --approach grammar_search_invention \
+#     --excluded_predicates all \
+#     --load_approach --load_data \
+#     --num_train_tasks 0 --num_test_tasks 1 \
+#     --blocks_test_task_json_dir $TASK_DIR \
+#     --pybullet_robot panda \
+#     --option_model_use_gui $VIZ_PLANNING \
+#     --option_model_name oracle --option_model_terminate_on_repeat False \
+#     --blocks_num_blocks_test [15] # just needs to be an upper bound
 
 echo "Converting plan to LISDF."
 python scripts/eval_trajectory_to_lisdf.py \
