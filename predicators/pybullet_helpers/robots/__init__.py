a243edf9a9f681c2daf151ccb9a82781f40392a6
Tom Silver <tomsilver@users.noreply.github.com>
2022-11-12 14:22:42 -0500

    make pybullet robot ee orns global settings  (#1286)

--- predicators/pybullet_helpers/robots/__init__.py
+++ predicators/pybullet_helpers/robots/__init__.py
@@ -1,11 +1,12 @@
 """Handles the creation of robots."""
 from typing import Dict, Type
 
-from predicators.pybullet_helpers.geometry import Pose, Pose3D, Quaternion
+from predicators.pybullet_helpers.geometry import Pose, Pose3D
 from predicators.pybullet_helpers.robots.fetch import FetchPyBulletRobot
 from predicators.pybullet_helpers.robots.panda import PandaPyBulletRobot
 from predicators.pybullet_helpers.robots.single_arm import \
     SingleArmPyBulletRobot
+from predicators.settings import CFG
 
 # Note: these are static base poses which suffice for the current environments.
 _ROBOT_TO_BASE_POSE: Dict[str, Pose] = {
@@ -20,16 +21,17 @@ _ROBOT_TO_CLS: Dict[str, Type[SingleArmPyBulletRobot]] = {
 
 
 def create_single_arm_pybullet_robot(
-    robot_name: str,
-    physics_client_id: int,
-    ee_home_pose: Pose3D = (1.35, 0.6, 0.7),
-    ee_orientation: Quaternion = (0.0, 0.0, 0.0, 1.0)
+        robot_name: str,
+        physics_client_id: int,
+        ee_home_pose: Pose3D = (1.35, 0.6, 0.7),
 ) -> SingleArmPyBulletRobot:
     """Create a single-arm PyBullet robot."""
-    if robot_name in _ROBOT_TO_CLS:
+    robot_to_ee_orn = CFG.pybullet_robot_ee_orns[CFG.env]
+    if robot_name in _ROBOT_TO_CLS and robot_name in robot_to_ee_orn:
         assert robot_name in _ROBOT_TO_BASE_POSE, \
             f"Base pose not specified for robot {robot_name}."
         base_pose = _ROBOT_TO_BASE_POSE[robot_name]
+        ee_orientation = robot_to_ee_orn[robot_name]
         cls = _ROBOT_TO_CLS[robot_name]
         return cls(ee_home_pose,
                    ee_orientation,
