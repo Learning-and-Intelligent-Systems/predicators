a483b0167e139c35b1ad82472862350c6cdb9f35
Tom Silver <tomssilver@gmail.com>
2022-11-13 10:41:56 -0500

    coverage

--- tests/nsrt_learning/test_option_learning.py
+++ tests/nsrt_learning/test_option_learning.py
@@ -445,3 +445,28 @@ def test_kinematic_action_conversion():
         loaded_converter = pkl.load(f)
     reduced_action_arr3 = loaded_converter.env_to_reduced(env_action_arr2)
     assert np.allclose(reduced_action_arr, reduced_action_arr3)
+    # Test that open and closed finger values are properly converted.
+    robot.go_home()
+    env_open = env_action_arr.copy()
+    env_open[robot.left_finger_joint_idx] = robot.open_fingers
+    env_open[robot.right_finger_joint_idx] = robot.open_fingers
+    reduced_open = converter.env_to_reduced(env_open)
+    assert reduced_open[-1] == 1.0
+    env_closed = env_action_arr.copy()
+    env_closed[robot.left_finger_joint_idx] = robot.closed_fingers
+    env_closed[robot.right_finger_joint_idx] = robot.closed_fingers
+    reduced_closed = converter.env_to_reduced(env_closed)
+    assert reduced_closed[-1] == 0.0
+    # Test rounding.
+    env_open = env_action_arr.copy()
+    open_value = (0.8 * robot.open_fingers + 0.2 * robot.closed_fingers)
+    env_open[robot.left_finger_joint_idx] = open_value
+    env_open[robot.right_finger_joint_idx] = open_value
+    reduced_open = converter.env_to_reduced(env_open)
+    assert reduced_open[-1] == 1.0
+    env_closed = env_action_arr.copy()
+    closed_value = (0.2 * robot.open_fingers + 0.8 * robot.closed_fingers)
+    env_closed[robot.left_finger_joint_idx] = closed_value
+    env_closed[robot.right_finger_joint_idx] = closed_value
+    reduced_closed = converter.env_to_reduced(env_closed)
+    assert reduced_closed[-1] == 0.0
