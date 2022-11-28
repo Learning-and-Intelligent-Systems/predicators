84b2517fb1f0cf2267423d169a35541aab299517
Lilian Luong <lilianluong64@gmail.com>
2022-11-28 14:37:12 -0500

    [narrow passage] make passage wider in test_oracle_approach to make it faster

--- tests/approaches/test_oracle_approach.py
+++ tests/approaches/test_oracle_approach.py
@@ -146,6 +146,9 @@ EXTRA_ARGS_ORACLE_APPROACH["doors"] = [{
     "doors_min_obstacles_per_room": 1,
     "doors_max_obstacles_per_room": 1,
 }]
+EXTRA_ARGS_ORACLE_APPROACH["narrow_passage"] = [{
+    "narrow_passage_passage_width_padding": 0.075,
+}]
 EXTRA_ARGS_ORACLE_APPROACH["pddl_delivery_procedural_tasks"] = [
     {
         "pddl_delivery_procedural_train_min_num_locs": 2,
