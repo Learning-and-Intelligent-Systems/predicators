195c1dc797a399190fb3b1d62dcb12ba0da514d3
Tom Silver <tomssilver@gmail.com>
2022-11-30 20:28:45 -0500

    small cleanup

--- scripts/run_blocks_real.sh
+++ scripts/run_blocks_real.sh
@@ -2,17 +2,24 @@
 
 set -e
 
+# Constants.
 DATA_DIR="blocks_vision_data"
 TASK_NUM="0"
 SEED="0"
 IMG_SUFFIX="6blocks.png"
-TASK_DIR=$DATA_DIR/tasks
-OUTPUT=$TASK_DIR/blocks-vision-task$TASK_NUM.json
 VIZ_PLANNING="True"
+TASK_DIR="${DATA_DIR}/tasks"
 
+# Set up file paths.
+TASK_FILE="${TASK_DIR}/blocks-vision-task${TASK_NUM}.json"
+EVAL_TRAJ_FILE="eval_trajectories/pybullet_blocks__oracle__${SEED}________task1.traj"
+LISDF_PLAN_FILE="/tmp/pybullet_blocks__oracle__${SEED}________task1.json"
+FINAL_PLAN_FILE="/tmp/file_plan.json"
+
+# Start the pipeline.
 mkdir -p $TASK_DIR
 
-echo "Capturing images."
+# echo "Capturing images."
 python scripts/realsense_helpers.py \
         --rgb $DATA_DIR/color-$TASK_NUM-$IMG_SUFFIX \
         --depth $DATA_DIR/depth-$TASK_NUM-$IMG_SUFFIX
@@ -24,7 +31,7 @@ python scripts/run_blocks_perception.py \
         --goal $DATA_DIR/goal-$TASK_NUM.json \
         --extrinsics $DATA_DIR/extrinsics.json \
         --intrinsics $DATA_DIR/intrinsics.json \
-        --output $OUTPUT  # --debug_viz
+        --output $TASK_FILE  # --debug_viz
 
 echo "Running planning with oracle models."
 python predicators/main.py --env pybullet_blocks --approach oracle \
@@ -41,16 +48,16 @@ python predicators/main.py --env pybullet_blocks --approach oracle \
 
 echo "Converting plan to LISDF."
 python scripts/eval_trajectory_to_lisdf.py \
-        --input eval_trajectories/pybullet_blocks__oracle__${SEED}________task1.traj \
-        --output /tmp/pybullet_blocks__oracle__${SEED}________task1.json
+        --input $EVAL_TRAJ_FILE \
+        --output $LISDF_PLAN_FILE
 
 echo "Planning to reset the robot."
 python scripts/lisdf_plan_to_reset.py \
-        --lisdf /tmp/pybullet_blocks__oracle__${SEED}________task1.json \
-        --output /tmp/final_plan.json
+        --lisdf $LISDF_PLAN_FILE \
+        --output $FINAL_PLAN_FILE
 
 echo "Visualizing LISDF plan."
-python scripts/lisdf_pybullet_visualizer.py --lisdf /tmp/final_plan.json
+python scripts/lisdf_pybullet_visualizer.py --lisdf $FINAL_PLAN_FILE
 
 echo "To execute the LISDF plan on the real robot, run this command:"
-echo "panda-client execute_lisdf_plan /tmp/final_plan.json"
+echo "panda-client execute_lisdf_plan ${FINAL_PLAN_FILE}"
