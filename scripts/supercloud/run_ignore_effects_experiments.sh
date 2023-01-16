487ebddc32116d29a24d166d75dc56ce60cddd7f
Nishanth Kumar <nishanth.kumar20@gmail.com>
2023-01-16 18:41:21 -0500

    modifies run ignore effects script to include loft with random replays

--- scripts/supercloud/run_ignore_effects_experiments.sh
+++ scripts/supercloud/run_ignore_effects_experiments.sh
@@ -8,10 +8,9 @@ FILE="scripts/supercloud/submit_supercloud_job.py"
 # RUN_LOAD_EXPERIMENTS=false, then rerun with RUN_LOAD_EXPERIMENTS=true.
 RUN_LOAD_EXPERIMENTS=false
 ALL_ENVS=(
-    "repeated_nextto_single_option"
-    "repeated_nextto_ambiguous"
-    "repeated_nextto_painting"
-    "screws"
+    # "repeated_nextto_single_option"
+    # "repeated_nextto_painting"
+    # "screws"
     "painting"
     "satellites"
     "satellites_simple"
@@ -33,21 +32,23 @@ for ENV in ${ALL_ENVS[@]}; do
             # Model-free GNN option policy baseline.
             python $FILE $COMMON_ARGS --experiment_id ${ENV}_gnn_modelfree_${DEMOS}demo --load_experiment_id ${ENV}_gnn_shooting_${DEMOS}demo --approach gnn_option_policy --num_train_tasks ${DEMOS} --gnn_option_policy_solve_with_shooting False --load_approach --load_data
         else
-            # PNAD search approach with various numbers of demonstrations.
-            python $FILE $COMMON_ARGS --experiment_id ${ENV}_pnadsearch_${DEMOS}demo --approach nsrt_learning --strips_learner pnad_search --num_train_tasks ${DEMOS}
-            # Main backchaining approach with various numbers of demonstrations.
-            python $FILE $COMMON_ARGS --experiment_id ${ENV}_backchaining_${DEMOS}demo --approach nsrt_learning --strips_learner backchaining --num_train_tasks ${DEMOS}
-            # Cluster-and-intersect (RLDM) baseline. Although it is guaranteed to
-            # preserve harmlessness, we disable the check because it takes a long
-            # time (since the operators have high arity).
-            python $FILE $COMMON_ARGS --experiment_id ${ENV}_cluster_and_intersect_${DEMOS}demo --approach nsrt_learning --strips_learner cluster_and_intersect --num_train_tasks ${DEMOS} --disable_harmlessness_check True
-            # LOFT baseline. Same note on harmlessness as for cluster-and-intersect.
-            python $FILE $COMMON_ARGS --experiment_id ${ENV}_cluster_and_search_${DEMOS}demo --approach nsrt_learning --strips_learner cluster_and_search --num_train_tasks ${DEMOS} --disable_harmlessness_check True
-            # Prediction error baseline that optimizes via hill climbing. Not
-            # guaranteed to preserve harmlessness.
-            python $FILE $COMMON_ARGS --experiment_id ${ENV}_pred_error_${DEMOS}demo --approach nsrt_learning --strips_learner cluster_and_intersect_sideline_prederror --num_train_tasks ${DEMOS} --disable_harmlessness_check True
-            # Model-based GNN option policy baseline.
-            python $FILE $COMMON_ARGS --experiment_id ${ENV}_gnn_shooting_${DEMOS}demo --approach gnn_option_policy --num_train_tasks ${DEMOS}
+            # # PNAD search approach with various numbers of demonstrations.
+            # python $FILE $COMMON_ARGS --experiment_id ${ENV}_pnadsearch_${DEMOS}demo --approach nsrt_learning --strips_learner pnad_search --num_train_tasks ${DEMOS}
+            # # Main backchaining approach with various numbers of demonstrations.
+            # python $FILE $COMMON_ARGS --experiment_id ${ENV}_backchaining_${DEMOS}demo --approach nsrt_learning --strips_learner backchaining --num_train_tasks ${DEMOS}
+            # # Cluster-and-intersect (RLDM) baseline. Although it is guaranteed to
+            # # preserve harmlessness, we disable the check because it takes a long
+            # # time (since the operators have high arity).
+            # python $FILE $COMMON_ARGS --experiment_id ${ENV}_cluster_and_intersect_${DEMOS}demo --approach nsrt_learning --strips_learner cluster_and_intersect --num_train_tasks ${DEMOS} --disable_harmlessness_check True
+            # # LOFT baseline without random replays. Same note on harmlessness as for cluster-and-intersect.
+            # python $FILE $COMMON_ARGS --experiment_id ${ENV}_cluster_and_search_${DEMOS}demo --approach nsrt_learning --strips_learner cluster_and_search --num_train_tasks ${DEMOS} --disable_harmlessness_check True
+            # LOFT baseline with random replays. Same note on harmlessness as for cluster-and-intersect.
+            python $FILE $COMMON_ARGS --experiment_id ${ENV}_cluster_and_search_random_replays_${DEMOS}demo --approach nsrt_learning --strips_learner cluster_and_search --num_train_tasks ${DEMOS} --disable_harmlessness_check True --offline_data_method demo+replay --sesame_allow_noops False --offline_data_num_replays 2500
+            # # Prediction error baseline that optimizes via hill climbing. Not
+            # # guaranteed to preserve harmlessness.
+            # python $FILE $COMMON_ARGS --experiment_id ${ENV}_pred_error_${DEMOS}demo --approach nsrt_learning --strips_learner cluster_and_intersect_sideline_prederror --num_train_tasks ${DEMOS} --disable_harmlessness_check True
+            # # Model-based GNN option policy baseline.
+            # python $FILE $COMMON_ARGS --experiment_id ${ENV}_gnn_shooting_${DEMOS}demo --approach gnn_option_policy --num_train_tasks ${DEMOS}
         fi
     done
 done
