e51b2e1ee2f99be06abdc989b57692b64803fc93
Tom Silver <tomssilver@gmail.com>
2023-02-24 16:38:05 -0500

    blocks

---
+++ predicators/ground_truth_models/blocks/__init__.py
@@ -0,0 +1,5 @@
+"""Ground-truth models for cover environment and variants."""
+
+from .nsrts import BlocksGroundTruthNSRTFactory
+
+__all__ = ["BlocksGroundTruthNSRTFactory"]
---
+++ predicators/ground_truth_models/blocks/nsrts.py
@@ -0,0 +1,146 @@
+"""Ground-truth NSRTs for the blocks environment."""
+
+from typing import Dict, Sequence, Set
+
+import numpy as np
+
+from predicators.ground_truth_models import GroundTruthNSRTFactory
+from predicators.settings import CFG
+from predicators.structs import NSRT, Array, GroundAtom, LiftedAtom, Object, \
+    ParameterizedOption, Predicate, State, Type, Variable
+from predicators.utils import null_sampler
+
+
+class BlocksGroundTruthNSRTFactory(GroundTruthNSRTFactory):
+    """Ground-truth NSRTs for the blocks environment."""
+
+    @classmethod
+    def get_env_names(cls) -> Set[str]:
+        return {"blocks", "pybullet_blocks", "blocks_clear"}
+
+    @staticmethod
+    def get_nsrts(env_name: str, types: Dict[str, Type],
+                  predicates: Dict[str, Predicate],
+                  options: Dict[str, ParameterizedOption]) -> Set[NSRT]:
+        # Types
+        block_type = types["block"]
+        robot_type = types["robot"]
+
+        # Predicates
+        On = predicates["On"]
+        OnTable = predicates["OnTable"]
+        GripperOpen = predicates["GripperOpen"]
+        Holding = predicates["Holding"]
+        Clear = predicates["Clear"]
+
+        # Options
+        Pick = options["Pick"]
+        Stack = options["Stack"]
+        PutOnTable = options["PutOnTable"]
+
+        nsrts = set()
+
+        # PickFromTable
+        block = Variable("?block", block_type)
+        robot = Variable("?robot", robot_type)
+        parameters = [block, robot]
+        option_vars = [robot, block]
+        option = Pick
+        preconditions = {
+            LiftedAtom(OnTable, [block]),
+            LiftedAtom(Clear, [block]),
+            LiftedAtom(GripperOpen, [robot])
+        }
+        add_effects = {LiftedAtom(Holding, [block])}
+        delete_effects = {
+            LiftedAtom(OnTable, [block]),
+            LiftedAtom(Clear, [block]),
+            LiftedAtom(GripperOpen, [robot])
+        }
+
+        pickfromtable_nsrt = NSRT("PickFromTable", parameters,
+                                  preconditions, add_effects, delete_effects,
+                                  set(), option, option_vars, null_sampler)
+        nsrts.add(pickfromtable_nsrt)
+
+        # Unstack
+        block = Variable("?block", block_type)
+        otherblock = Variable("?otherblock", block_type)
+        robot = Variable("?robot", robot_type)
+        parameters = [block, otherblock, robot]
+        option_vars = [robot, block]
+        option = Pick
+        preconditions = {
+            LiftedAtom(On, [block, otherblock]),
+            LiftedAtom(Clear, [block]),
+            LiftedAtom(GripperOpen, [robot])
+        }
+        add_effects = {
+            LiftedAtom(Holding, [block]),
+            LiftedAtom(Clear, [otherblock])
+        }
+        delete_effects = {
+            LiftedAtom(On, [block, otherblock]),
+            LiftedAtom(Clear, [block]),
+            LiftedAtom(GripperOpen, [robot])
+        }
+        unstack_nsrt = NSRT("Unstack", parameters, preconditions, add_effects,
+                            delete_effects, set(), option, option_vars,
+                            null_sampler)
+        nsrts.add(unstack_nsrt)
+
+        # Stack
+        block = Variable("?block", block_type)
+        otherblock = Variable("?otherblock", block_type)
+        robot = Variable("?robot", robot_type)
+        parameters = [block, otherblock, robot]
+        option_vars = [robot, otherblock]
+        option = Stack
+        preconditions = {
+            LiftedAtom(Holding, [block]),
+            LiftedAtom(Clear, [otherblock])
+        }
+        add_effects = {
+            LiftedAtom(On, [block, otherblock]),
+            LiftedAtom(Clear, [block]),
+            LiftedAtom(GripperOpen, [robot])
+        }
+        delete_effects = {
+            LiftedAtom(Holding, [block]),
+            LiftedAtom(Clear, [otherblock])
+        }
+
+        stack_nsrt = NSRT("Stack", parameters, preconditions, add_effects,
+                          delete_effects, set(), option, option_vars,
+                          null_sampler)
+        nsrts.add(stack_nsrt)
+
+        # PutOnTable
+        block = Variable("?block", block_type)
+        robot = Variable("?robot", robot_type)
+        parameters = [block, robot]
+        option_vars = [robot]
+        option = PutOnTable
+        preconditions = {LiftedAtom(Holding, [block])}
+        add_effects = {
+            LiftedAtom(OnTable, [block]),
+            LiftedAtom(Clear, [block]),
+            LiftedAtom(GripperOpen, [robot])
+        }
+        delete_effects = {LiftedAtom(Holding, [block])}
+
+        def putontable_sampler(state: State, goal: Set[GroundAtom],
+                               rng: np.random.Generator,
+                               objs: Sequence[Object]) -> Array:
+            del state, goal, objs  # unused
+            # Note: normalized coordinates w.r.t. workspace.
+            x = rng.uniform()
+            y = rng.uniform()
+            return np.array([x, y], dtype=np.float32)
+
+        putontable_nsrt = NSRT("PutOnTable", parameters, preconditions,
+                               add_effects, delete_effects, set(), option,
+                               option_vars, putontable_sampler)
+        nsrts.add(putontable_nsrt)
+
+        return nsrts
