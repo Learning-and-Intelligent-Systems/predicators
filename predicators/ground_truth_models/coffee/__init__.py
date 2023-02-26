c1411ba0485984ed5ec33412464b4dbf2d5df483
Tom Silver <tomssilver@gmail.com>
2023-02-25 20:00:05 -0500

    Revert "coffee"
    This reverts commit bd6827337496ca5d291e4f040594e8f2c02d1328.

--- predicators/ground_truth_models/coffee/__init__.py
+++ predicators/ground_truth_models/coffee/__init__.py
@@ -1,6 +1,5 @@
 """Ground-truth models for coffee environment and variants."""
 
 from .nsrts import CoffeeGroundTruthNSRTFactory
-from .options import CoffeeGroundTruthOptionFactory
 
-__all__ = ["CoffeeGroundTruthNSRTFactory", "CoffeeGroundTruthOptionFactory"]
+__all__ = ["CoffeeGroundTruthNSRTFactory"]
