aa08fd49c36546bd8f0ee0ef9c7096ae9f4fa62b
Tom Silver <tomssilver@gmail.com>
2023-03-08 11:57:30 -0500

    nsrts

--- predicators/ground_truth_models/pybullet_shelf/nsrts.py
+++ predicators/ground_truth_models/pybullet_shelf/nsrts.py
@@ -4,10 +4,10 @@ from typing import Dict, Sequence, Set
 
 import numpy as np
 
-from predicators.envs.pybullet_shelf import PyBulletShelfEnv
 from predicators.ground_truth_models import GroundTruthNSRTFactory
 from predicators.structs import NSRT, Array, GroundAtom, LiftedAtom, Object, \
     ParameterizedOption, Predicate, State, Type, Variable
+from predicators.utils import null_sampler
 
 
 class PyBulletShelfGroundTruthNSRTFactory(GroundTruthNSRTFactory):
@@ -22,5 +22,39 @@ class PyBulletShelfGroundTruthNSRTFactory(GroundTruthNSRTFactory):
                   predicates: Dict[str, Predicate],
                   options: Dict[str, ParameterizedOption]) -> Set[NSRT]:
 
-        import ipdb
-        ipdb.set_trace()
+        # Types
+        block_type = types["block"]
+        robot_type = types["robot"]
+        shelf_type = types["shelf"]
+
+        # Predicates
+        OnTable = predicates["OnTable"]
+        InShelf = predicates["InShelf"]
+
+        # Options
+        PickPlace = options["PickPlace"]
+
+        nsrts = set()
+
+        robot = Variable("?robot", robot_type)
+        block = Variable("?block", block_type)
+        shelf = Variable("?shelf", shelf_type)
+        parameters = [robot, block, shelf]
+        option_vars = [robot, block]
+        option = PickPlace
+        preconditions = {
+            LiftedAtom(OnTable, [block])
+        }
+        add_effects = {
+            LiftedAtom(InShelf, [block, shelf])
+        }
+        delete_effects = {
+            LiftedAtom(OnTable, [block])
+        }
+        ignore_effects = set()
+        pick_place_nsrt = NSRT("PickPlace", parameters, preconditions,
+                                  add_effects, delete_effects, ignore_effects,
+                                  option, option_vars, null_sampler)
+        nsrts.add(pick_place_nsrt)
+
+        return nsrts
