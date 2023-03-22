c9b71a851c246e7a21b17afcaa889353f38f3347
Lilian Luong <lilianluong64@gmail.com>
2023-03-22 18:00:26 -0400

    [exit_garage] implemented ExitGarageEnv and ground truth models [untested]

---
+++ predicators/ground_truth_models/exit_garage/nsrts.py
@@ -0,0 +1,113 @@
+"""Ground-truth NSRTs for the exit garage environment."""
+
+from typing import Dict, Sequence, Set
+
+import numpy as np
+
+from predicators.ground_truth_models import GroundTruthNSRTFactory
+from predicators.structs import NSRT, Array, GroundAtom, LiftedAtom, Object, \
+    ParameterizedOption, Predicate, State, Type, Variable
+
+
+class ExitGarageGroundTruthNSRTFactory(GroundTruthNSRTFactory):
+    """Ground-truth NSRTs for the exit garage environment."""
+
+    @classmethod
+    def get_env_names(cls) -> Set[str]:
+        return {"exit_garage"}
+
+    @staticmethod
+    def get_nsrts(env_name: str, types: Dict[str, Type],
+                  predicates: Dict[str, Predicate],
+                  options: Dict[str, ParameterizedOption]) -> Set[NSRT]:
+        # Types
+        car_type = types["car"]
+        robot_type = types["robot"]
+        obstacle_type = types["obstacle"]
+        storage_type = types["storage"]
+
+        # Predicates
+        CarHasExited = predicates["CarHasExited"]
+        CarryingObstacle = predicates["CarryingObstacle"]
+        ObstacleCleared = predicates["ObstacleCleared"]
+        ObstacleNotCleared = predicates["ObstacleNotCleared"]
+        NotCarryingObstacle = predicates["NotCarryingObstacle"]
+
+        # Options
+        DriveCarToExit = options["DriveCarToExit"]
+        PickupObstacle = options["PickupObstacle"]
+        StoreObstacle = options["StoreObstacle"]
+
+        nsrts = set()
+
+        def random_sampler(state: State, goal: Set[GroundAtom],
+                           rng: np.random.Generator,
+                           objs: Sequence[Object]) -> Array:
+            del state, goal, objs  # unused
+            # Note: just return a random value from 0 to 1
+            return np.array([rng.uniform()], dtype=np.float32)
+
+        # DriveCarToExit
+        car = Variable("?car", car_type)
+        parameters = [car]
+        option_vars = [car]
+        option = DriveCarToExit
+        preconditions: Set[LiftedAtom] = set()
+        add_effects: Set[LiftedAtom] = {
+            LiftedAtom(CarHasExited, [car]),
+        }
+        delete_effects: Set[LiftedAtom] = set()
+        ignore_effects: Set[Predicate] = set()
+        drive_car_to_exit_nsrt = NSRT("DriveCarToExit", parameters,
+                                      preconditions, add_effects,
+                                      delete_effects, ignore_effects, option,
+                                      option_vars, random_sampler)
+        nsrts.add(drive_car_to_exit_nsrt)
+
+        # PickupObstacle
+        robot = Variable("?robot", robot_type)
+        obstacle = Variable("?obstacle", obstacle_type)
+        parameters = [robot, obstacle]
+        option_vars = [robot, obstacle]
+        option = PickupObstacle
+        preconditions = {
+            LiftedAtom(NotCarryingObstacle, [robot]),
+            LiftedAtom(ObstacleNotCleared, [obstacle]),
+        }
+        add_effects = {
+            LiftedAtom(CarryingObstacle, [robot, obstacle]),
+        }
+        delete_effects = {
+            LiftedAtom(NotCarryingObstacle, [robot]),
+            LiftedAtom(ObstacleNotCleared, [obstacle]),
+        }
+        ignore_effects = set()
+        pickup_obstacle_nsrt = NSRT("PickupObstacle", parameters,
+                                    preconditions, add_effects, delete_effects,
+                                    ignore_effects, option, option_vars,
+                                    random_sampler)
+        nsrts.add(pickup_obstacle_nsrt)
+
+        # StoreObstacle
+        robot = Variable("?robot", robot_type)
+        obstacle = Variable("?obstacle", obstacle_type)
+        parameters = [robot, obstacle]
+        option_vars = [robot, obstacle]
+        option = StoreObstacle
+        preconditions = {
+            LiftedAtom(CarryingObstacle, [robot, obstacle]),
+        }
+        add_effects = {
+            LiftedAtom(NotCarryingObstacle, [robot]),
+            LiftedAtom(ObstacleCleared, [obstacle]),
+        }
+        delete_effects = {
+            LiftedAtom(CarryingObstacle, [robot, obstacle]),
+        }
+        ignore_effects = set()
+        store_obstacle_nsrt = NSRT("StoreObstacle", parameters, preconditions,
+                                   add_effects, delete_effects, ignore_effects,
+                                   option, option_vars, random_sampler)
+        nsrts.add(store_obstacle_nsrt)
+
+        return nsrts
---
+++ predicators/ground_truth_models/exit_garage/options.py
@@ -0,0 +1,336 @@
+"""Ground-truth options for the exit garage environment."""
+
+import time
+from typing import Callable, Dict, Iterator, Sequence, Set
+
+import numpy as np
+from gym.spaces import Box
+
+from predicators import utils
+from predicators.envs.exit_garage import ExitGarageEnv
+from predicators.ground_truth_models import GroundTruthOptionFactory
+from predicators.settings import CFG
+from predicators.structs import Action, Array, Object, \
+    ParameterizedInitiable, ParameterizedOption, Predicate, State, Type
+
+
+class ExitGarageGroundTruthOptionFactory(GroundTruthOptionFactory):
+    """Ground-truth options for the exit garage environment."""
+
+    @classmethod
+    def get_env_names(cls) -> Set[str]:
+        return {"exit_garage"}
+
+    @classmethod
+    def get_options(cls, env_name: str, types: Dict[str, Type],
+                    predicates: Dict[str, Predicate],
+                    action_space: Box) -> Set[ParameterizedOption]:
+        car_type = types["car"]
+        robot_type = types["robot"]
+        obstacle_type = types["obstacle"]
+        storage_type = types["storage"]
+
+        CarHasExited = predicates["CarHasExited"]
+        CarryingObstacle = predicates["CarryingObstacle"]
+        ObstacleCleared = predicates["ObstacleCleared"]
+        ObstacleNotCleared = predicates["ObstacleNotCleared"]
+        NotCarryingObstacle = predicates["NotCarryingObstacle"]
+
+        def _motion_plan_policy(state: State, memory: Dict,
+                                objects: Sequence[Object],
+                                params: Array) -> Action:
+            del state, objects, params  # unused
+            assert memory["action_plan"], "Motion plan did not reach its goal"
+            return memory["action_plan"].pop(0)
+
+        # DriveCarToExit
+        def _DriveCarToExit_terminal(state: State, memory: Dict,
+                                     objects: Sequence[Object],
+                                     params: Array) -> bool:
+            del memory, params  # unused
+            return CarHasExited.holds(state, objects)
+
+        def _DriveCarToExit_initiable(state: State, memory: Dict,
+                                      objects: Sequence[Object],
+                                      params: Array) -> bool:
+            car, = objects
+
+            # Set up the RRT goal function
+            def _goal_fn(pt: Array) -> bool:
+                # Create a hypothetical state for the car at this point and
+                # check whether CarHasExited is True
+                x, y, theta = pt
+                s = state.copy()
+                s.set(car, "x", x)
+                s.set(car, "y", y)
+                s.set(car, "theta", theta)
+                return CarHasExited.holds(state, objects)
+
+            # Set up the target input for the motion planner.
+            target_x = 0.95
+            target_y = 0.4 - ExitGarageEnv.exit_width / 2
+            target_theta = 0
+            success = cls._run_rrt(state,
+                                   memory,
+                                   params,
+                                   car,
+                                   np.array([target_x, target_y,
+                                             target_theta]),
+                                   goal_fn=_goal_fn)
+            return success
+
+        DriveCarToExit = ParameterizedOption(
+            "DriveCarToExit",
+            types=[car_type],
+            params_space=Box(0, 1, (1, )),
+            policy=_motion_plan_policy,
+            initiable=_DriveCarToExit_initiable,
+            terminal=_DriveCarToExit_terminal,
+        )
+
+        # PickupObstacle
+        def _PickupObstacle_terminal(state: State, memory: Dict,
+                                     objects: Sequence[Object],
+                                     params: Array) -> bool:
+            del memory, params  # unused
+            return CarryingObstacle.holds(state, objects)
+
+        def _PickupObstacle_initiable(state: State, memory: Dict,
+                                      objects: Sequence[Object],
+                                      params: Array) -> bool:
+            robot, obstacle = objects
+            if not ObstacleNotCleared.holds(state, objects[1:]):
+                return False  # obstacle already picked or cleared
+            if not NotCarryingObstacle.holds(state, objects[:1]):
+                return False  # robot already carrying something else
+            # Set up the target input for the motion planner.
+            target_x = state.get(obstacle, "x")
+            target_y = state.get(obstacle, "y")
+            success = cls._run_birrt(state, memory, params, robot,
+                                     np.array([target_x, target_y]))
+            if not success:
+                # Failed to find motion plan, so option is not initiable
+                return False
+            # Append pickup action to memory action plan
+            memory["action_plan"].append(
+                Action(np.array([0.0, 0.0, 0.0, 0.0, 1.0], dtype=np.float32)))
+            # Picking an obstacle takes a bit of time to plan, artificially
+            time.sleep(CFG.exit_garage_pick_place_refine_penalty)
+            return True
+
+        PickupObstacle = ParameterizedOption(
+            "PickupObstacle",
+            types=[robot_type, obstacle_type],
+            params_space=Box(0, 1, (1, )),
+            policy=_motion_plan_policy,
+            initiable=_PickupObstacle_initiable,
+            terminal=_PickupObstacle_terminal,
+        )
+
+        # StoreObstacle
+        def _StoreObstacle_terminal(state: State, memory: Dict,
+                                    objects: Sequence[Object],
+                                    params: Array) -> bool:
+            del memory, params  # unused
+            return ObstacleCleared.holds(state, objects[1:])
+
+        def _StoreObstacle_initiable(state: State, memory: Dict,
+                                     objects: Sequence[Object],
+                                     params: Array) -> bool:
+            robot, target = objects
+            if not CarryingObstacle.holds(state, objects):
+                return False  # obstacle isn't being carried, so can't store
+            storage, = state.get_objects(storage_type)
+            num_stored = state.get(storage, "num_stored")
+            # Set up the target input for the motion planner.
+            target_x = (0.01 + ExitGarageEnv.obstacle_radius * 2) * num_stored
+            target_x += ExitGarageEnv.obstacle_radius
+            target_y = 1.0 - ExitGarageEnv.storage_area_width / 2
+            success = cls._run_birrt(state, memory, params, robot,
+                                     np.array([target_x, target_y]))
+            if not success:
+                # Failed to find motion plan, so option is not initiable
+                return False
+            # Append place action to memory action plan
+            memory["action_plan"].append(
+                Action(np.array([0.0, 0.0, 0.0, 0.0, 1.0], dtype=np.float32)))
+            # Placing an obstacle takes a bit of time to plan, artificially
+            time.sleep(CFG.exit_garage_pick_place_refine_penalty)
+            return True
+
+        StoreObstacle = ParameterizedOption(
+            "StoreObstacle",
+            types=[robot_type, obstacle_type],
+            params_space=Box(0, 1, (1, )),
+            policy=_motion_plan_policy,
+            initiable=_StoreObstacle_initiable,
+            terminal=_StoreObstacle_terminal,
+        )
+
+        return {DriveCarToExit, PickupObstacle, StoreObstacle}
+
+    @classmethod
+    def _run_rrt(cls, state: State, memory: Dict, params: Array,
+                 move_obj: Object, target_position: Array,
+                 goal_fn: Callable[[Array], bool]) -> bool:
+        """Runs RRT to motion plan from start car configuration to the target,
+        or when the car reaches the exit. Store the position and action plans
+        in memory if successful.
+
+        Returns True if successful, else False.
+        """
+        rng = np.random.default_rng(int(params[0] * 1e4))
+
+        def _sample_fn(_: Array) -> Array:
+            # Sample a point in the environment
+            sample = [
+                rng.uniform(ExitGarageEnv.x_lb, ExitGarageEnv.x_ub),  # x
+                rng.uniform(ExitGarageEnv.y_lb, ExitGarageEnv.y_ub),  # y
+                rng.uniform(-np.pi, np.pi),  # theta
+            ]
+            return np.array(sample, dtype=np.float32)
+
+        def _distance_fn(from_pt: Array, to_pt: Array) -> float:
+            distance = np.sum(np.subtract(from_pt[:2], to_pt[:2])**2)
+            scaled_angle_dist = (from_pt[2] - to_pt[2]) / np.pi
+            scaled_angle_dist = (scaled_angle_dist + 1) % 2.0 - 1
+            distance += scaled_angle_dist**2
+            return distance
+
+        def _sample_control_result(pt: Array) -> Array:
+            x, y, theta = pt
+            vel = rng.uniform(-ExitGarageEnv.car_max_absolute_vel,
+                              ExitGarageEnv.car_max_absolute_vel)
+            omega = rng.uniform(-ExitGarageEnv.car_steering_omega_limit,
+                                ExitGarageEnv.car_steering_omega_limit)
+            new_x = x + np.cos(theta) * vel
+            new_y = y + np.sin(theta) * vel
+            new_theta = (theta + omega + np.pi) % (2 * np.pi) - np.pi
+            return np.array([new_x, new_y, new_theta], dtype=np.float32)
+
+        def _extend_fn(pt1: Array, pt2: Array) -> Iterator[Array]:
+            # Make sure that we obey the bounds on actions.
+            # For non-holonomic, we need to sample motion controls
+            # and choose the one that produces the closest result
+            current_pt = pt1
+            distance_thres = CFG.exit_garage_rrt_extend_fn_threshold
+            while _distance_fn(current_pt, pt2) > distance_thres:
+                current_pt = min(
+                    (_sample_control_result(current_pt)
+                     for _ in range(CFG.exit_garage_rrt_num_control_samples)),
+                    key=lambda pt: _distance_fn(pt, pt2))
+                yield current_pt
+
+        def _collision_fn(pt: Array) -> bool:
+            # Check for collision of car in non-holonomic case
+            # Make a hypothetical state for the car at this point and check
+            # if there would be collisions.
+            x, y, theta = pt
+            s = state.copy()
+            s.set(move_obj, "x", x)
+            s.set(move_obj, "y", y)
+            s.set(move_obj, "theta", theta)
+            return ExitGarageEnv.car_has_collision(s)
+
+        rrt = utils.RRT(_sample_fn,
+                        _extend_fn,
+                        _collision_fn,
+                        _distance_fn,
+                        rng,
+                        num_attempts=CFG.exit_garage_rrt_num_attempts,
+                        num_iters=CFG.exit_garage_rrt_num_iters,
+                        smooth_amt=CFG.exit_garage_rrt_smooth_amt,
+                        sample_goal_eps=CFG.exit_garage_rrt_sample_goal_eps,
+                        goal_fn=goal_fn)
+        # Run planning.
+        start_pos_list = [
+            state.get(move_obj, "x"),
+            state.get(move_obj, "y"),
+            state.get(move_obj, "theta"),
+        ]
+        start_position = np.array(start_pos_list)
+        position_plan = rrt.query(start_position, target_position)
+        # If motion planning fails, determine the option to be not initiable.
+        if position_plan is None:
+            return False
+        # The position plan is used for the termination check, and possibly
+        # can be used for debug drawing in the rendering in the future.
+        memory["position_plan"] = position_plan
+        # Convert the plan from position space to action space.
+        action_plan = []
+        for i in range(len(position_plan) - 1):
+            x1, y1, theta1 = position_plan[i]
+            x2, y2, theta2 = position_plan[i + 1]
+            vel = ((x2 - x1) / np.cos(theta1) if np.cos(theta1) != 0 else
+                   (y2 - y1) / np.sin(theta1))
+            assert abs(vel) < ExitGarageEnv.car_max_absolute_vel
+            omega = (theta2 - theta1 + np.pi) % (2 * np.pi) - np.pi
+            assert abs(omega) < ExitGarageEnv.car_steering_omega_limit
+            action_plan.append(
+                Action(np.array([vel, omega, 0.0, 0.0, 0.0],
+                                dtype=np.float32)))
+        memory["action_plan"] = action_plan
+        return True
+
+    @classmethod
+    def _run_birrt(cls, state: State, memory: Dict, params: Array,
+                   move_obj: Object, target_position: Array) -> bool:
+        """Runs BiRRT to motion plan from start to target positions, and store
+        the position and action plans in memory if successful.
+
+        Returns True if successful, else False
+        """
+        rng = np.random.default_rng(int(params[0] * 1e4))
+
+        def _sample_fn(_: Array) -> Array:
+            # Sample a point in the environment
+            sample = [
+                rng.uniform(ExitGarageEnv.x_lb, ExitGarageEnv.x_ub),  # x
+                rng.uniform(ExitGarageEnv.y_lb, ExitGarageEnv.y_ub),  # y
+            ]
+            return np.array(sample, dtype=np.float32)
+
+        def _distance_fn(from_pt: Array, to_pt: Array) -> float:
+            distance = np.sum(np.subtract(from_pt[:2], to_pt[:2])**2)
+            return distance
+
+        def _extend_fn(pt1: Array, pt2: Array) -> Iterator[Array]:
+            # Make sure that we obey the bounds on actions.
+            distance = np.linalg.norm(pt2 - pt1)
+            num = int(distance / ExitGarageEnv.robot_action_magnitude) + 1
+            for i in range(1, num + 1):
+                yield pt1 * (1 - i / num) + pt2 * i / num
+
+        def _collision_fn(pt: Array) -> bool:
+            # For ceiling robot, there are no collisions
+            return False
+
+        birrt = utils.BiRRT(_sample_fn,
+                            _extend_fn,
+                            _collision_fn,
+                            _distance_fn,
+                            rng,
+                            num_attempts=CFG.exit_garage_rrt_num_attempts,
+                            num_iters=CFG.exit_garage_rrt_num_iters,
+                            smooth_amt=CFG.exit_garage_rrt_smooth_amt)
+        # Run planning.
+        start_pos_list = [
+            state.get(move_obj, "x"),
+            state.get(move_obj, "y"),
+        ]
+        start_position = np.array(start_pos_list)
+        position_plan = birrt.query(start_position, target_position)
+        # If motion planning fails, determine the option to be not initiable.
+        if position_plan is None:
+            return False
+        # The position plan is used for the termination check, and possibly
+        # can be used for debug drawing in the rendering in the future.
+        memory["position_plan"] = position_plan
+        # Convert the plan from position space to action space.
+        deltas = np.subtract(position_plan[1:], position_plan[:-1])
+        action_plan = [
+            Action(np.array([0.0, 0.0, dx, dy, 0.0], dtype=np.float32))
+            for (dx, dy) in deltas
+        ]
+        memory["action_plan"] = action_plan
+        return True
