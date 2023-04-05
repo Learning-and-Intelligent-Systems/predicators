4428a7bc89bb950cb802808df39b2873dcbcf4a0
Lilian Luong <lilianluong64@gmail.com>
2023-04-05 18:16:53 -0400

    [gnn refinement] improved tests for GNNRefinementEstimator

--- tests/refinement_estimators/test_gnn_refinement_estimator.py
+++ tests/refinement_estimators/test_gnn_refinement_estimator.py
@@ -1,14 +1,28 @@
 """Test cases for the GNN refinement cost estimator."""
 
-import pytest
+import os
+import shutil
+from pathlib import Path
+from unittest.mock import PropertyMock, patch
 
+import numpy as np
+import pytest
+from gym.spaces import Box
+
+import predicators.envs.narrow_passage
 from predicators import utils
 from predicators.envs.narrow_passage import NarrowPassageEnv
 from predicators.ground_truth_models import get_gt_nsrts, get_gt_options
+from predicators.ground_truth_models.narrow_passage import \
+    NarrowPassageGroundTruthNSRTFactory, \
+    NarrowPassageGroundTruthOptionFactory
 from predicators.refinement_estimators.gnn_refinement_estimator import \
     GNNRefinementEstimator
 from predicators.settings import CFG
-from predicators.structs import GroundAtom
+from predicators.structs import NSRT, Action, GroundAtom, \
+    ParameterizedOption, Predicate, Task, Variable
+
+_ENV_MODULE_NAME = predicators.envs.narrow_passage.__name__
 
 
 def test_gnn_refinement_estimator():
@@ -94,3 +108,110 @@ def test_narrow_passage_gnn_refinement_estimator():
                                                 move_through_door_skeleton,
                                                 move_through_door_atoms_seq)
     assert move_through_door_cost < float('inf')
+
+
+def test_gnn_refinement_estimator_arities():
+    """Test GNN refinement cost estimator on mocked predicate/NSRT sets that
+    are 0-arity, unary, and binary."""
+    utils.reset_config({
+        "env": "narrow_passage",
+        "gnn_num_message_passing": 1,
+        "gnn_layer_size": 3,
+        "gnn_num_epochs": 1,
+        "gnn_use_validation_set": False,
+    })
+
+    # Get base environment, types, predicates, NSRTs
+    env = NarrowPassageEnv()
+    _, DoorIsOpen, TouchedGoal = sorted(env.predicates)
+    door_type, _, robot_type, target_type, _ = sorted(env.types)
+    base_options = get_gt_options(env.get_name())
+    gt_nsrts = get_gt_nsrts(CFG.env, env.predicates, base_options)
+    move_and_open_door_option, _ = sorted(base_options)
+    move_and_open_door_nsrt, _ = sorted(gt_nsrts)
+
+    # Make predicates of all arities
+    ZeroArityPred = Predicate("ZeroArityPred", [], lambda s, o: False)
+    UnaryPred = DoorIsOpen
+    BinaryPred = TouchedGoal
+
+    # Make dummy options and NSRTs of all arities
+
+    _policy = lambda _1, _2, _3, _4: Action(
+        np.array([0, 0, 0], dtype=np.float32))
+    _initiable = lambda _1, _2, _3, _4: True
+    _sampler = lambda _1, _2, rng, _4: np.array([rng.uniform()],
+                                                dtype=np.float32)
+
+    ZeroArityOption = ParameterizedOption("ZeroArityOption", [],
+                                          Box(0, 1,
+                                              (1, )), _policy, _initiable,
+                                          lambda _1, _2, _3, _4: True)
+    ZeroArityNSRT = NSRT("ZeroArityNSRT", [], set(), set(), set(), set(),
+                         ZeroArityOption, [], _sampler)
+
+    UnaryOption = ParameterizedOption("UnaryOption", [robot_type],
+                                      Box(0, 1, (1, )), _policy, _initiable,
+                                      lambda _1, _2, _3, _4: True)
+    robot = Variable("?robot", robot_type)
+    UnaryNSRT = NSRT("UnaryNSRT", [robot], set(), set(), set(), set(),
+                     UnaryOption, [robot], _sampler)
+    BinaryNSRT = move_and_open_door_nsrt
+
+    mock_preds = {ZeroArityPred, UnaryPred, BinaryPred}
+    mock_options = {ZeroArityOption, UnaryOption, move_and_open_door_option}
+    mock_nsrts = {ZeroArityNSRT, UnaryNSRT, BinaryNSRT}
+
+    with patch(f"{_ENV_MODULE_NAME}.NarrowPassageEnv.predicates",
+               new_callable=PropertyMock) as mock_env, \
+            patch.object(NarrowPassageGroundTruthOptionFactory,
+                         "get_options",
+                         return_value=mock_options), \
+            patch.object(NarrowPassageGroundTruthNSRTFactory,
+                         "get_nsrts",
+                         return_value=mock_nsrts):
+        mock_env.return_value = mock_preds
+        # Test that _setup_fields() works
+        estimator = GNNRefinementEstimator()
+        estimator2 = GNNRefinementEstimator()
+
+    # Make a test Task with all types of predicates/NSRTs involved
+    sample_task = env.get_train_tasks()[0].task
+    initial_state = sample_task.init
+    robot, = initial_state.get_objects(robot_type)
+    door, = initial_state.get_objects(door_type)
+    target, = initial_state.get_objects(target_type)
+    goal = {
+        GroundAtom(ZeroArityPred, []),
+        GroundAtom(UnaryPred, [door]),
+        GroundAtom(BinaryPred, [robot, target]),
+    }
+    task = Task(initial_state, goal)
+
+    # Make a test skeleton and atoms_sequence
+    skeleton = [ZeroArityNSRT.ground([])]
+    atoms_sequence = [goal, goal]
+
+    # Create sample refinement training data
+    data = [(task, skeleton, atoms_sequence, False, [5.0])]
+    # Check that train() and _graphify_single_input() successfully run
+    estimator.train(data)
+
+    # Test that getting a cost returns a finite cost
+    test_cost = estimator.get_cost(task, skeleton, atoms_sequence)
+    assert test_cost < float('inf')
+
+    # Create fake directory to test saving and loading model
+    parent_dir = os.path.dirname(__file__)
+    approach_dir = os.path.join(parent_dir, "_fake_approach")
+    os.makedirs(approach_dir, exist_ok=True)
+    test_approach_path = Path(approach_dir) / "test.estimator"
+    estimator.save_model(test_approach_path)
+    estimator2.load_model(test_approach_path)
+
+    # Check that the loaded model is the same as the saved one
+    test_cost2 = estimator2.get_cost(task, skeleton, atoms_sequence)
+    assert test_cost2 == test_cost
+
+    # Remove temp directory
+    shutil.rmtree(approach_dir)
