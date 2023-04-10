75a35e0ebb39014c39e793a317f78cbd476f47ac
Lilian Luong <lilianluong64@gmail.com>
2023-04-10 18:14:11 -0400

    [scripts] supercloud launch scripts/configs for refinement estimation

--- scripts/cluster_utils.py
+++ scripts/cluster_utils.py
@@ -21,6 +21,7 @@ class RunConfig:
     args: List[str]  # e.g. --make_test_videos
     flags: Dict[str, Any]  # e.g. --num_train_tasks 1
     use_gpu: bool  # e.g. --use_gpu True
+    train_refinement_estimator: bool  # e.g. --train_refinement_estimator True
 
     def __post_init__(self) -> None:
         # For simplicity, disallow overrides of the SAVE_DIRS.
@@ -50,7 +51,9 @@ def config_to_logfile(cfg: RunConfig, suffix: str = ".log") -> str:
     else:
         assert isinstance(cfg, BatchSeedRunConfig)
         seed = None
-    return f"{cfg.env}__{cfg.approach}__{cfg.experiment_id}__{seed}" + suffix
+    name = "train_" if cfg.train_refinement_estimator else ""
+    name += f"{cfg.env}__{cfg.approach}__{cfg.experiment_id}__{seed}" + suffix
+    return name
 
 
 def config_to_cmd_flags(cfg: RunConfig) -> str:
@@ -89,6 +92,10 @@ def generate_run_configs(config_filename: str,
             use_gpu = config["USE_GPU"]
         else:
             use_gpu = False
+        if "TRAIN_REFINEMENT_ESTIMATOR" in config.keys():
+            train_refinement_estimator = config["TRAIN_REFINEMENT_ESTIMATOR"]
+        else:
+            train_refinement_estimator = False
         # Loop over approaches.
         for approach_exp_id, approach_config in config["APPROACHES"].items():
             approach = approach_config["NAME"]
@@ -111,11 +118,13 @@ def generate_run_configs(config_filename: str,
                 if batch_seeds:
                     yield BatchSeedRunConfig(experiment_id, approach, env,
                                              run_args, run_flags, use_gpu,
+                                             train_refinement_estimator,
                                              start_seed, num_seeds)
                 else:
                     for seed in range(start_seed, start_seed + num_seeds):
                         yield SingleSeedRunConfig(experiment_id, approach, env,
                                                   run_args, run_flags, use_gpu,
+                                                  train_refinement_estimator,
                                                   seed)
 
 
