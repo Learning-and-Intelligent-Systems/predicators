f7160f572dfa46e5f635c37eefc27d9e5331bbe7
Tom Silver <tomssilver@gmail.com>
2023-04-11 09:58:51 -0400

    revert

--- predicators/ground_truth_models/exit_garage/__init__.py
+++ predicators/ground_truth_models/exit_garage/__init__.py
@@ -1,10 +1,8 @@
 """Ground-truth models for exit garage environment and variants."""
 
-from .ldl_bridge_policy import ExitGarageLDLBridgePolicyFactory
 from .nsrts import ExitGarageGroundTruthNSRTFactory
 from .options import ExitGarageGroundTruthOptionFactory
 
 __all__ = [
-    "ExitGarageGroundTruthNSRTFactory", "ExitGarageGroundTruthOptionFactory",
-    "ExitGarageLDLBridgePolicyFactory"
+    "ExitGarageGroundTruthNSRTFactory", "ExitGarageGroundTruthOptionFactory"
 ]
