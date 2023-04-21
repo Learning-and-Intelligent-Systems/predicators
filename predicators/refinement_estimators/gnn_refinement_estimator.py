4c318f790168d404ec365b2dd6ab3370637f6354
Lilian Luong <lilianluong64@gmail.com>
2023-04-21 18:20:49 -0400

    [gnn refinement] bugfix

--- predicators/refinement_estimators/gnn_refinement_estimator.py
+++ predicators/refinement_estimators/gnn_refinement_estimator.py
@@ -97,8 +97,9 @@ class GNNRefinementEstimator(BaseRefinementEstimator):
                 graph_inputs.append(
                     self._graphify_single_input(state, atoms, goal, action))
                 graph_targets.append(
-                    self._graphify_single_target(target_time,
-                                                 low_level_count[i]))
+                    self._graphify_single_target(
+                        target_time,
+                        low_level_count[i] if succeeded else 0))
         assert len(graph_inputs) and len(graph_targets), "No usable data"
         self._data_exemplar = (graph_inputs[0], graph_targets[0])
 
