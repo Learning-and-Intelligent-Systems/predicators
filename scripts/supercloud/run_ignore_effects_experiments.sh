9b9bfae9e2a05707959ff1afd406022b219e1db8
Nishanth Kumar <nishanth.kumar20@gmail.com>
2023-05-21 16:27:48 -0400

    recollect data - it's fine...

--- scripts/supercloud/run_ignore_effects_experiments.sh
+++ scripts/supercloud/run_ignore_effects_experiments.sh
@@ -33,22 +33,22 @@ for ENV in ${ALL_ENVS[@]}; do
             python $FILE $COMMON_ARGS --experiment_id ${ENV}_gnn_modelfree_${DEMOS}demo --load_experiment_id ${ENV}_gnn_shooting_${DEMOS}demo --approach gnn_option_policy --num_train_tasks ${DEMOS} --gnn_option_policy_solve_with_shooting False --load_approach --load_data --sesame_max_skeletons_optimized 1
         else
             # PNAD search approach with various numbers of demonstrations.
-            python $FILE $COMMON_ARGS --experiment_id ${ENV}_pnadsearch_${DEMOS}demo --approach nsrt_learning --strips_learner pnad_search --num_train_tasks ${DEMOS} --sesame_max_skeletons_optimized 1 --load_atoms --load_data
+            python $FILE $COMMON_ARGS --experiment_id ${ENV}_pnadsearch_${DEMOS}demo --approach nsrt_learning --strips_learner pnad_search --num_train_tasks ${DEMOS} --sesame_max_skeletons_optimized 1
             # # Main backchaining approach with various numbers of demonstrations.
             # python $FILE $COMMON_ARGS --experiment_id ${ENV}_backchaining_${DEMOS}demo --approach nsrt_learning --strips_learner backchaining --num_train_tasks ${DEMOS}
             # Cluster-and-intersect (RLDM) baseline. Although it is guaranteed to
             # preserve harmlessness, we disable the check because it takes a long
             # time (since the operators have high arity).
-            python $FILE $COMMON_ARGS --experiment_id ${ENV}_cluster_and_intersect_${DEMOS}demo --approach nsrt_learning --strips_learner cluster_and_intersect --num_train_tasks ${DEMOS} --disable_harmlessness_check True --sesame_max_skeletons_optimized 1 --load_atoms --load_data
+            python $FILE $COMMON_ARGS --experiment_id ${ENV}_cluster_and_intersect_${DEMOS}demo --approach nsrt_learning --strips_learner cluster_and_intersect --num_train_tasks ${DEMOS} --disable_harmlessness_check True --sesame_max_skeletons_optimized 1
             # LOFT baseline without random replays. Same note on harmlessness as for cluster-and-intersect.
-            python $FILE $COMMON_ARGS --experiment_id ${ENV}_cluster_and_search_${DEMOS}demo --approach nsrt_learning --strips_learner cluster_and_search --num_train_tasks ${DEMOS} --disable_harmlessness_check True --sesame_max_skeletons_optimized 1 --load_atoms --load_data
+            python $FILE $COMMON_ARGS --experiment_id ${ENV}_cluster_and_search_${DEMOS}demo --approach nsrt_learning --strips_learner cluster_and_search --num_train_tasks ${DEMOS} --disable_harmlessness_check True --sesame_max_skeletons_optimized 1
             # LOFT baseline with random replays. Same note on harmlessness as for cluster-and-intersect.
-            python $FILE $COMMON_ARGS --experiment_id ${ENV}_cluster_and_search_random_replays_${DEMOS}demo --approach nsrt_learning --strips_learner cluster_and_search --num_train_tasks ${DEMOS} --disable_harmlessness_check True --offline_data_method demo+replay --sesame_allow_noops False --offline_data_num_replays 2500 --sesame_max_skeletons_optimized 1 --load_atoms --load_data
+            python $FILE $COMMON_ARGS --experiment_id ${ENV}_cluster_and_search_random_replays_${DEMOS}demo --approach nsrt_learning --strips_learner cluster_and_search --num_train_tasks ${DEMOS} --disable_harmlessness_check True --offline_data_method demo+replay --sesame_allow_noops False --offline_data_num_replays 2500 --sesame_max_skeletons_optimized 1
             # Prediction error baseline that optimizes via hill climbing. Not
             # guaranteed to preserve harmlessness.
-            python $FILE $COMMON_ARGS --experiment_id ${ENV}_pred_error_${DEMOS}demo --approach nsrt_learning --strips_learner cluster_and_intersect_sideline_prederror --num_train_tasks ${DEMOS} --disable_harmlessness_check True --sesame_max_skeletons_optimized 1 --load_atoms --load_data
+            python $FILE $COMMON_ARGS --experiment_id ${ENV}_pred_error_${DEMOS}demo --approach nsrt_learning --strips_learner cluster_and_intersect_sideline_prederror --num_train_tasks ${DEMOS} --disable_harmlessness_check True --sesame_max_skeletons_optimized 1
             # Model-based GNN option policy baseline.
-            python $FILE $COMMON_ARGS --experiment_id ${ENV}_gnn_shooting_${DEMOS}demo --approach gnn_option_policy --num_train_tasks ${DEMOS} --sesame_max_skeletons_optimized 1 --load_atoms --load_data
+            python $FILE $COMMON_ARGS --experiment_id ${ENV}_gnn_shooting_${DEMOS}demo --approach gnn_option_policy --num_train_tasks ${DEMOS} --sesame_max_skeletons_optimized 1
         fi
     done
 done
