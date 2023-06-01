f15ebde7ebbc3ee182008ffdf4265a0b49b9e1a4
Willie McClinton <wbm3.important@gmail.com>
2023-06-01 11:18:58 -0400

    fixed low_wall_rack grasp

--- predicators/ground_truth_models/spot/nsrts.py
+++ predicators/ground_truth_models/spot/nsrts.py
@@ -39,9 +39,9 @@ class SpotEnvsGroundTruthNSRTFactory(GroundTruthNSRTFactory):
             del state, goal, rng
             if objs[1].type.name == "bag":  # pragma: no cover
                 return np.array([0.0, 0.0, 0.0, -1.0])
-            if objs[2].type.name == "low_wall_rack":  # pragma: no cover
-                return np.array([0.0, 0.0, 0.15, -1.0])
-            return np.array([0.0, 0.0, 0.0, -1.0])
+            if objs[2].name == "low_wall_rack":  # pragma: no cover
+                return np.array([0.0, 0.0, 0.1, 0.0])
+            return np.array([0.0, 0.0, 0.0, 0.0])
 
         def place_sampler(state: State, goal: Set[GroundAtom],
                           rng: np.random.Generator,
