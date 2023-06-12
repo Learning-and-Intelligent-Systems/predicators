2c49c2612e9ebfd5e79da810fb3e9e198a900dd4
Tom Silver <tsilver@theaiinstitute.com>
2023-06-12 18:25:52 -0400

    giving up for the day

--- predicators/envs/cover.py
+++ predicators/envs/cover.py
@@ -253,7 +253,8 @@ class CoverEnv(BaseEnv):
                 GroundAtom(self._Covers, [blocks[0], targets[0]]),
                 GroundAtom(self._Covers, [blocks[1], targets[1]])
             }
-            goals.append(goal3)
+            # TODO add back
+            # goals.append(goal3)
         for i in range(num):
             init = self._create_initial_state(blocks, targets, rng)
             assert init.get_objects(self._block_type) == blocks
@@ -1037,6 +1038,8 @@ class BumpyCoverEnv(CoverEnvRegrasp):
                               rng: np.random.Generator) -> State:
         data: Dict[Object, Array] = {}
         assert len(CFG.cover_block_widths) == len(blocks)
+        # TODO REMOVE!!!!
+        rng = np.random.default_rng(12982183)
         for i, (block, width) in enumerate(zip(blocks,
                                                CFG.cover_block_widths)):
             while True:
