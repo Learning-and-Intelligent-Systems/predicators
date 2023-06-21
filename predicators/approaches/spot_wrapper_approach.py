dcd1a8e983425703604b1d466b310843594e6249
Tom Silver <tsilver@theaiinstitute.com>
2023-06-21 18:16:17 -0400

    minor

--- predicators/approaches/spot_wrapper_approach.py
+++ predicators/approaches/spot_wrapper_approach.py
@@ -35,7 +35,8 @@ class SpotWrapperApproach(BaseApproachWrapper):
     def _solve(self, task: Task, timeout: int) -> Callable[[State], Action]:
 
         # Maintain policy from the base approach.
-        base_approach_policy: Optional[Callable[[State], Action]] = None
+        base_approach_policy: Optional[Callable[[State], Action]] = \
+            self._base_approach.solve(task, timeout)
         need_stow = False
 
         def _policy(state: State) -> Action:
