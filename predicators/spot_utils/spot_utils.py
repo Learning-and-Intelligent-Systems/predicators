bcd1b65f5ef2d5c296dfa95222a9e159c74f2966
NishanthJKumar <njk@mit.edu>
2023-06-21 16:46:19 -0400

    y don't u just clean code automagically yapf...

--- predicators/spot_utils/spot_utils.py
+++ predicators/spot_utils/spot_utils.py
@@ -543,7 +543,8 @@ class _SpotInterface():
                 break
             for _ in range(8):
                 objects_in_view: Dict[str, Tuple[float, float, float]] = {}
-                objects_in_view_by_camera = self.get_objects_in_view_by_camera()
+                objects_in_view_by_camera = self.get_objects_in_view_by_camera(
+                )
                 for v in objects_in_view_by_camera.values():
                     objects_in_view.update(v)
                 obj_poses.update(objects_in_view)
