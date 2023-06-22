bb6fe7f8a9c862b0db864343358c63fe58e0b32a
Tom Silver <tsilver@theaiinstitute.com>
2023-06-22 09:03:06 -0400

    add back gripper closing

--- predicators/spot_utils/spot_utils.py
+++ predicators/spot_utils/spot_utils.py
@@ -522,14 +522,16 @@ class _SpotInterface():
                 self.hand_movement(np.array([0.0, 0.0, 0.0]),
                                    keep_hand_pose=False,
                                    angle=(np.cos(np.pi / 8), 0,
-                                          np.sin(np.pi / 8), 0))
+                                          np.sin(np.pi / 8), 0),
+                                   open_gripper=False)
                 time.sleep(1.0)
                 return
             if "floor" in objs[2].name:
                 self.hand_movement(np.array([-0.2, 0.0, -0.25]),
                                    keep_hand_pose=False,
                                    angle=(np.cos(np.pi / 6), 0,
-                                          np.sin(np.pi / 6), 0))
+                                          np.sin(np.pi / 6), 0),
+                                   open_gripper=False)
                 time.sleep(1.0)
                 return
         self.stow_arm()
