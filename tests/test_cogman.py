0416f1cf3305d9ff230c146f8727f09e35e1536d
Tom Silver <tomsilver@users.noreply.github.com>
2023-06-27 16:17:04 -0400

    add expected atoms execution monitor  (#1487)

--- tests/test_cogman.py
+++ tests/test_cogman.py
@@ -40,3 +40,35 @@ def test_cogman(exec_monitor_name):
     next_obs = env.step(act)
     next_act = cogman.step(next_obs)
     assert not np.allclose(act.arr, next_act.arr)
+
+
+def test_cogman_with_expected_atoms_monitor():
+    """Tests for CogMan() with bilevel planning and the 'expected_atoms'
+    execution monitor."""
+    env_name = "cover"
+    utils.reset_config({
+        "env": env_name,
+        "num_train_tasks": 0,
+        "num_test_tasks": 2,
+        "bilevel_plan_without_sim": True,
+        "approach": "oracle"
+    })
+    env = get_or_create_env(env_name)
+    env_train_tasks = env.get_train_tasks()
+    env_test_tasks = env.get_test_tasks()
+    train_tasks = [t.task for t in env_train_tasks]
+    options = get_gt_options(env.get_name())
+    perceiver = create_perceiver("trivial")
+    exec_monitor = create_execution_monitor("expected_atoms")
+    approach = create_approach("oracle", env.predicates, options, env.types,
+                               env.action_space, train_tasks)
+    cogman = CogMan(approach, perceiver, exec_monitor)
+    env.reset("test", 0)
+    env_task = env_test_tasks[0]
+    cogman.reset(env_task)
+    obs = env_task.init_obs
+    act = cogman.step(obs)
+    assert env.action_space.contains(act.arr)
+    next_obs = env.step(act)
+    next_act = cogman.step(next_obs)
+    assert not np.allclose(act.arr, next_act.arr)
