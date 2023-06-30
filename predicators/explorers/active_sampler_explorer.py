a2bd9e64fbe62af07dc461efe09c8d720ac84338
Tom Silver <tsilver@theaiinstitute.com>
2023-06-30 13:16:34 -0400

    mf

--- predicators/explorers/active_sampler_explorer.py
+++ predicators/explorers/active_sampler_explorer.py
@@ -50,6 +50,7 @@ class ActiveSamplerExplorer(BaseExplorer):
         self._ground_op_hist = ground_op_hist
         self._last_executed_nsrt: Optional[_GroundNSRT] = None
         self._last_executed_option: Optional[_Option] = None
+        self._last_init_option_state: Optional[State] = None
         self._nsrt_to_explorer_sampler = nsrt_to_explorer_sampler
 
     @classmethod
@@ -149,6 +150,7 @@ class ActiveSamplerExplorer(BaseExplorer):
         # they succeeded, to update the ground_op_hist.
         self._last_executed_nsrt = None
         self._last_executed_option = None
+        self._last_init_option_state = None
 
         def _wrapped_option_policy(state: State) -> _Option:
             # Update ground_op_hist.
@@ -158,6 +160,7 @@ class ActiveSamplerExplorer(BaseExplorer):
             ground_nsrt = utils.option_to_ground_nsrt(option, self._nsrts)
             self._last_executed_nsrt = ground_nsrt
             self._last_executed_option = option
+            self._last_init_option_state = state
             return option
 
         # Finalize policy.
@@ -193,7 +196,7 @@ class ActiveSamplerExplorer(BaseExplorer):
         objects = option.objects
         params = option.params
         sampler_input = utils.construct_active_sampler_input(
-            state, objects, params, option.parent)
+            self._last_init_option_state, objects, params, option.parent)
         sampler_output = int(success)
         # Now, we need to get the file location and the max
         # datapoint id saved at this location.
