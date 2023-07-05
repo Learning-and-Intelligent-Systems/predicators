3d320228a28422df944a20b2134715cf9738c218
Tom Silver <tsilver@theaiinstitute.com>
2023-07-05 14:19:33 -0400

    wip

--- predicators/explorers/active_sampler_explorer.py
+++ predicators/explorers/active_sampler_explorer.py
@@ -260,6 +260,9 @@ class ActiveSamplerExplorer(BaseExplorer):
         bonus = c * np.sqrt(np.log(total_trials) / num_tries)
         logging.info(f"[Explorer]   num attempts: {num_tries}")
         # Try less successful operators more often.
-        score = (1.0 - success_rate) + bonus
+        if "Place" in ground_op.name and "extra" in str(ground_op.objects):
+            score = float("inf")
+        else:
+            score = (1.0 - success_rate) + bonus
         logging.info(f"[Explorer]   total score: {score}")
         return score
--- scripts/spot_cube_place_active_sampler_analysis.py
+++ scripts/spot_cube_place_active_sampler_analysis.py
@@ -1,6 +1,7 @@
 """Analysis for spot cube placing with active sampler learning."""
 
 import glob
+import logging
 import os
 from typing import List, Optional
 
@@ -13,7 +14,7 @@ from matplotlib import colormaps
 from matplotlib.colors import Normalize
 
 from predicators import utils
-from predicators.ml_models import BinaryClassifier
+from predicators.ml_models import BinaryClassifier, MLPBinaryClassifier
 from predicators.settings import CFG
 from predicators.structs import Array, Image
 
@@ -22,6 +23,11 @@ def _main() -> None:
     # Parse & validate args
     args = utils.parse_args()
     utils.update_config(args)
+    handlers: List[logging.Handler] = [logging.StreamHandler()]
+    logging.basicConfig(level=CFG.loglevel,
+                        format="%(message)s",
+                        handlers=handlers)
+    logging.getLogger('matplotlib.font_manager').setLevel(logging.ERROR)
     _analyze_saved_data()
     # _analyze_online_learning_cycles()
 
@@ -51,6 +57,8 @@ def _analyze_saved_data() -> None:
     img_outfile = "videos/spot_cube_active_sampler_learning_saved_data.png"
     imageio.imsave(img_outfile, img)
     print(f"Wrote out to {img_outfile}")
+    # Run sample efficiency analysis.
+    _run_sample_efficiency_analysis(X, y)
 
 
 def _analyze_online_learning_cycles() -> None:
@@ -162,6 +170,135 @@ def _create_image(X: List[Array],
     plt.ylim((y_min - 3 * radius, y_max + 3 * radius))
 
     return utils.fig2data(fig, dpi=150)
+
+
+class _OracleModel(BinaryClassifier):
+    """Oracle hand-written model."""
+
+    def fit(self, X: List[Array], y: List[Array]) -> None:
+        pass
+
+    def classify(self, x: Array) -> bool:
+        # TODO factor out.
+        place_robot_xy = math_helpers.Vec2(*x[-3:-1])
+
+        world_fiducial = math_helpers.Vec2(
+            x[12],  # state.get(surface, "x"),
+            x[13],  # state.get(surface, "y"),
+        )
+        world_to_robot = math_helpers.SE2Pose(
+            x[3],  # state.get(robot, "x"),
+            x[4],  # state.get(robot, "y"),
+            x[6],  # state.get(robot, "yaw"))
+        )
+        fiducial_in_robot_frame = world_to_robot.inverse() * world_fiducial
+        _, y_pt = place_robot_xy - fiducial_in_robot_frame
+
+        # Approximate.
+        return y_pt > 0
+
+    def predict_proba(self, x: Array) -> float:
+        return 1.0 if self.classify(x) else 0.0
+
+
+class _ConstantModel(BinaryClassifier):
+    """Oracle hand-written model."""
+
+    def __init__(self, seed: int, constant: bool) -> None:
+        super().__init__(seed)
+        self._constant = constant
+
+    def fit(self, X: List[Array], y: List[Array]) -> None:
+        pass
+
+    def classify(self, x: Array) -> bool:
+        return self._constant
+
+    def predict_proba(self, x: Array) -> float:
+        return 1.0 if self.classify(x) else 0.0
+
+
+def _run_sample_efficiency_analysis(X: List[Array], y: List[Array]) -> None:
+
+    # Do k-fold cross validation.
+    validation_frac = 0.1
+    num_data = len(X)
+    num_valid = int(num_data * validation_frac)
+    num_trials = 10
+
+    models = {
+        "oracle":
+        lambda: _OracleModel(seed=CFG.seed),
+        # "always-true": lambda: _ConstantModel(CFG.seed, True),
+        "always-false":
+        lambda: _ConstantModel(CFG.seed, False),
+        "mlp":
+        lambda: MLPBinaryClassifier(
+            seed=CFG.seed,
+            balance_data=CFG.mlp_classifier_balance_data,
+            max_train_iters=CFG.sampler_mlp_classifier_max_itr,
+            learning_rate=CFG.learning_rate,
+            weight_decay=CFG.weight_decay,
+            use_torch_gpu=CFG.use_torch_gpu,
+            train_print_every=CFG.pytorch_train_print_every,
+            n_iter_no_change=CFG.mlp_classifier_n_iter_no_change,
+            hid_sizes=CFG.mlp_classifier_hid_sizes,
+            n_reinitialize_tries=CFG.
+            sampler_mlp_classifier_n_reinitialize_tries,
+            weight_init="default")
+    }
+    training_data_results = {}
+    for training_frac in np.linspace(0.05, 1.0 - validation_frac, 5):
+        num_training_data = int(len(X) * training_frac)
+        model_accuracies = {}
+        for model_name, create_model in models.items():
+            print("Starting model:", model_name)
+            rng = np.random.default_rng(CFG.seed)
+            model_accuracy = []
+            for i in range(num_trials):
+                # Split the data randomly.
+                idxs = list(range(num_data))
+                rng.shuffle(idxs)
+                train_idxs = idxs[num_valid:num_valid + num_training_data]
+                valid_idxs = idxs[:num_valid]
+                X_train = np.array([X[i] for i in train_idxs])
+                y_train = np.array([y[i] for i in train_idxs])
+                X_valid = [X[i] for i in valid_idxs]
+                y_valid = [y[i] for i in valid_idxs]
+                # Train.
+                model = create_model()
+                model.fit(X_train, y_train)
+                # Predict.
+                y_pred = [model.classify(x) for x in X_valid]
+                acc = np.mean([(y == y_hat)
+                               for y, y_hat in zip(y_valid, y_pred)])
+                print(f"Trial {i} accuracy: {acc}")
+                model_accuracy.append(acc)
+            model_accuracies[model_name] = model_accuracy
+        print(f"Overall accuracies for training_frac={training_frac}")
+        print("------------------")
+        for model_name, model_accuracy in model_accuracies.items():
+            print(f"{model_name}: {np.mean(model_accuracy)}")
+        training_data_results[num_training_data] = model_accuracies
+
+    # Make num training data versus validation accuracy plot.
+    plt.figure()
+    plt.title("Spot Place Offline Sample Complexity Analysis")
+    plt.xlabel("# Training Examples for Sampler")
+    plt.ylabel("Validation Classification Accuracy")
+    xs = sorted(training_data_results)
+    for model_name in models:
+        all_ys = np.array([training_data_results[x][model_name] for x in xs])
+        ys = np.mean(all_ys, axis=1)
+        ys_std = np.std(all_ys, axis=1)
+        plt.plot(xs, ys, label=model_name)
+        plt.fill_between(xs, ys - ys_std, ys + ys_std, alpha=0.2)
+    plt.legend()
+    plt.xlim(left=0)
+    plt.ylim(-0.1, 1.1)
+    outfile = "spot_place_sample_complexity.png"
+    plt.savefig(outfile)
+    print(f"Wrote out to {outfile}")
 
 
 if __name__ == "__main__":
