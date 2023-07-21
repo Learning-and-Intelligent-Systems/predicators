6ca408fa085aaad4accfaecfc8f30a5382353a8e
Tom Silver <tsilver@theaiinstitute.com>
2023-07-21 09:49:20 -0400

    revert downstream changes

--- predicators/explorers/active_sampler_explorer.py
+++ predicators/explorers/active_sampler_explorer.py
@@ -1,13 +1,8 @@
 """An explorer for active sampler learning."""
 
-import glob
 import logging
-import os
-import re
-import time
 from typing import Callable, Dict, Iterator, List, Optional, Set
 
-import dill as pkl
 import numpy as np
 from gym.spaces import Box
 
@@ -49,8 +44,6 @@ class ActiveSamplerExplorer(BaseExplorer):
         self._nsrts = nsrts
         self._ground_op_hist = ground_op_hist
         self._last_executed_nsrt: Optional[_GroundNSRT] = None
-        self._last_executed_option: Optional[_Option] = None
-        self._last_init_option_state: Optional[State] = None
         self._nsrt_to_explorer_sampler = nsrt_to_explorer_sampler
 
     @classmethod
@@ -81,7 +74,6 @@ class ActiveSamplerExplorer(BaseExplorer):
         next_practice_nsrt: Optional[_GroundNSRT] = None
 
         def _option_policy(state: State) -> _Option:
-            logging.info("[Explorer] Option policy called.")
             nonlocal assigned_task_goal_reached, current_policy, \
                 next_practice_nsrt
 
@@ -171,12 +163,11 @@ class ActiveSamplerExplorer(BaseExplorer):
         # Wrap the option policy to keep track of the executed NSRTs and if
         # they succeeded, to update the ground_op_hist.
         initialized = False
+
         def _wrapped_option_policy(state: State) -> _Option:
             nonlocal initialized
             if not initialized:
                 self._last_executed_nsrt = None
-                self._last_executed_option = None
-                self._last_init_option_state = None
                 initialized = True
             # Update ground_op_hist.
             self._update_ground_op_hist(state)
@@ -184,8 +175,6 @@ class ActiveSamplerExplorer(BaseExplorer):
             option = _option_policy(state)
             ground_nsrt = utils.option_to_ground_nsrt(option, self._nsrts)
             self._last_executed_nsrt = ground_nsrt
-            self._last_executed_option = option
-            self._last_init_option_state = state
             return option
 
         # Finalize policy.
@@ -217,39 +206,6 @@ class ActiveSamplerExplorer(BaseExplorer):
         if last_executed_op not in self._ground_op_hist:
             self._ground_op_hist[last_executed_op] = []
         self._ground_op_hist[last_executed_op].append(success)
-        # Aggressively save data after every single option execution.
-        init_state = self._last_init_option_state
-        assert init_state is not None
-        option = self._last_executed_option
-        assert option is not None
-        objects = option.objects
-        params = option.params
-        sampler_input = utils.construct_active_sampler_input(
-            init_state, objects, params, option.parent)
-        sampler_output = int(success)
-        # Now, we need to get the file location and the max
-        # datapoint id saved at this location.
-        os.makedirs(CFG.data_dir, exist_ok=True)
-        objects_tuple_str = str(tuple(nsrt.objects))
-        objects_tuple_str = objects_tuple_str.strip('()')
-        prefix = f"{CFG.data_dir}/{CFG.env}_{nsrt.name}({objects_tuple_str})_"
-        filepath_template = f"{prefix}*.data"
-        datapoint_id = 0
-        all_saved_files = glob.glob(filepath_template)
-        if all_saved_files:
-            regex_prefix = re.escape(prefix)
-            regex = f"{regex_prefix}(\\d+).data"
-            for filename in all_saved_files:
-                regex_match = re.match(regex, filename)
-                assert regex_match is not None
-                d_id = int(regex_match.groups()[0])
-                datapoint_id = max(datapoint_id, d_id + 1)
-        data = {
-            "datapoint": (sampler_input, sampler_output),
-            "time": time.time()
-        }
-        with open(f"{prefix}{datapoint_id}.data", "wb") as f:
-            pkl.dump(data, f)
 
     def _get_option_policy_for_task(self,
                                     task: Task) -> Callable[[State], _Option]:
