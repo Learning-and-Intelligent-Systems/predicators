b219f60e2d7899a8a8a3e05361ac5a9816164131
Tom Silver <tsilver@theaiinstitute.com>
2023-07-21 09:51:20 -0400

    fix bug in active sampler explorer

--- predicators/explorers/active_sampler_explorer.py
+++ predicators/explorers/active_sampler_explorer.py
@@ -170,11 +170,14 @@ class ActiveSamplerExplorer(BaseExplorer):
 
         # Wrap the option policy to keep track of the executed NSRTs and if
         # they succeeded, to update the ground_op_hist.
-        self._last_executed_nsrt = None
-        self._last_executed_option = None
-        self._last_init_option_state = None
-
+        initialized = False
         def _wrapped_option_policy(state: State) -> _Option:
+            nonlocal initialized
+            if not initialized:
+                self._last_executed_nsrt = None
+                self._last_executed_option = None
+                self._last_init_option_state = None
+                initialized = True
             # Update ground_op_hist.
             self._update_ground_op_hist(state)
             # Record last executed NSRT.
