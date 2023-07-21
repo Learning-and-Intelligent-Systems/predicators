5b6968f44dd353bae8dff4ab1799c380203ac51b
Tom Silver <tsilver@theaiinstitute.com>
2023-07-21 16:56:33 -0400

    refactor

--- predicators/utils.py
+++ predicators/utils.py
@@ -3365,3 +3365,22 @@ def roundrobin(iterables: Sequence[Iterator]) -> Iterator:
     while num_active:
         for nxt in nexts:
             yield nxt()
+
+
+def get_task_seed(train_or_test: str, task_idx: int) -> int:
+    """Parses task seed from CFG.test_env_seed_offset."""
+    assert task_idx < CFG.test_env_seed_offset
+    # SeedSequence generates a sequence of random values given an integer
+    # "entropy". We use CFG.seed to define the "entropy" and then get the
+    # n^th generated random value and use that to seed the gym environment.
+    # This is all to avoid unintentional dependence between experiments
+    # that are conducted with consecutive random seeds. For example, if
+    # we used CFG.seed + task_idx to seed the gym environment, there would
+    # be overlap between experiments when CFG.seed = 1, CFG.seed = 2, etc.
+    seed_entropy = CFG.seed
+    if train_or_test == "test":
+        seed_entropy += CFG.test_env_seed_offset
+    seed_sequence = np.random.SeedSequence(seed_entropy)
+    # Need to cast to int because generate_state() returns a numpy int.
+    task_seed = int(seed_sequence.generate_state(task_idx + 1)[-1])
+    return task_seed
