40d9c94927b6ba3798cf23bb75e47f56df511408
Tom Silver <tsilver@theaiinstitute.com>
2023-07-23 08:10:44 -0400

    add kettle knob

--- scripts/plotting/create_active_sampler_learning_plots.py
+++ scripts/plotting/create_active_sampler_learning_plots.py
@@ -90,6 +90,10 @@ PLOT_GROUPS = {
         ("Active Learning", "black", lambda df: df["EXPERIMENT_ID"].apply(
             lambda v: "kitchen-knob-only-active_sampler_learning" in v)),
     ],
+    "Kitchen": [
+        ("Active Learning", "black", lambda df: df["EXPERIMENT_ID"].apply(
+            lambda v: "kitchen-kettle-knob-active_sampler_learning" in v)),
+    ],
 }
 
 # If True, add (0, 0) to every plot.
