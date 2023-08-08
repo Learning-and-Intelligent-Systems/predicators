8f37c691008e3a166f6f4532b2dea63313ab856c
Tom Silver <tsilver@theaiinstitute.com>
2023-08-08 09:55:32 -0400

    fix

--- predicators/envs/sticky_table.py
+++ predicators/envs/sticky_table.py
@@ -34,14 +34,14 @@ class StickyTableEnv(BaseEnv):
     x_ub: ClassVar[float] = 1.0
     y_lb: ClassVar[float] = 0.0
     y_ub: ClassVar[float] = 1.0
-    cube_scale: ClassVar[float] = 0.1  # as a function of table radius
+    cube_scale: ClassVar[float] = 0.25  # as a function of table radius
 
     def __init__(self, use_gui: bool = True) -> None:
         super().__init__(use_gui)
 
         # Types
         self._cube_type = Type("robot", ["x", "y", "size", "held"])
-        self._table_type = Type("target", ["x", "y", "radius", "is_sticky"])
+        self._table_type = Type("target", ["x", "y", "radius", "sticky"])
 
         # Predicates
         self._OnTable = Predicate("OnTable",
@@ -78,12 +78,24 @@ class StickyTableEnv(BaseEnv):
 
     @property
     def types(self) -> Set[Type]:
-        return {self._cube_type, self._target_type}
+        return {self._cube_type, self._table_type}
 
     @property
     def action_space(self) -> Box:
         return Box(np.array([self.x_lb, self.y_lb], dtype=np.float32),
                    np.array([self.x_ub, self.y_ub], dtype=np.float32))
+
+    def _object_to_geom(self, obj: Object, state: State) -> utils._Geom2D:
+        if obj.is_instance(self._cube_type):
+            x = state.get(obj, "x")
+            y = state.get(obj, "y")
+            size = state.get(obj, "size")
+            return utils.Rectangle(x, y, size, size, 0.0)
+        assert obj.is_instance(self._table_type)
+        x = state.get(obj, "x")
+        y = state.get(obj, "y")
+        radius = state.get(obj, "radius")
+        return utils.Circle(x, y, radius)
 
     def render_state_plt(
             self,
@@ -99,20 +111,17 @@ class StickyTableEnv(BaseEnv):
         cube, = state.get_objects(self._cube_type)
         tables = state.get_objects(self._table_type)
         for table in tables:
-            x = state.get(table, "x")
-            y = state.get(table, "y")
-            radius = state.get(table, "radius")
             is_sticky = self._table_is_sticky(table, state)
-            circ = utils.Circle(x, y, radius)
+            circ = self._object_to_geom(table, state)
             color = sticky_table_color if is_sticky else normal_table_color
             circ.plot(ax, facecolor=color, edgecolor="black", alpha=alpha)
             if is_sticky:
-                wedge = Wedge((x, y), radius, 0, 180, fill=False, hatch="/")
+                x = state.get(table, "x")
+                y = state.get(table, "y")
+                radius = state.get(table, "radius")
+                wedge = Wedge((x, y), radius, 0, 180, fill=False, hatch="OO")
                 ax.add_artist(wedge)
-        x = state.get(cube, "x")
-        y = state.get(cube, "y")
-        size = state.get(cube, "size")
-        rect = utils.Rectangle(x, y, size, size, 0.0)
+        rect = self._object_to_geom(cube, state)
         rect.plot(ax, facecolor=cube_color, edgecolor="black", alpha=alpha)
         if caption is not None:
             plt.suptitle(caption, wrap=True)
@@ -130,15 +139,17 @@ class StickyTableEnv(BaseEnv):
             assert num_tables >= 2
             state_dict: Dict[Object, Dict[str, float]] = {}
             # Generate the tables in a ring around the center of the room.
+            origin_x = (self.x_ub - self.x_lb) / 2
+            origin_y = (self.y_ub - self.y_lb) / 2
             d = min(self.x_ub - self.x_lb, self.y_ub - self.y_lb) / 3
-            thetas = np.linspace(0, 2 * np.pi, num=num_tables)
-            angle_diff = thetas[1] - thetas[0]
-            radius = d * np.sin(angle_diff / 2)
+            thetas = np.linspace(0, 2 * np.pi, num=num_tables, endpoint=False)
             # Select the radius to prevent any overlap. Exact would be
-            # 2 * d * sin(theta / 2). Divide by 2 to be conservative.
+            # d * sin(theta / 2). Divide by 2 to be conservative.
+            angle_diff = thetas[1] - thetas[0]
+            radius = d * np.sin(angle_diff / 2) / 2
             for i, theta in enumerate(thetas):
-                x = np.cos(theta)
-                y = np.sin(theta)
+                x = d * np.cos(theta) + origin_x
+                y = d * np.sin(theta) + origin_y
                 if i > 0:
                     prefix = "normal"
                     sticky = 0.0
@@ -153,25 +164,27 @@ class StickyTableEnv(BaseEnv):
                     "sticky": sticky
                 }
             tables = sorted(state_dict)
-            rng.shuffle(tables)
+            rng.shuffle(tables)  # type: ignore
             init_table, target_table = tables[:2]
             # Create cube.
             size = radius * self.cube_scale
-            x_lb = state_dict[init_table]["x"] - radius + size
-            x_ub = state_dict[init_table]["x"] + radius - size
-            y_lb = state_dict[init_table]["y"] - radius + size
-            y_ub = state_dict[init_table]["y"] + radius - size
-            x = rng.uniform(x_lb, x_ub)
-            y = rng.uniform(y_lb, y_ub)
-            cube = Object("cube", self._cube_type)
-            state_dict[cube] = {
-                "x": x,
-                "y": y,
-                "size": size,
-                "held": 0.0,
-            }
-            state = utils.create_state_from_dict(state_dict)
-            assert self._OnTable_holds(state, [cube, init_table])
+            table_x = state_dict[init_table]["x"]
+            table_y = state_dict[init_table]["y"]
+            while True:
+                theta = rng.uniform(0, 2 * np.pi)
+                dist = rng.uniform(0, radius)
+                x = table_x + dist * np.cos(theta)
+                y = table_y + dist * np.sin(theta)
+                cube = Object("cube", self._cube_type)
+                state_dict[cube] = {
+                    "x": x,
+                    "y": y,
+                    "size": size,
+                    "held": 0.0,
+                }
+                state = utils.create_state_from_dict(state_dict)
+                if self._OnTable_holds(state, [cube, init_table]):
+                    break
             goal = {GroundAtom(self._OnTable, [cube, target_table])}
             task = EnvironmentTask(state, goal)
             tasks.append(task)
@@ -179,14 +192,13 @@ class StickyTableEnv(BaseEnv):
 
     def _OnTable_holds(self, state: State, objects: Sequence[Object]) -> bool:
         cube, table = objects
-        size = state.get(table, "size")
-        radius = state.get(table, "radius")
-        x_lb = state.get(table, "x") - radius + size
-        x_ub = state.get(table, "x") + radius - size
-        y_lb = state.get(table, "y") - radius + size
-        y_ub = state.get(table, "y") + radius - size
-        return x_lb < state.get(cube, "x") < x_ub and \
-               y_lb < state.get(cube, "y") < y_ub
+        rect = self._object_to_geom(cube, state)
+        circ = self._object_to_geom(table, state)
+        assert isinstance(rect, utils.Rectangle)
+        for x, y in rect.vertices:
+            if not circ.contains_point(x, y):
+                return False
+        return True
 
     def _OnFloor_holds(self, state: State, objects: Sequence[Object]) -> bool:
         cube, = objects
@@ -202,5 +214,8 @@ class StickyTableEnv(BaseEnv):
     def _HandEmpty_holds(self, state: State,
                          objects: Sequence[Object]) -> bool:
         assert not objects
-        cube, = state.get(self._cube_type)
+        cube, = state.get_objects(self._cube_type)
         return not self._Holding_holds(state, [cube])
+
+    def _table_is_sticky(self, table: Object, state: State) -> bool:
+        return state.get(table, "sticky") > 0.5
