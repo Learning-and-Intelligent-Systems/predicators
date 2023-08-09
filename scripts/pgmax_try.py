5ee0294e444e83da9c6d2ad255241e8d3282522f
Tom Silver <tsilver@theaiinstitute.com>
2023-08-09 14:40:59 -0400

    wip

--- scripts/pgmax_try.py
+++ scripts/pgmax_try.py
@@ -1,24 +1,22 @@
+from functools import partial
+
 import jax
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
 import numpy as np
-
-from functools import partial
-
-from scipy.stats import beta, bernoulli
-from scipy.special import logsumexp
-
 from pgmax import fgraph, fgroup, infer, vgroup
-
-
+from scipy.special import logsumexp
+from scipy.stats import bernoulli, beta
 
 # Quantizing.
 num_quants = 20
+
 
 def _get_quant_centers(start=0.0, end=1.0, num_quants=num_quants):
     lefts = np.linspace(start, end, num_quants, endpoint=False)
     delta = lefts[1] - lefts[0]
     return (lefts + delta / 2).squeeze()
+
 
 def _quantize(fn, start=0.0, end=1.0, num_quants=num_quants):
     centers = _get_quant_centers(start, end, num_quants)
@@ -26,9 +24,11 @@ def _quantize(fn, start=0.0, end=1.0, num_quants=num_quants):
     z = logsumexp(unnormed)
     return unnormed - z
 
+
 def _quant_to_value(quant, start=0.0, end=1.0, num_quants=num_quants):
     centers = _get_quant_centers(start, end, num_quants)
     return centers[quant]
+
 
 def _quantize2d(fn, start=0.0, end=1.0, num_quants=num_quants):
     # TODO figure out way to unify this.
@@ -59,9 +59,10 @@ cum_num_outcomes = np.cumsum(all_num_outcomes)
 obs_variable_groups = []
 for cycle, cycle_outcomes in enumerate(outcomes):
     num_outcomes = len(cycle_outcomes)
-    obs_cycle_group = vgroup.NDVarArray(num_states=2, shape=(num_outcomes,))
+    obs_cycle_group = vgroup.NDVarArray(num_states=2, shape=(num_outcomes, ))
     obs_variable_groups.append(obs_cycle_group)
-competence_variable_group = vgroup.NDVarArray(num_states=num_quants, shape=(num_cycles,))
+competence_variable_group = vgroup.NDVarArray(num_states=num_quants,
+                                              shape=(num_cycles, ))
 variable_groups = obs_variable_groups + [competence_variable_group]
 fg = fgraph.FactorGraph(variable_groups=variable_groups)
 
@@ -70,11 +71,15 @@ factors = []
 
 # Create prior factor for initial competence.
 a, b = 0.5, 0.5  # beta distribution parameters
+
+
 def initial_competence_log_potential(competence):
     return beta.logpdf(competence, a, b)
 
-quantized_initial_competence_log_potentials = _quantize(initial_competence_log_potential)
-        
+
+quantized_initial_competence_log_potentials = _quantize(
+    initial_competence_log_potential)
+
 initial_competence_prior = fgroup.EnumFactorGroup(
     variables_for_factors=[[competence_variable_group[0]]],
     factor_configs=np.arange(num_quants)[:, None],
@@ -89,15 +94,18 @@ for cycle, cycle_outcomes in enumerate(outcomes):
     for i, out in enumerate(cycle_outcomes):
         visible_log_potentials[i, int(out)] = 0
     cycle_visible_factor = fgroup.EnumFactorGroup(
-        variables_for_factors=[[obs_variable_groups[cycle][i]] for i in range(num_outcomes)],
+        variables_for_factors=[[obs_variable_groups[cycle][i]]
+                               for i in range(num_outcomes)],
         factor_configs=np.arange(2)[:, None],
         log_potentials=visible_log_potentials,
     )
     factors.append(cycle_visible_factor)
 
+
 # Create observation model factors.
 def observation_log_potential(outcome, competence):
     return bernoulli.logpmf(outcome, competence)
+
 
 quantized_observation_log_potentials = np.array([
     _quantize(partial(observation_log_potential, False)),
@@ -107,42 +115,49 @@ quantized_observation_log_potentials = np.array([
 for cycle, cycle_outcomes in enumerate(outcomes):
     num_outcomes = len(cycle_outcomes)
     cycle_observation_factor = fgroup.PairwiseFactorGroup(
-        variables_for_factors=[[obs_variable_groups[cycle][i], competence_variable_group[cycle]] for i in range(num_outcomes)],
+        variables_for_factors=[[
+            obs_variable_groups[cycle][i], competence_variable_group[cycle]
+        ] for i in range(num_outcomes)],
         log_potential_matrix=quantized_observation_log_potentials,
     )
     factors.append(cycle_observation_factor)
 
 # Create competence transition factors.
 # TODO make these hyperparameters RVs.
-logistic_x0 = 50
-logistic_k = 1
-logistic_L = 1
-# y = L / (1 + exp(-k(x - x0)))
-# inverse: x0 + k^(-1)(-log(-1 + L/x))
-def _generalized_logit(x):
-    return logistic_x0 + 1/(logistic_k) * (-np.log(logistic_L / x - 1))
+progress_max = 1.0
+progress_scale = 1.0
+progress_midpoint = 50
+logistic = lambda x: progress_max / (1 + np.exp(-progress_scale *
+                                                (x - progress_midpoint)))
+
 
 def create_competence_transition_log_potential(current_cycle):
     current_cum_outcomes = cum_num_outcomes[current_cycle]
     next_cum_outcomes = cum_num_outcomes[current_cycle + 1]
     assert next_cum_outcomes >= current_cum_outcomes
-    expected_gain = next_cum_outcomes - current_cum_outcomes
+    # Expected gain is less when we already have a lot of data.
+    current_logistic = logistic(current_cum_outcomes)
+    next_logistic = logistic(next_cum_outcomes)
+    expected_gain = next_logistic - current_logistic
+    assert expected_gain >= 0
 
-    def competence_transition_log_potential(current_competence, next_competence):
-        current_logit = _generalized_logit(current_competence)
-        next_logit = _generalized_logit(next_competence)
-        gain = next_logit - current_logit
-        if gain < 0:  # can't get worse!
+    def competence_transition_log_potential(current_competence,
+                                            next_competence):
+        if next_competence < current_competence:  # can't get worse!
             return -np.inf
-        return -(expected_gain - gain)**2
-    
+        gain = next_competence - current_competence
+        return (expected_gain - gain)**2
+
     return competence_transition_log_potential
 
-for cycle in range(num_cycles-1):
+
+for cycle in range(num_cycles - 1):
     current_competence_var = competence_variable_group[cycle]
     next_competence_var = competence_variable_group[cycle + 1]
-    competence_transition_log_potential = create_competence_transition_log_potential(cycle)
-    quantized_competence_transition_log_potential = _quantize2d(competence_transition_log_potential)
+    competence_transition_log_potential = create_competence_transition_log_potential(
+        cycle)
+    quantized_competence_transition_log_potential = _quantize2d(
+        competence_transition_log_potential)
     cycle_competence_transition_factor = fgroup.PairwiseFactorGroup(
         variables_for_factors=[[current_competence_var, next_competence_var]],
         log_potential_matrix=quantized_competence_transition_log_potential,
@@ -160,8 +175,13 @@ map_states = infer.decode_map_states(beliefs)
 
 # Print results.
 print("Inference results...")
-map_competences = [_quant_to_value(map_states[competence_variable_group][i]) for i in range(num_cycles)]
+map_competences = [
+    _quant_to_value(map_states[competence_variable_group][i])
+    for i in range(num_cycles)
+]
 for cycle in range(num_cycles):
     print(f"Competence {cycle}: {map_competences[cycle]}")
 
-import ipdb; ipdb.set_trace()
+import ipdb
+
+ipdb.set_trace()
