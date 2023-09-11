84849ed2511b97611a5cd3ce5da5c2f82ad8c071
Nishanth Kumar <nishanth.kumar20@gmail.com>
2023-09-10 22:12:22 -0400

    linting

--- predicators/settings.py
+++ predicators/settings.py
@@ -536,7 +536,8 @@ class GlobalSettings:
     active_sampler_learning_num_samples = 100
     active_sampler_learning_score_gamma = 0.5
     active_sampler_learning_fitted_q_iters = 5
-    active_sampler_learning_n_iter_no_change = 5000  # shared with maple q function learning
+    # shared with maple q function learning
+    active_sampler_learning_n_iter_no_change = 5000
     active_sampler_learning_num_lookahead_samples = 5
     active_sampler_learning_explore_length_base = 2
     active_sampler_learning_num_ensemble_members = 10
