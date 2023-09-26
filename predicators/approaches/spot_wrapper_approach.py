b5df86679476d4516a459fd1c15c2f8be7b63677
Tom Silver <tsilver@theaiinstitute.com>
2023-09-26 13:37:06 -0400

    wip

--- predicators/approaches/spot_wrapper_approach.py
+++ predicators/approaches/spot_wrapper_approach.py
@@ -17,6 +17,7 @@ from gym.spaces import Box
 
 from predicators import utils
 from predicators.approaches import BaseApproach, BaseApproachWrapper
+from predicators.spot_utils.skills.spot_find_objects import find_object
 from predicators.structs import Action, Object, ParameterizedOption, \
     Predicate, State, Task, Type
 
@@ -65,7 +66,10 @@ class SpotWrapperApproach(BaseApproachWrapper):
                 base_approach_policy = None
                 need_stow = True
                 self._base_approach_has_control = False
-                raise NotImplementedError("Coming soon!")
+                return utils.create_spot_env_action(
+                    "execute",
+                    [],
+                )
             # Found the objects. Stow the arm before replanning.
             if need_stow:
                 logging.info("[Spot Wrapper] Lost objects found, stowing.")
