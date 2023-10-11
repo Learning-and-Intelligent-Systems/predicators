51e1e27b87de67a5dfc0ce36f9226b973e1748dc
Bartłomiej Cieślar <bcieslar2001@gmail.com>
2023-10-10 20:17:59 -0400

    MR changes

--- predicators/approaches/delivery_approach.py
+++ predicators/approaches/delivery_approach.py
@@ -5,18 +5,21 @@ Example command line:
         --env pddl_easy_delivery_procedural_tasks
 """
 
-from typing import Callable, Iterable, Optional, Set, cast
+from typing import Callable, Iterable, List, Optional, Set, cast
 
 import numpy as np
+from gym.spaces.box import Box
 
 from predicators.approaches import ApproachFailure, BaseApproach
 from predicators.envs.pddl_env import _PDDLEnvState
-from predicators.structs import Action, GroundAtom, Object, Predicate, State, \
-    Task, _Option
+from predicators.structs import Action, GroundAtom, Object, \
+    ParameterizedOption, Predicate, State, Task, Type, _Option
+
+__all__ = ["DeliverySpecificApproach"]
 
 
 def filter_predicated_objs(objs: Iterable[Object], pred: Predicate,
-                           ground_atoms: Iterable[Object]) -> Set[GroundAtom]:
+                           ground_atoms: Iterable[GroundAtom]) -> Set[Object]:
     """Filters the objects based on whether they fulfill a unitary predicate in
     a given set of ground atoms.
 
@@ -27,6 +30,10 @@ def filter_predicated_objs(objs: Iterable[Object], pred: Predicate,
 
 
 def check_option(ground_option: _Option, state: _PDDLEnvState) -> None:
+    """Makes sure that the ground option is initiable on the state.
+
+    If not, throws an exception.
+    """
     if not ground_option.initiable(state):
         raise ApproachFailure("Could not execute option")
 
@@ -37,8 +44,14 @@ class DeliverySpecificApproach(BaseApproach):
     See envs/assets/pddl/delivery/domain.pddl for the domain definition
     """
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, initial_predicates: Set[Predicate],
+                 initial_options: Set[ParameterizedOption], types: Set[Type],
+                 action_space: Box, train_tasks: List[Task]):
+        super().__init__(initial_predicates=initial_predicates,
+                         initial_options=initial_options,
+                         types=types,
+                         action_space=action_space,
+                         train_tasks=train_tasks)
 
         # Extracts domain-specific class instances for the delivery domain
         types = {t.name: t for t in self._types}
@@ -74,7 +87,7 @@ class DeliverySpecificApproach(BaseApproach):
         if len(at_locs) > 1:
             raise ApproachFailure(
                 "Cannot be at multiple locations at the same time")
-        return next(iter(at_locs), None)
+        return min(at_locs, default=None)
 
     def _extract_at_loc(self, locs: Iterable[Object],
                         ground_atoms: Iterable[GroundAtom]) -> Object:
@@ -106,8 +119,8 @@ class DeliverySpecificApproach(BaseApproach):
 
     def _solve(self, task: Task, timeout: int) -> Callable[[State], Action]:
         # Extracting task-specific information
-        init_state = cast(_PDDLEnvState, task.init)
-        init_ground_atoms = init_state.get_ground_atoms()
+        assert isinstance(_PDDLEnvState, task.init)
+        init_ground_atoms = task.init.get_ground_atoms()
         obj_locs = set(task.init.get_objects(self.type_loc))
         obj_papers = set(task.init.get_objects(self.type_paper))
 
@@ -117,13 +130,10 @@ class DeliverySpecificApproach(BaseApproach):
         obj_home_bases = filter_predicated_objs(obj_locs,
                                                 self.pred_is_home_base,
                                                 init_ground_atoms)
-        obj_home_base = next(iter(obj_home_bases), None)
+        obj_home_base = min(obj_home_bases, default=None)
 
         # Extracting goal objects
         goal_at_loc = self._extract_at_loc_optional(obj_locs, task.goal)
-        goal_wants_paper = filter_predicated_objs(obj_locs,
-                                                  self.pred_wants_paper,
-                                                  task.goal)
         goal_satisfied = filter_predicated_objs(obj_locs, self.pred_satisfied,
                                                 task.goal)
         goal_unpacked = filter_predicated_objs(obj_papers, self.pred_unpacked,
@@ -160,8 +170,9 @@ class DeliverySpecificApproach(BaseApproach):
                 if obj_at_loc != obj_home_base:
                     return self._move(state, obj_at_loc, obj_home_base)
 
-                obj_satisfying_paper = next(
-                    iter(obj_satisfying_papers - obj_carried_papers), None)
+                obj_satisfying_paper = min(obj_satisfying_papers -
+                                           obj_carried_papers,
+                                           default=None)
                 if obj_satisfying_paper is None:
                     raise ApproachFailure(
                         "Too little paper that can satisfy locations")
@@ -170,10 +181,11 @@ class DeliverySpecificApproach(BaseApproach):
 
             # If already at a location to satisfy, satisfy it
             if obj_at_loc in obj_locs_to_satisfy:
-                carried_paper = obj_carried_papers.pop()
+                carried_paper = min(obj_carried_papers)
                 return self._deliver(state, carried_paper, obj_at_loc)
 
-            # Go to some location to satisfy if needed (satisfies all the "satisfied" goals)
+            # Go to some location to satisfy if needed
+            # (satisfies all the "satisfied" goals)
             for obj_loc_to_satisfy in obj_locs_to_satisfy:
                 return self._move(state, obj_at_loc, obj_loc_to_satisfy)
 
@@ -185,13 +197,11 @@ class DeliverySpecificApproach(BaseApproach):
                     return self._move(state, obj_at_loc, obj_home_base)
                 return self._pick_up(state, obj_paper_to_carry, obj_home_base)
 
-            # Finally go to the desired goal location (satisfies the "at_loc" goal)
+            # Finally go to the desired goal location
+            # (satisfies the "at_loc" goal)
             if goal_at_loc and goal_at_loc != obj_at_loc:
                 return self._move(state, obj_at_loc, goal_at_loc)
 
             ApproachFailure("Ran out of things to do")
 
         return _policy
-
-
-__all__ = [DeliverySpecificApproach]
