950cda8fccdb53ad8c6669768cedd4b1bba6853e
Ryan Yang <ryanyang@Ryans-MacBook-Pro.local>
2023-11-08 17:58:35 -0500

    added plan correctness for nsrt variables only

--- predicators/approaches/gpt_analogy_approach.py
+++ predicators/approaches/gpt_analogy_approach.py
@@ -15,12 +15,14 @@ from typing import Any, Dict, Iterator, List, Set, Tuple
 
 import smepy
 import numpy as np
+import itertools
 
 from predicators.approaches.pg3_analogy_approach import PG3AnalogyApproach
 from predicators.envs.base_env import BaseEnv
 from predicators.settings import CFG
 from predicators.structs import NSRT, LDLRule, LiftedAtom, \
     LiftedDecisionList, Predicate, Type, Variable
+from predicators import utils
 
 
 class GPTAnalogyApproach(PG3AnalogyApproach):
@@ -29,6 +31,12 @@ class GPTAnalogyApproach(PG3AnalogyApproach):
     @classmethod
     def get_name(cls) -> str:
         return "gpt_pg3"
+
+    def learn_from_offline_dataset(self, dataset: Dataset) -> None:
+        # Get a task and trajectory for correctness of generated conditions
+        self._correctness_task = self._train_tasks[0]
+        self._correctness_traj = dataset.trajectories[0]._states
+        super().learn_from_offline_dataset(dataset)
 
     def _induce_policies_by_analogy(
             self, base_policy: LiftedDecisionList, base_env: BaseEnv,
@@ -41,7 +49,7 @@ class GPTAnalogyApproach(PG3AnalogyApproach):
         target_policies: List[LiftedDecisionList] = []
         for analogy in analogies:
             # Use the analogy to create an initial policy for the target env.
-            target_policy = _apply_analogy_to_ldl(analogy, base_policy)
+            target_policy = _apply_analogy_to_ldl(analogy, base_policy, self._correctness_traj, self._correctness_task, self._initial_predicates)
             target_policies.append(target_policy)
         return target_policies
 
@@ -153,7 +161,8 @@ def _find_env_analogies(base_env: BaseEnv, target_env: BaseEnv,
 
 
 def _apply_analogy_to_ldl(analogy: _Analogy,
-                          ldl: LiftedDecisionList) -> LiftedDecisionList:
+                          ldl: LiftedDecisionList, 
+                          correctness_traj, correctness_task, initial_predicates) -> LiftedDecisionList:
     new_rules = []
     for rule in ldl.rules:
         # Can't create a rule if there is no NSRT match.
@@ -163,10 +172,10 @@ def _apply_analogy_to_ldl(analogy: _Analogy,
         for new_rule_nsrt in new_rule_nsrts:
             new_rule_name = rule.name 
             nsrt_name_pair = (rule.nsrt.name, new_rule_nsrt.name)
-            new_rule_pos_preconditions = _apply_analogy_to_atoms(analogy, nsrt_name_pair, rule.pos_state_preconditions)
+            new_rule_pos_preconditions = _apply_analogy_to_atoms(analogy, nsrt_name_pair, rule.pos_state_preconditions, new_rule_nsrt, "pos_state_preconditions", correctness_traj, correctness_task, initial_predicates)
             new_rule_pos_preconditions.update(new_rule_nsrt.preconditions)
-            new_rule_neg_preconditions = _apply_analogy_to_atoms(analogy, nsrt_name_pair, rule.neg_state_preconditions)
-            new_rule_goal_preconditions = _apply_analogy_to_atoms(analogy, nsrt_name_pair, rule.goal_preconditions)
+            new_rule_neg_preconditions = _apply_analogy_to_atoms(analogy, nsrt_name_pair, rule.neg_state_preconditions, new_rule_nsrt, "neg_state_preconditions", correctness_traj, correctness_task, initial_predicates)
+            new_rule_goal_preconditions = _apply_analogy_to_atoms(analogy, nsrt_name_pair, rule.goal_preconditions, new_rule_nsrt, "goal_preconditions", correctness_traj, correctness_task, initial_predicates)
             all_atoms = new_rule_pos_preconditions | new_rule_neg_preconditions | \
                         new_rule_goal_preconditions
             new_rule_params_set = {v for a in all_atoms for v in a.variables}
@@ -181,19 +190,7 @@ def _apply_analogy_to_ldl(analogy: _Analogy,
     return LiftedDecisionList(new_rules)
 
 
-def _create_variable_mapping_for_rule(
-        analogy: _Analogy, rule: LDLRule) -> Dict[Variable, Variable]:
-    old_to_new_var = analogy.base_nsrt_to_variable_analogy(rule.nsrt).copy()
-    for old_var in rule.parameters:
-        if old_var in old_to_new_var:
-            continue
-        new_var_type = analogy.types[old_var.type]
-        new_var = Variable(old_var.name, new_var_type)
-        old_to_new_var[old_var] = new_var
-    return old_to_new_var
-
-
-def _apply_analogy_to_atoms(analogy: _Analogy, nsrt_pair: Tuple[str, str], atoms: Set[LiftedAtom]) -> Set[LiftedAtom]:
+def _apply_analogy_to_atoms(analogy: _Analogy, nsrt_pair: Tuple[str, str], atoms: Set[LiftedAtom], new_rule_nsrt: NSRT, atom_location: str, correctness_traj, correctness_task, initial_predicates) -> Set[LiftedAtom]:
     new_atoms: Set[LiftedAtom] = set()
     for atom in atoms:
         # Can't create an atom if there is no predicate match.
@@ -202,35 +199,69 @@ def _apply_analogy_to_atoms(analogy: _Analogy, nsrt_pair: Tuple[str, str], atoms
         new_predicates = analogy.predicates[atom.predicate]
 
         for new_predicate in new_predicates:
-            new_variables = _get_variables(new_predicate, analogy, nsrt_pair, atoms)
+            new_variables = _generate_variables(new_predicate, atom, analogy, nsrt_pair, new_rule_nsrt, atom_location, correctness_traj, correctness_task, initial_predicates)
 
-        """
-        new_variables = []
-        for var in atom.variables:
-            new_var = None
-            if var in analogy.variables[nsrt_pair]:
-                new_var = analogy.variables[nsrt_pair][var]
-            else:
-                new_var = var
-            
-            if new_var != None:
-                new_variables.append(new_var)
-
-        
-        for new_predicate in new_predicates:
-
-            if new_predicate.arity < len(new_variables):
-                new_variables = new_variables[:new_predicate.arity] # TODO: FIX THIS IS VERY HACKY
-            else:
-                possible_variables = []
-                for var in analogy.variables[nsrt_pair].values():
-                    if var is not None:
-                        possible_variables.append(var)
-
-                new_variables = np.random.choice(possible_variables, new_predicate.arity)
-            # print(LiftedAtom(new_predicate, new_variables))
-            # import ipdb; ipdb.set_trace();
-            new_atoms.add(LiftedAtom(new_predicate, new_variables)) 
-        """
     return new_atoms
+
+def _generate_variables(predicate, atom: LiftedAtom, analogy: _Analogy, nsrt_pair: Tuple[str, str], new_rule_nsrt: NSRT, atom_location: str, correctness_traj, correctness_task, initial_predicates):
+    # 3 cases: 
+    # new predicate arity > old predicate arity, 
+    # new predicate arity = old predicate arity,
+    # new predicate arity < old predicate arity
+    # what if a variable isn't one of the default ones?
+
+    # Generating must use variables
+    must_use_variables = []
+    for var in atom.variables:
+        if var in analogy.variables[nsrt_pair] and analogy.variables[nsrt_pair][var] != None:
+            must_use_variables.append(analogy.variables[nsrt_pair][var])
+    
+    # Only use given variables
+    if len(must_use_variables) >= predicate.arity:
+        all_possible_params = list(itertools.permutations(must_use_variables, predicate.arity))
+        for candidate_param in all_possible_params:
+            temporary_condition = predicate(candidate_param)
+            temporary_ldl_rules = []
+            # TODO: ONE HACK IS WE ONLY USE ONE TRAJECTORY (SO IF IT DOESN"T USE EVERY NSRT, THAT"S A PROBLEM) - USE ALL PROBLEMS?
+            temporary_params = sorted(set(candidate_param) | set(new_rule_nsrt.parameters))
+            temporary_preconditions = new_rule_nsrt.preconditions
+            temporary_negative_preconditions = set()
+            temporary_goalconditions = set()
+            
+            if atom_location == "pos_state_preconditions":
+                temporary_preconditions.add(temporary_condition)
+            elif atom_location == "neg_state_preconditions":
+                temporary_negative_preconditions.add(temporary_condition)
+            elif atom_location == "goal_preconditions":
+                temporary_goalconditions.add(temporary_condition)
+            else:
+                raise Exception("No valid atom location!")
+
+            new_rule = LDLRule("temporary-rule", temporary_params, temporary_preconditions, temporary_negative_preconditions, temporary_goalconditions, new_rule_nsrt)
+            temporary_ldl_rules.append(new_rule)
+            temporary_ldl = LiftedDecisionList(temporary_ldl_rules)
+            temp_init_atoms = utils.abstract(correctness_task.init, initial_predicates)
+            found_valid_action = False
+            for i in range(len(correctness_traj)-1):
+                ans = utils.query_ldl(temporary_ldl, correctness_traj[i].simulator_state, correctness_traj[0].data, correctness_task.goal, static_predicates=initial_predicates, init_atoms= correctness_traj[i].simulator_state) #TODO: CHECK INIT ATOMS (DOESN'T WORK OTHERWISE?)
+                if ans != None:
+                    found_valid_action = True
+                    break
+            
+            if found_valid_action:
+                return candidate_param
+            else:
+                raise AssertionError("WE NEED VALID PARAMS SOMEWHERE!")
+
+    # Need to generate new variables
+    else:
+
+    # print("WANT:", predicate, predicate.arity)
+    # print("POTENTIALS:", must_use_variables)
+    # print(atom.predicate, atom.predicate.arity)
+    print()
+
+
+    if atom.predicate.arity != predicate.arity:
+        import ipdb; ipdb.set_trace();
 
