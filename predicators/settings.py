246680f888c1faebe3ada10ac326c12cd66a6ed9
Tom Silver <tomssilver@gmail.com>
2023-12-15 16:10:06 -0500

    add active_sampler_explorer_skip_perfect

--- predicators/settings.py
+++ predicators/settings.py
@@ -603,6 +603,7 @@ class GlobalSettings:
     active_sampler_explorer_replan_frequency = 100
     active_sampler_explorer_planning_progress_max_tasks = 10
     active_sampler_explorer_planning_progress_max_replan_tasks = 5
+    active_sampler_explorer_skip_perfect = False
     active_sampler_learning_init_cycles_to_pursue_goal = 1
 
     # grammar search invention parameters
