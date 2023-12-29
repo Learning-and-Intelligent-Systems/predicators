28ac5c409cce09783414b909a77c21d8ee93fa7f
Tom Silver <tomssilver@gmail.com>
2023-12-29 11:47:35 -0500

    sorry

--- predicators/explorers/active_sampler_explorer.py
+++ predicators/explorers/active_sampler_explorer.py
@@ -88,7 +88,7 @@ class ActiveSamplerExplorer(BaseExplorer):
         logger_name = f"{utils.get_config_path_str()}-explore"
         self._logger = logging.getLogger(logger_name)
         self._logger.setLevel(logging.DEBUG)
-        os.path.makedirs(CFG.log_dir, exist_ok=True)
+        os.makedirs(CFG.log_dir, exist_ok=True)
         fh = logging.FileHandler(f"{CFG.log_dir}/{logger_name}.log")
         fh.setLevel(logging.DEBUG)
         self._logger.addHandler(fh)
