ec19b1cdd77c50af415a6bbf060f01ece3ada1da
Tom Silver <tsilver@theaiinstitute.com>
2024-01-19 17:32:05 -0500

    yapf

--- predicators/spot_utils/perception/object_specific_grasp_selection.py
+++ predicators/spot_utils/perception/object_specific_grasp_selection.py
@@ -102,7 +102,7 @@ def _get_chair_grasp_pixel(
     rgbds: Dict[str, RGBDImageWithContext], artifacts: Dict[str, Any],
     camera_name: str, rng: np.random.Generator
 ) -> Tuple[Tuple[int, int], Optional[math_helpers.Quat]]:
-    del rng,  #rgbds
+    del rng, rgbds
     detections = artifacts["language"]["object_id_to_img_detections"]
     try:
         seg_bb = detections[chair_obj][camera_name]
@@ -125,27 +125,16 @@ def _get_chair_grasp_pixel(
                                          lo,
                                          hi,
                                          min_component_size=10)
-    # # This can happen sometimes. If that happens, just pick the center top pixel in
-    # # the mask, which should be the rim.
-    # if centroid is None:
-    #     mask_args = np.argwhere(mask)
-    #     mask_min_c = min(mask_args[:, 1])
-    #     mask_max_c = max(mask_args[:, 1])
-    #     c_len = mask_max_c - mask_min_c
-    #     middle_c = mask_min_c + c_len // 2
-    #     min_r = min(r for r, c in mask_args if c == middle_c)
-    #     selected_pixel = (middle_c, min_r)
-    # else:
     pixel = (centroid[0], centroid[1])
 
     # Uncomment for debugging. Make sure also to not del rgbds (above).
-    rgbd = rgbds[camera_name]
-    bgr = cv2.cvtColor(rgbd.rgb, cv2.COLOR_RGB2BGR)
-    cv2.circle(bgr, pixel, 5, (0, 255, 0), -1)
-    cv2.circle(bgr, pixel, 5, (255, 0, 0), -1)
-    cv2.imshow("Selected grasp", bgr)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # rgbd = rgbds[camera_name]
+    # bgr = cv2.cvtColor(rgbd.rgb, cv2.COLOR_RGB2BGR)
+    # cv2.circle(bgr, pixel, 5, (0, 255, 0), -1)
+    # cv2.circle(bgr, pixel, 5, (255, 0, 0), -1)
+    # cv2.imshow("Selected grasp", bgr)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
     return pixel, None
 
 
