large fix for weird silent bugs

predicators/explorers/active_sampler_explorer.py
@@ -465,9 +465,9 @@ class ActiveSamplerExplorer(BaseExplorer):
             max_horizon=np.inf)
 
         # Uncomment for debugging
-        # for act in plan:
-        #     print((act.name, act.objects))
-        # import ipdb; ipdb.set_trace()
+        for act in plan:
+            print((act.name, act.objects))
+        import ipdb; ipdb.set_trace()
 
         return utils.nsrt_plan_to_greedy_option_policy(
             plan, task.goal, self._rng, necessary_atoms_seq=atoms_seq)
@@ -612,6 +612,10 @@ class ActiveSamplerExplorer(BaseExplorer):
             except (PlanningFailure, PlanningTimeout):  # pragma: no cover
                 self._log("WARNING: task planning failed in the explorer.")
                 self._task_plan_cache[task_id] = None
+
+        # Uncomment for debugging.
+        # for act in plan:
+        #     print((act.name, act.objects))
 
         self._task_plan_calls_since_replan[task_id] += 1
         return self._task_plan_cache[task_id]

predicators/ground_truth_models/spot_env/options.py
@@ -326,7 +326,7 @@ def _sweep_objects_into_container_policy(name: str, robot_obj_idx: int,
 
     start_x = mean_x + 0.175
     start_y = mean_y + 0.3
-    start_z = mean_z + 0.235
+    start_z = mean_z + 0.24
     pitch = math_helpers.Quat.from_pitch(np.pi / 2)
     yaw = math_helpers.Quat.from_yaw(np.pi / 4)
     rot = pitch * yaw