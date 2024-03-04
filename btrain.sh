18ee80ecac810bae231ce25aa9a027a56f9cde13
Nishanth Kumar <NishanthJKumar@users.noreply.github.com>
2024-03-04 10:08:31 -0500

    add nishanth's update

--- btrain.sh
+++ btrain.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 # Define the environments
-# environments=("cover_multistep_options")
-environments=("cover_multistep_options" "doors" "stick_button" "coffee")
+# environments=("doors")
+environments=("doors" "coffee")
+# environments=("cover_multistep_options" "doors" "stick_button" "coffee")
 
 # Define the number of training tasks
+# num_train_tasks=(50)
 num_train_tasks=(50 200 500 750 1000)
+
+# segmenter
+# segmenter = "option_changes"
+segmenter = "contact"
 
 # Loop over the environments
 for env in "${environments[@]}"; do
     # Loop over the number of training tasks
     for num in "${num_train_tasks[@]}"; do
-        sbatch -J "${env}_${num}" execute.sh python predicators/main.py \
-            --env $env --num_train_tasks $num --seed 0 --experiment_id $num\
-            --approach grammar_search_invention --option_learner direct_bc \
-            --min_perc_data_for_nsrt 1 --segmenter contacts --timeout 300 \
-            --excluded_predicates all
+        sbatch -J "oc_${env}_${num}_+" execute.sh python predicators/main.py \
+            --env $env --num_train_tasks $num --seed 0 \
+            --experiment_id "${num}_${segmenter}" \
+            --approach grammar_search_invention --option_learner no_learning \
+            --min_perc_data_for_nsrt 1 --segmenter $segmenter \
+            --timeout 300 --excluded_predicates all 
     done
 done
