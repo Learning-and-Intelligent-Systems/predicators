06e885efb5b0d2d0ecad44b9c2cf6bf6605ea4e5
Nishanth Kumar <NishanthJKumar@users.noreply.github.com>
2024-03-23 00:05:53 -0400

    Adds new variant of stick button + flag for pruning grammar (#1619)
    * adds new flag to deactivate pruned grammar
    
    * initial euclidean distance grammar implementation
    
    * add command line flags
    
    * need to modify stick button to contain movement options
    
    * adds nsrts and options correctly!
    
    * oracle works for new stick_button env!
    
    * gets debug grammar mostly working! 37/50!
    
    * good to go
    
    * revert ipdb
    
    * lots of type and linting fixes
    
    * remove cov files
    
    * all tests
    
    * fixes linting
    
    * fixes tests and other things
    
    * adds new predicate to get 50/50 with debug grammar!
    
    * rerun OG predicators experiments
    
    * OG gets 41/40
    
    * adds new predicates and operators to get oracle to 50/50!
    
    * revert
    
    * good to go hopefully!
    
    * isort
    
    * fixes tests!
    
    * revert bad changes

--- tests/envs/test_stick_button.py
+++ tests/envs/test_stick_button.py
@@ -5,7 +5,8 @@ import numpy as np
 import pytest
 
 from predicators import utils
-from predicators.envs.stick_button import StickButtonEnv
+from predicators.envs.stick_button import StickButtonEnv, \
+    StickButtonMovementEnv
 from predicators.ground_truth_models import get_gt_nsrts, get_gt_options
 from predicators.structs import Action, EnvironmentTask, GroundAtom
 
@@ -175,7 +176,7 @@ def test_stick_button():
     assert StickPressButton.name == "StickPressButton"
 
     # Test RobotPressButton.
-    option = RobotPressButton.ground([robot, reachable_button], [])
+    option = RobotPressButton.ground([robot, reachable_button, stick], [])
     option_plan = [option]
 
     policy = utils.option_plan_to_policy(option_plan)
@@ -298,6 +299,131 @@ def test_stick_button():
         event_to_action(state, event)
     assert "Human quit" in str(e)
     plt.close()
+
+    # Special test for PlaceStick NSRT because it's not used by oracle.
+    nsrts = get_gt_nsrts(env.get_name(), env.predicates, options)
+    nsrt = next(iter(n for n in nsrts if n.name == "PlaceStickFromNothing"))
+    ground_nsrt = nsrt.ground([robot, stick])
+    rng = np.random.default_rng(123)
+    option = ground_nsrt.sample_option(state, set(), rng)
+    assert -1 <= option.params[0] <= 1
+
+
+def test_stick_button_move():
+    """Tests for the movement variant of stick button."""
+    utils.reset_config({
+        "env": "stick_button_move",
+        "stick_button_num_buttons_train": [2],
+        "stick_button_disable_angles": False,
+        "stick_button_holder_scale": 0.001,
+    })
+    env = StickButtonMovementEnv()
+    for task in env.get_train_tasks():
+        for obj in task.init:
+            assert len(obj.type.feature_names) == len(task.init[obj])
+    for task in env.get_test_tasks():
+        for obj in task.init:
+            assert len(obj.type.feature_names) == len(task.init[obj])
+    assert len(env.predicates) == 6
+    assert len(env.goal_predicates) == 1
+    AboveNoButton = [p for p in env.predicates if p.name == "AboveNoButton"][0]
+    assert {pred.name for pred in env.goal_predicates} == {"Pressed"}
+    assert len(get_gt_options(env.get_name())) == 6
+    assert len(env.types) == 4
+    button_type, holder_type, robot_type, stick_type = sorted(env.types)
+    assert button_type.name == "button"
+    assert holder_type.name == "holder"
+    assert robot_type.name == "robot"
+    assert stick_type.name == "stick"
+    assert env.action_space.shape == (4, )
+    # Create a custom initial state, with the robot in the middle, one button
+    # reachable on the left, one button out of the reachable zone in the middle,
+    # and the stick on the right at a 45 degree angle.
+    state = env.get_train_tasks()[0].init.copy()
+    robot, = state.get_objects(robot_type)
+    stick, = state.get_objects(stick_type)
+    buttons = state.get_objects(button_type)
+    assert len(buttons) == 2
+    robot_x = (env.rz_x_ub + env.rz_x_lb) / 2
+    state.set(robot, "x", robot_x)
+    state.set(robot, "y", (env.rz_y_ub + env.rz_y_lb) / 2)
+    state.set(robot, "theta", np.pi / 2)
+    reachable_button, unreachable_button = buttons
+    reachable_x = (env.rz_x_ub + env.rz_x_lb) / 4
+    state.set(reachable_button, "x", reachable_x)
+    state.set(reachable_button, "y", (env.rz_y_ub + env.rz_y_lb) / 2)
+    unreachable_x = robot_x
+    state.set(unreachable_button, "x", unreachable_x)
+    unreachable_y = 0.75 * env.y_ub
+    assert not env.rz_y_lb <= unreachable_y <= env.rz_y_ub
+    state.set(unreachable_button, "y", unreachable_y)
+    stick_x = 3 * (env.rz_x_ub + env.rz_x_lb) / 4
+    state.set(stick, "x", stick_x)
+    state.set(stick, "y", (env.rz_y_ub + env.rz_y_lb) / 4)
+    state.set(stick, "theta", np.pi / 4)
+    task = EnvironmentTask(state, task.goal)
+    env.render_state(state, task)
+    assert GroundAtom(AboveNoButton, []).holds(state)
+    ## Test options ##
+    options = get_gt_options(env.get_name())
+    PickStick, PlaceStick, RobotMoveToButton, RobotPressButton, \
+        StickMoveToButton, StickPressButton = sorted(options)
+    assert PickStick.name == "PickStick"
+    assert PlaceStick.name == "PlaceStick"
+    assert RobotPressButton.name == "RobotPressButton"
+    assert StickPressButton.name == "StickPressButton"
+    assert RobotMoveToButton.name == "RobotMoveToButton"
+    assert StickMoveToButton.name == "StickMoveToButton"
+
+    # Test PickStick.
+    option = PickStick.ground([robot, stick], [0.1])
+    option_plan = [option]
+
+    policy = utils.option_plan_to_policy(option_plan)
+    traj = utils.run_policy_with_simulator(
+        policy,
+        env.simulate,
+        task.init,
+        lambda _: False,
+        max_num_steps=1000,
+        exceptions_to_break_on={utils.OptionExecutionFailure})
+    assert traj.states[-2].get(stick, "held") < 0.5
+    assert traj.states[-1].get(stick, "held") > 0.5
+
+    # Test StickPressButton without moving first to show it doesn't work.
+    option = StickPressButton.ground([robot, stick, unreachable_button], [])
+    bad_option_plan = option_plan[:]
+    bad_option_plan.append(option)
+
+    policy = utils.option_plan_to_policy(bad_option_plan)
+    traj = utils.run_policy_with_simulator(
+        policy,
+        env.simulate,
+        task.init,
+        lambda _: False,
+        max_num_steps=1000,
+        exceptions_to_break_on={utils.OptionExecutionFailure})
+    assert traj.states[-2].get(unreachable_button, "pressed") < 0.5
+    # assert pressing didn't work.
+    assert traj.states[-1].get(unreachable_button, "pressed") < 0.5
+
+    # Test StickPressButton properly with moving first.
+    option = StickMoveToButton.ground([robot, unreachable_button, stick], [])
+    option_plan.append(option)
+    option = StickPressButton.ground([robot, stick, unreachable_button], [])
+    option_plan.append(option)
+
+    policy = utils.option_plan_to_policy(option_plan)
+    traj = utils.run_policy_with_simulator(
+        policy,
+        env.simulate,
+        task.init,
+        lambda _: False,
+        max_num_steps=1000,
+        exceptions_to_break_on={utils.OptionExecutionFailure})
+    assert traj.states[-2].get(unreachable_button, "pressed") < 0.5
+    # assert pressing worked.
+    assert traj.states[-1].get(unreachable_button, "pressed") > 0.5
 
     # Special test for PlaceStick NSRT because it's not used by oracle.
     nsrts = get_gt_nsrts(env.get_name(), env.predicates, options)
