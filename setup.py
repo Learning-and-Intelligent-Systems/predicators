504a25efdbf562def1633620c91cfeee41b876b1
Linfeng Zhao <zlf0625@gmail.com>
2024-05-02 18:02:18 -0400

    update requirement, tenacity

--- setup.py
+++ setup.py
@@ -35,7 +35,8 @@ setup(
         "pg3@git+https://github.com/tomsilver/pg3.git",
         "gym_sokoban@git+https://github.com/Learning-and-Intelligent-Systems/gym-sokoban.git",  # pylint: disable=line-too-long
         "ImageHash",
-        "google-generativeai"
+        "google-generativeai",
+        "tenacity",
     ],
     include_package_data=True,
     extras_require={
