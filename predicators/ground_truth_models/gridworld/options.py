a8f5b169fcd928384837b8dc539e2b71d84baca6
Ashay Athalye <me@ashayathalye.com>
2024-05-10 12:11:38 -0400

    Progress.

--- predicators/ground_truth_models/gridworld/options.py
+++ predicators/ground_truth_models/gridworld/options.py
@@ -74,25 +74,25 @@ class GridWorldGroundTruthOptionFactory(GroundTruthOptionFactory):
             ix = state.get(item, "col")
             iy = state.get(item, "row")
 
-            if GridWorldEnv._Facing_holds(state, [robot, item]):
-                return Action[np.array([0, 0, -1, 1, 0])]
+            if GridWorldEnv.Facing_holds(state, [robot, item]):
+                return Action[np.array([0, 0, -1, 1, 0], dtype=np.float32)]
 
-            elif GridWorldEnv._Adjacent_holds(state, [robot, item]):
+            elif GridWorldEnv.Adjacent_holds(state, [robot, item]):
                 if rx == ix:
                     if ry > iy:
-                        return Action(np.array([0, 0, 2, 0, 0]))
+                        return Action(np.array([0, 0, 2, 0, 0], dtype=np.float32))
                     elif ry < iy:
-                        return Action(np.array([0, 0, 0, 0, 0]))
+                        return Action(np.array([0, 0, 0, 0, 0], dtype=np.float32))
                 elif ry == iy:
                     if rx > ix:
-                        return Action(np.array([0, 0, 1, 0, 0]))
+                        return Action(np.array([0, 0, 1, 0, 0], dtype=np.float32))
                     elif rx < ix:
-                        return Action(np.array([0, 0, 3, 0, 0]))
+                        return Action(np.array([0, 0, 3, 0, 0], dtype=np.float32))
 
             # Move until we are adjacent
             dx = np.clip(ix - rx, -1, 1)
             dy = np.clip(iy - ry, -1, 1)
-            return Action(np.array([dx, dy, -1, 0, 0]))
+            return Action(np.array([dx, dy, -1, 0, 0], dtype=np.float32))
 
         return policy
 
