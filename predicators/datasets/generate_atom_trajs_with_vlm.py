138e67cefa718a77b9bf1994547657e164a04216
Tom Silver <tomsilver@users.noreply.github.com>
2024-05-16 14:17:38 -0400

    allow dashes and underlines in object names for VLM parsing (#1648)

--- predicators/datasets/generate_atom_trajs_with_vlm.py
+++ predicators/datasets/generate_atom_trajs_with_vlm.py
@@ -182,7 +182,7 @@ def _parse_unique_atom_proposals_from_list(
         assert len(atoms_proposal_for_traj) == 1
         curr_atoms_proposal = atoms_proposal_for_traj[0]
         # Regex pattern to match predicates
-        atom_match_pattern = r"\b[a-z_]+\([a-z0-9_, ]+\)"
+        atom_match_pattern = r"\b[a-z0-9_-]+\([a-z0-9_, -]+\)"
         # Find all matches in the text
         matches = re.findall(atom_match_pattern,
                              curr_atoms_proposal,
@@ -190,7 +190,7 @@ def _parse_unique_atom_proposals_from_list(
         for atom_proposal_txt in matches:
             num_atoms_considered += 1
             atom_is_valid = True
-            atom = re.sub(r"[^\w\s\(\),]", "", atom_proposal_txt).strip(' ')
+            atom = re.sub(r"[^\w\s\(\),_-]", "", atom_proposal_txt).strip(' ')
             obj_names = re.findall(r'\((.*?)\)', atom)
             if obj_names:
                 obj_names_list = [
