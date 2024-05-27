d9eb4aa20f89754dccf97b906d9b2655d8a82519
“matrixbalto” <ilfangliu@gmail.com>
2024-05-27 19:40:36 -0400

    pr grid_row_door

--- predicators/ground_truth_models/grid_row/options.py
+++ predicators/ground_truth_models/grid_row/options.py
@@ -80,3 +80,77 @@ class GridRowGroundTruthOptionFactory(GroundTruthOptionFactory):
         )
 
         return {MoveRobot, TurnOnLight, TurnOffLight, JumpToLight}
+
+
+class GridRowDoorGroundTruthOptionFactory(GridRowGroundTruthOptionFactory):
+    """Ground-truth options for the grid row door environment."""
+
+    @classmethod
+    def get_env_names(cls) -> Set[str]:
+        return {"grid_row_door"}
+
+    @classmethod
+    def get_options(cls, env_name: str, types: Dict[str, Type],
+                    predicates: Dict[str, Predicate],
+                    action_space: Box) -> Set[ParameterizedOption]:
+
+        robot_type = types["robot"]
+        light_type = types["light"]
+        cell_type = types["cell"]
+        door_type = types["door"]
+
+        # MoveTo
+        def _MoveTo_policy(state: State, memory: Dict,
+                           objects: Sequence[Object], params: Array) -> Action:
+            del memory, params  # unused
+            robot, _, target_cell = objects
+            rob_x = state.get(robot, "x")
+            target_x = state.get(target_cell, "x")
+            dx = target_x - rob_x
+            return Action(np.array([dx, 0.0, 0.0], dtype=np.float32))
+
+        MoveRobot = utils.SingletonParameterizedOption(
+            "MoveRobot",
+            types=[robot_type, cell_type, cell_type],
+            policy=_MoveTo_policy,
+        )
+
+        def _toggle_light_policy(state: State, memory: Dict,
+                                 objects: Sequence[Object],
+                                 params: Array) -> Action:
+            del state, objects, memory  # unused
+            dlight, = params
+            return Action(np.array([0.0, dlight, 0.0], dtype=np.float32))
+
+        # TurnOnLight
+        TurnOnLight = utils.SingletonParameterizedOption(
+            "TurnOnLight",
+            types=[robot_type, cell_type, light_type],
+            policy=_toggle_light_policy,
+            params_space=Box(-1.0, 1.0, (1, )),
+        )
+
+        # TurnOffLight
+        TurnOffLight = utils.SingletonParameterizedOption(
+            "TurnOffLight",
+            types=[robot_type, cell_type, light_type],
+            policy=_toggle_light_policy,
+            params_space=Box(-1.0, 1.0, (1, )),
+        )
+
+        #OpenDoor
+        def _toggle_door_policy(state: State, memory: Dict,
+                                objects: Sequence[Object],
+                                params: Array) -> Action:
+            del state, objects, memory  # unused
+            ddoor, = params
+            return Action(np.array([0.0, 0.0, ddoor], dtype=np.float32))
+
+        OpenDoor = utils.SingletonParameterizedOption(
+            "OpenDoor",
+            types=[robot_type, cell_type, door_type],
+            policy=_toggle_door_policy,
+            params_space=Box(0.0, 1.0, (1, )),
+        )
+
+        return {MoveRobot, TurnOnLight, TurnOffLight, OpenDoor}
--- tests/envs/test_grid_row.py
+++ tests/envs/test_grid_row.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 
 from predicators import utils
-from predicators.envs.grid_row import GridRowEnv
+from predicators.envs.grid_row import GridRowDoorEnv, GridRowEnv
 from predicators.ground_truth_models import get_gt_nsrts, get_gt_options
 
 
@@ -134,3 +134,118 @@ def test_grid_row():
         state = env.simulate(state, action)
         assert option.terminal(state)
     assert not all(a.holds(state) for a in ground_nsrt.add_effects)
+
+
+def test_grid_row_door():
+    """Tests for the Grid Row environment."""
+    utils.reset_config({
+        "env": "grid_row_door",
+        "num_train_tasks": 1,
+        "num_test_tasks": 2,
+        "grid_row_num_cells": 10,
+    })
+    env = GridRowDoorEnv()
+    assert env.get_name() == "grid_row_door"
+    for env_task in env.get_train_tasks():
+        task = env_task.task
+        for obj in task.init:
+            assert len(obj.type.feature_names) == len(task.init[obj])
+    for env_task in env.get_test_tasks():
+        task = env_task.task
+        for obj in task.init:
+            assert len(obj.type.feature_names) == len(task.init[obj])
+    assert len(env.predicates) == 6
+    Adjacent, DoorInCell, LightInCell, LightOff, LightOn, RobotInCell = \
+        sorted(env.predicates)
+    assert Adjacent.name == "Adjacent"
+    assert LightInCell.name == "LightInCell"
+    assert LightOff.name == "LightOff"
+    assert LightOn.name == "LightOn"
+    assert RobotInCell.name == "RobotInCell"
+    assert DoorInCell.name == "DoorInCell"
+    assert env.goal_predicates == {LightOn}
+    options = get_gt_options(env.get_name())
+    assert len(env.types) == 4
+    cell_type, door_type, light_type, robot_type = sorted(env.types)
+    assert cell_type.name == "cell"
+    assert light_type.name == "light"
+    assert robot_type.name == "robot"
+    assert door_type.name == "door"
+    assert env.action_space.shape == (3, )
+    nsrts = get_gt_nsrts(env.get_name(), env.predicates, options)
+    assert len(nsrts) == 4
+    assert len(options) == len(nsrts)
+    env_train_tasks = env.get_train_tasks()
+    assert len(env_train_tasks) == 1
+    env_test_tasks = env.get_test_tasks()
+    assert len(env_test_tasks) == 2
+    env_task = env_test_tasks[1]
+    env.reset("test", 1)
+    with pytest.raises(NotImplementedError):
+        env.render(caption="Test")
+
+    # Test NSRTs.
+    MoveRobot, OpenDoor, TurnOffLight, TurnOnLight = sorted(nsrts)
+    assert OpenDoor.name == "OpenDoor"
+    assert MoveRobot.name == "MoveRobot"
+    assert TurnOffLight.name == "TurnOffLight"
+    assert TurnOnLight.name == "TurnOnLight"
+
+    init_state = env_test_tasks[0].task.init
+    rng = np.random.default_rng(123)
+
+    # Test successful turning on the light.
+    robot, = init_state.get_objects(robot_type)
+    light, = init_state.get_objects(light_type)
+    door, = init_state.get_objects(door_type)
+    cell_order = sorted(init_state.get_objects(cell_type),
+                        key=lambda o: int(o.name[len("cell"):]))
+    ground_nsrt_plan = []
+    # First move to the light.
+    state = init_state.copy()
+    for cell, next_cell in zip(cell_order[:-1], cell_order[1:]):
+        if len(ground_nsrt_plan) == state.get(door, "x") - 0.5:
+            ground_nsrt_plan.append(OpenDoor.ground([robot, cell, door]))
+            ground_nsrt_plan.append(MoveRobot.ground([robot, cell, next_cell]))
+        else:
+            ground_nsrt_plan.append(MoveRobot.ground([robot, cell, next_cell]))
+    print(ground_nsrt_plan)
+    print(state.get(door, "x"))
+    rng = np.random.default_rng(123)
+    for ground_nsrt in ground_nsrt_plan:
+        print(state)
+        print(ground_nsrt.preconditions)
+        assert all(a.holds(state) for a in ground_nsrt.preconditions)
+        option = ground_nsrt.sample_option(state, set(), rng)
+        assert option.initiable(state)
+        action = option.policy(state)
+        state = env.simulate(state, action)
+        assert option.terminal(state)
+        assert all(a.holds(state) for a in ground_nsrt.add_effects)
+        assert not any(a.holds(state) for a in ground_nsrt.delete_effects)
+    # Now repeatedly turn on the light until it succeeds.
+    ground_nsrt = TurnOnLight.ground([robot, cell_order[-1], light])
+    for _ in range(100):
+        assert all(a.holds(state) for a in ground_nsrt.preconditions)
+        option = ground_nsrt.sample_option(state, set(), rng)
+        assert option.initiable(state)
+        action = option.policy(state)
+        state = env.simulate(state, action)
+        assert option.terminal(state)
+        if all(a.holds(state) for a in ground_nsrt.add_effects):
+            break
+    assert all(a.holds(state) for a in ground_nsrt.add_effects)
+    assert not any(a.holds(state) for a in ground_nsrt.delete_effects)
+    # Now repeatedly turn off the light until it succeeds.
+    ground_nsrt = TurnOffLight.ground([robot, cell_order[-1], light])
+    for _ in range(100):
+        assert all(a.holds(state) for a in ground_nsrt.preconditions)
+        option = ground_nsrt.sample_option(state, set(), rng)
+        assert option.initiable(state)
+        action = option.policy(state)
+        state = env.simulate(state, action)
+        assert option.terminal(state)
+        if all(a.holds(state) for a in ground_nsrt.add_effects):
+            break
+    assert all(a.holds(state) for a in ground_nsrt.add_effects)
+    assert not any(a.holds(state) for a in ground_nsrt.delete_effects)
