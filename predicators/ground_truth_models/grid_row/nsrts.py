4d54813496756d3cdf76b548b69b9a23c311010f
Alicia Li <102700848+matrixbalto@users.noreply.github.com>
2024-05-29 12:05:53 -0400

    Added GridRowDoor environment! (#1659)
    * pr grid_row_door
    
    * minor changes
    
    * minor fixes
    
    * minor minor fixes
    
    ---------
    
    Co-authored-by: “matrixbalto” <“ilfangliu@gmail.com”>

--- predicators/ground_truth_models/grid_row/nsrts.py
+++ predicators/ground_truth_models/grid_row/nsrts.py
@@ -147,3 +147,143 @@ class GridRowGroundTruthNSRTFactory(GroundTruthNSRTFactory):
         nsrts.add(impossible_nsrt)
 
         return nsrts
+
+
+class GridRowDoorGroundTruthNSRTFactory(GridRowGroundTruthNSRTFactory):
+    """Ground-truth NSRTs for the grid row door environment."""
+
+    @classmethod
+    def get_env_names(cls) -> Set[str]:
+        return {"grid_row_door"}
+
+    @staticmethod
+    def get_nsrts(env_name: str, types: Dict[str, Type],
+                  predicates: Dict[str, Predicate],
+                  options: Dict[str, ParameterizedOption]) -> Set[NSRT]:
+        # Types
+        robot_type = types["robot"]
+        light_type = types["light"]
+        cell_type = types["cell"]
+        door_type = types["door"]
+
+        # Predicates
+        RobotInCell = predicates["RobotInCell"]
+        LightInCell = predicates["LightInCell"]
+        LightOn = predicates["LightOn"]
+        LightOff = predicates["LightOff"]
+        Adjacent = predicates["Adjacent"]
+        DoorInCell = predicates["DoorInCell"]
+
+        # Options
+        MoveRobot = options["MoveRobot"]
+        TurnOnLight = options["TurnOnLight"]
+        TurnOffLight = options["TurnOffLight"]
+        OpenDoor = options["OpenDoor"]
+
+        nsrts = set()
+
+        def light_sampler(state: State, goal: Set[GroundAtom],
+                          rng: np.random.Generator,
+                          objs: Sequence[Object]) -> Array:
+            del state, goal, objs  # unused
+            # Note: just return a random value from -1 to 1
+            return np.array([rng.uniform(-1.0, 1.0)], dtype=np.float32)
+
+        # MoveRobot
+        robot = Variable("?robot", robot_type)
+        current_cell = Variable("?current_cell", cell_type)
+        target_cell = Variable("?target_cell", cell_type)
+        parameters = [robot, current_cell, target_cell]
+        option_vars = parameters
+        option = MoveRobot
+        preconditions = {
+            LiftedAtom(Adjacent, [current_cell, target_cell]),
+            LiftedAtom(RobotInCell, [robot, current_cell]),
+        }
+        add_effects = {
+            LiftedAtom(RobotInCell, [robot, target_cell]),
+        }
+        delete_effects = {
+            LiftedAtom(RobotInCell, [robot, current_cell]),
+        }
+        ignore_effects: Set[Predicate] = set()
+        move_robot_nsrt = NSRT("MoveRobot", parameters, preconditions,
+                               add_effects, delete_effects, ignore_effects,
+                               option, option_vars, null_sampler)
+        nsrts.add(move_robot_nsrt)
+
+        # TurnOnLight
+        robot = Variable("?robot", robot_type)
+        current_cell = Variable("?current_cell", cell_type)
+        light = Variable("?light", light_type)
+        parameters = [robot, current_cell, light]
+        option_vars = parameters
+        option = TurnOnLight
+        preconditions = {
+            LiftedAtom(LightInCell, [light, current_cell]),
+            LiftedAtom(RobotInCell, [robot, current_cell]),
+            LiftedAtom(LightOff, [light]),
+        }
+        add_effects = {
+            LiftedAtom(LightOn, [light]),
+        }
+        delete_effects = {
+            LiftedAtom(LightOff, [light]),
+        }
+        ignore_effects = set()
+        turn_light_on_nsrt = NSRT("TurnOnLight", parameters, preconditions,
+                                  add_effects, delete_effects, ignore_effects,
+                                  option, option_vars, light_sampler)
+        nsrts.add(turn_light_on_nsrt)
+
+        # TurnOffLight
+        robot = Variable("?robot", robot_type)
+        current_cell = Variable("?current_cell", cell_type)
+        light = Variable("?light", light_type)
+        parameters = [robot, current_cell, light]
+        option_vars = parameters
+        option = TurnOffLight
+        preconditions = {
+            LiftedAtom(LightInCell, [light, current_cell]),
+            LiftedAtom(RobotInCell, [robot, current_cell]),
+            LiftedAtom(LightOn, [light]),
+        }
+        add_effects = {
+            LiftedAtom(LightOff, [light]),
+        }
+        delete_effects = {
+            LiftedAtom(LightOn, [light]),
+        }
+        ignore_effects = set()
+        turn_light_off_nsrt = NSRT("TurnOffLight", parameters, preconditions,
+                                   add_effects, delete_effects, ignore_effects,
+                                   option, option_vars, light_sampler)
+        nsrts.add(turn_light_off_nsrt)
+
+        # OpenDoor
+        def door_sampler(state: State, goal: Set[GroundAtom],
+                         rng: np.random.Generator,
+                         objs: Sequence[Object]) -> Array:
+            del state, goal, objs  # unused
+            # Note: return 1.0 to show door is now open
+            return np.array([rng.uniform(1.0, 1.0)], dtype=np.float32)
+
+        robot = Variable("?robot", robot_type)
+        current_cell = Variable("?current_cell", cell_type)
+        door = Variable("?door", door_type)
+        parameters = [robot, current_cell, door]
+        option_vars = parameters
+        option = OpenDoor
+        preconditions = {
+            LiftedAtom(DoorInCell, [door, current_cell]),
+            LiftedAtom(RobotInCell, [robot, current_cell]),
+        }
+        add_effects = set()
+        delete_effects = set()
+        ignore_effects = set()
+        open_door_nsrt = NSRT("OpenDoor", parameters, preconditions,
+                              add_effects, delete_effects, ignore_effects,
+                              option, option_vars, door_sampler)
+        nsrts.add(open_door_nsrt)
+
+        return nsrts
