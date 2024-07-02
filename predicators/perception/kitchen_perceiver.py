77ff62e6eadb2363a9248c16ab785646c48d37b5
Nishanth Kumar <NishanthJKumar@users.noreply.github.com>
2024-07-01 23:35:43 -0700

    Kitchen goal complexity (#1685)
    * making progress
    
    * planning for new kitchen goal(s) works!
    
    * fix mypy
    
    * yeet

--- predicators/perception/kitchen_perceiver.py
+++ predicators/perception/kitchen_perceiver.py
@@ -21,28 +21,41 @@ class KitchenPerceiver(BasePerceiver):
         KettleBoiling = pred_name_to_pred["KettleBoiling"]
         kettle = KitchenEnv.object_name_to_object("kettle")
         knob4 = KitchenEnv.object_name_to_object("knob4")
+        knob3 = KitchenEnv.object_name_to_object("knob3")
         burner4 = KitchenEnv.object_name_to_object("burner4")
+        burner3 = KitchenEnv.object_name_to_object("burner3")
         light = KitchenEnv.object_name_to_object("light")
         goal_desc = env_task.goal_description
-        if goal_desc == ("Move the kettle to the back burner and turn it on; "
-                         "also turn on the light"):
+        if goal_desc == (
+                "Move the kettle to the back left burner and turn it on; "
+                "also turn on the light"):
             goal = {
                 GroundAtom(TurnedOn, [knob4]),
                 GroundAtom(OnTop, [kettle, burner4]),
                 GroundAtom(TurnedOn, [light]),
             }
-        elif goal_desc == "Move the kettle to the back burner":
+        elif goal_desc == "Move the kettle to the back left burner":
             goal = {GroundAtom(OnTop, [kettle, burner4])}
-        elif goal_desc == "Turn on the back burner":
+        elif goal_desc == "Move the kettle to the back right burner":
+            goal = {GroundAtom(OnTop, [kettle, burner3])}
+        elif goal_desc == "Turn on the back left burner":
             goal = {
                 GroundAtom(TurnedOn, [knob4]),
+            }
+        elif goal_desc == "Turn on the back right burner":
+            goal = {
+                GroundAtom(TurnedOn, [knob3]),
             }
         elif goal_desc == "Turn on the light":
             goal = {
                 GroundAtom(TurnedOn, [light]),
             }
-        elif goal_desc == "Move the kettle to the back burner and turn it on":
+        elif goal_desc == ("Move the kettle to the back left burner "
+                           "and turn it on"):
             goal = {GroundAtom(KettleBoiling, [kettle, burner4, knob4])}
+        elif goal_desc == ("Move the kettle to the back right burner "
+                           "and turn it on"):
+            goal = {GroundAtom(KettleBoiling, [kettle, burner3, knob3])}
         else:
             raise NotImplementedError(f"Unrecognized goal: {goal_desc}")
         return Task(state, goal)
