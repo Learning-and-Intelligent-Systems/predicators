57b3ab0efba5fef422aadf59c0f81f0054c05fc5
Yichao Liang <ycliang6@gmail.com>
2024-08-03 19:30:54 +0100

    Introducing an INTERPRET-Inspired Baseline for VLM predicate (#1699)
    * draft generate dataset with vision api. use with flat --vlm_predicate_vision_api_generate_ground_atoms True
    
    * vision api baseline running
    
    * fix pylint
    
    * works with multiple demos
    
    * yapf"
    
    * fix mypy
    
    * improved regex
    
    * uncomment sample
    
    * restore accdentally deleted files and changes to docstrings
    
    * fix pylint
    
    * add test for lfd-vision-api
    
    * add pylint: disable=exec-used
    
    * yapf
    
    * add prompt file for pytest
    
    * fix tests
    
    * yapf
    
    * pylint style
    
    * git add tests
    
    * yapf
    
    * fix test
    
    * refactor
    
    * yapf
    
    ---------
    
    Co-authored-by: yichao-liang <yichao-liang>

--- predicators/envs/burger.py
+++ predicators/envs/burger.py
@@ -58,16 +58,13 @@ class BurgerEnv(BaseEnv):
     _object_type = Type("object", [])
     _item_type = Type("item", [], _object_type)
     _station_type = Type("station", [], _object_type)
-
     _robot_type = Type("robot", ["row", "col", "z", "fingers", "dir"],
                        _object_type)
-
     _patty_type = Type("patty", ["row", "col", "z"], _item_type)
     _tomato_type = Type("tomato", ["row", "col", "z"], _item_type)
     _cheese_type = Type("cheese", ["row", "col", "z"], _item_type)
     _bottom_bun_type = Type("bottom_bun", ["row", "col", "z"], _item_type)
     _top_bun_type = Type("top_bun", ["row", "col", "z"], _item_type)
-
     _grill_type = Type("grill", ["row", "col", "z"], _station_type)
     _cutting_board_type = Type("cutting_board", ["row", "col", "z"],
                                _station_type)
