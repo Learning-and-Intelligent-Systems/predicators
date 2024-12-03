d767217296e2742a9186b72bf1f607b7611a18f8
yichao-liang <yichao-liang>
2024-12-03 06:47:06 -0500

    clean up code

--- predicators/envs/pybullet_coffee.py
+++ predicators/envs/pybullet_coffee.py
@@ -66,28 +66,21 @@ class PyBulletCoffeeEnv(PyBulletEnv, CoffeeEnv):
     button_x: ClassVar[float] = machine_x
     button_y: ClassVar[
         float] = machine_y - machine_y_len / 2 - machine_top_y_len
-    # button_z: ClassVar[float] = z_lb + 3 * machine_z_len / 4
     button_z: ClassVar[float] = z_lb + machine_z_len - button_radius
     button_press_threshold: ClassVar[float] = 1e-3
     machine_color: ClassVar[Tuple[float, float, float, float]] =\
         (0.1, 0.1, 0.1, 1) # Black
-    # (0.75, 0.75, 0.75, 1.0) # Grey
     button_color_on: ClassVar[Tuple[float, float, float,
                                     float]] = (0.2, 0.5, 0.2, 1.0)
     plate_color_on: ClassVar[Tuple[float, float, float, float]] = machine_color
-    #    float]] = (0.9, 0.3, 0.0, 0.7)
     button_color_off: ClassVar[Tuple[float, float, float,
                                      float]] = (0.5, 0.2, 0.2, 1.0)
     plate_color_off: ClassVar[Tuple[float, float, float,
                                     float]] = machine_color
-    # float]] = (0.6, 0.6, 0.6, 0.5)
     jug_color: ClassVar[Tuple[float, float, float, float]] =\
         (0.5,1,0,0.5) # Green
-    # (1.0, 1.0, 1.0, 1.0) # White
-    # jug_color: ClassVar[Tuple[float, float, float, float]] = (1.0, 1.0, 1.0, 1.0)
     jug_radius: ClassVar[float] = 0.3 * machine_y_len
     jug_height: ClassVar[float] = 0.15 * (z_ub - z_lb)  # kettle urdf
-    # jug_height: ClassVar[float] = 0.2 * (z_ub - z_lb) # cup urdf
     jug_init_x_lb: ClassVar[
         float] = machine_x - machine_x_len / 2 + init_padding
     jug_init_x_ub: ClassVar[
@@ -99,7 +92,6 @@ class PyBulletCoffeeEnv(PyBulletEnv, CoffeeEnv):
     jug_init_y_ub_og: ClassVar[
         float] = machine_y - machine_y_len - 3 * jug_radius - init_padding
     jug_handle_offset: ClassVar[float] = 3 * jug_radius  # kettle urdf
-    # jug_handle_offset: ClassVar[float] = 2.2 * jug_radius # cup urdf
     jug_handle_height: ClassVar[float] = jug_height
     jug_init_rot_lb: ClassVar[float] = -2 * np.pi / 3
     jug_init_rot_ub: ClassVar[float] = 2 * np.pi / 3
