5589fe05539654a6351a58f6e15991cf92c9cf8f
yichao-liang <ycliang6@gmail.com>
2025-01-24 13:30:21 +0000

    refactor create_object and update_object to use default orientation and add mass parameter

--- predicators/pybullet_helpers/objects.py
+++ predicators/pybullet_helpers/objects.py
@@ -4,17 +4,17 @@ from typing import Optional, Sequence, Tuple
 import pybullet as p
 
 from predicators import utils
-from predicators.envs.pybullet_env import PyBulletEnv
 from predicators.pybullet_helpers.geometry import Pose, Pose3D, Quaternion
 
 # import numpy as np
-
+default_orn: Sequence[float] = [0.0, 0.0, 0.0, 1.0]
 
 def create_object(asset_path: str,
                   position: Pose3D = (0, 0, 0),
-                  orientation: Quaternion = PyBulletEnv._default_orn,
+                  orientation: Quaternion = default_orn,
                   color: Optional[Tuple[float, float, float, float]] = None,
                   scale: float = 0.2,
+                  mass: Optional[float] = None,
                   use_fixed_base: bool = False,
                   physics_client_id: int = 0) -> int:
     """Create a pot object in the environment."""
@@ -32,13 +32,17 @@ def create_object(asset_path: str,
         # Change color of all links
         for link_id in range(p.getNumJoints(obj_id)):
             p.changeVisualShape(obj_id, link_id, rgbaColor=color)
+    
+    if mass is not None:
+        p.changeDynamics(obj_id, -1, mass=mass, 
+                         physicsClientId=physics_client_id)
 
     return obj_id
 
 
 def update_object(obj_id: int,
                   position: Optional[Pose3D] = None,
-                  orientation: Quaternion = PyBulletEnv._default_orn,
+                  orientation: Quaternion = default_orn,
                   color: Optional[Tuple[float, float, float, float]] = None,
                   physics_client_id: int = 0) -> None:
     """Update the position and orientation of an object."""
